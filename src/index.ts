784873184ea87908e58ba80f9de6f301283b895d
softmarshmallow <woojoo@softmarshmallow.com>
2020-09-22 18:34:53 +0900

    ts skia wasm implemented with typings

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
-import { CanvasKit } from "canvaskit-wasm"
-// import * as CanvasKitInit from "canvaskit-wasm"
+import { CanvasKit, SkCanvas } from "canvaskit-wasm"
+import CanvasKitInit from "canvaskit-wasm/bin/canvaskit";
+
+async function main() {
+
+    const CanvasKit: CanvasKit = await CanvasKitInit().ready();
+
+    const el = document.getElementById('main');
+    const surface = CanvasKit.MakeCanvasSurface(el);
 
 
-// CanvasKitInit({
-//     locateFile: (file) => __dirname + '/bin/' + file,
-// }).then((CanvasKit) => {
-//     // Code goes here using CanvasKit
-// });
-// function test() {
-//     CanvasKitInit()
-// }
+    const paint = new CanvasKit.SkPaint();
+    paint.setColor(CanvasKit.Color(0.9, 0, 0, 1.0));
+    paint.setStyle(CanvasKit.PaintStyle.Stroke);
+    paint.setAntiAlias(true);
+    const rr = CanvasKit.RRectXY(CanvasKit.LTRBRect(10, 60, 210, 260), 25, 15);
+
+    function draw(canvas: SkCanvas) {
+        canvas.clear(CanvasKit.WHITE);
+        canvas.drawRRect(rr, paint);
+    }
+
+    surface.drawOnce(draw);
+}
+
+main();
