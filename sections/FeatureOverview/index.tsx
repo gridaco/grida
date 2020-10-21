782540462617d4cee6c1af66c71484740822662b
You-J <ijyk3061@gmail.com>
2020-10-21 23:44:15 +0900

    Stash add grid

--- sections/FeatureOverview/index.tsx
+++ sections/FeatureOverview/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Text } from '../../components';
 // @ts-ignore
 import styles from './index.module.scss';
+// @ts-ignore
+import codeBg from '../../images/codeBg.svg';
 
 const FeatureOverview = () => {
   return (
     <>
-      <div className={styles.code_background}></div>
+      <div className={styles.code_background}>
+        <img src={codeBg} />
+      </div>
+      <div>
+        
+      </div>
     </>
   );
 };
