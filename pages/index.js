3b5f406bec53f4199c9e8a4b12050895d7bcfb45
softmarshmallow <woojoo@softmarshmallow.com>
2020-10-25 17:04:32 +0900

    added meta tags for seo optimization

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title> bridged.xyz </title> <link rel="icon" href="/favicon.ico " />{' '}
+        {/* region seo optimizations */}
+        <title> bridged.xyz </title>
+        <meta name="description" content="designs that are meant to be implemented. automate your frontend development process. no more boring."></meta>
+        <meta name="keywords" content="flutter, design to code, figma to code, flutter code generation, design handoff, design linting, code generation"></meta>
+        <meta name="author" content="bridged.xyz team and community collaborators"></meta>
+        {/* region seo optimizations */}
+        <link rel="icon" href="/favicon.ico " />{' '}
       </Head>
       <main className={styles.main}>
         <Header />
