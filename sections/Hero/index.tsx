956be700b577147151d8deeea0fc549b154dc1e7
You-J <ijyk3061@gmail.com>
2021-01-06 07:26:01 +0900

    Added youtube player layout

--- sections/Hero/index.tsx
+++ sections/Hero/index.tsx
@@ -4,19 +4,31 @@ import {
   CardMedia,
   Card,
   CardActionArea,
+  IconButton,
 } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { CustomButton, Text } from '../../components';
 // @ts-ignore
 import styles from './index.module.scss';
 // @ts-ignore
 import home from '../../styles/Home.module.scss';
-
 import { BRIDGED_COMMUNITY_FLUTTER } from '../../common/toolkit';
-
 import { heroContents } from './toolkit';
+import { VideoPlayer } from '../Hero/videoPlayer';
+
+// @ts-ignore
+import videoPreview from '../../images/hero-video-preview.png';
+// @ts-ignore
+import videoPlayBtn from '../../images/video-play.svg';
+import CloseIcon from '@material-ui/icons/Close';
 
 const Hero = () => {
+  const [isPlayer, setIsPlayer] = useState('none');
+
+  const ControlPlayer = (display: string) => {
+    setIsPlayer(display);
+  };
+
   return (
     <>
       <div className={styles.title_box}>
@@ -40,14 +52,52 @@ const Hero = () => {
             <Text variant="button" value="GET STARTED" />
           </Button>
         </div>
-        <div className={styles.video}>
-          <iframe
-            className={styles.video}
-            src="https://www.youtube.com/embed/RIZjZFoDhRc?controls=0"
-            // frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            // allowfullscreen
-          ></iframe>
+        <div className={styles.video_player_wrapper}>
+          <div className={styles.video}>
+            <img
+              src={videoPreview}
+              className={styles.video}
+              alt="video preview"
+              onClick={() => {
+                ControlPlayer('block');
+              }}
+            />
+            <img
+              src={videoPlayBtn}
+              className={styles.video_play}
+              alt="video player button"
+              onClick={() => {
+                ControlPlayer('block');
+              }}
+            />
+          </div>
+
+          <div
+            className={styles.youtube_player}
+            style={{ display: `${isPlayer}` }}
+          >
+            <div className={styles.player_background}>
+              <IconButton
+                aria-label="add to shopping cart"
+                onClick={() => {
+                  ControlPlayer('none');
+                }}
+                style={{ color: '#fff' }}
+              >
+                <CloseIcon fontSize="large" />
+              </IconButton>
+            </div>
+            <div className={styles.youtube_player_wrapper}>
+              <iframe
+                width="640"
+                height="360"
+                src="https://www.youtube.com/embed/RIZjZFoDhRc?autoplay=1&mute=1"
+                frameborder="0"
+                allowfullscreen="1"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              ></iframe>
+            </div>
+          </div>
         </div>
       </div>
     </>
