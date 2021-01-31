d53b40540de7e9b0c9953462a7fb29852cb52a41
GwonHeeJun <developer_gwon@naver.com>
2021-02-01 05:27:36 +0900

    add : sitemap generation code for SEO

---
+++ sitemap.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd public && rm -rf sitemap && mkdir sitemap
+cd .. && cd scripts
+printf "\n"
+
+
+for SITEMAP in 'common' 'whatsnew'; do
+    echo "Generating sitemap-${SITEMAP}.xml..."
+    node ./sitemap-${SITEMAP}.js
+    printf "\n"
+done
+
+echo "Compressing generated xml files..."
+node ./compress.js
+printf "\n"
+
+
+echo "Generating sitemap index files..."
+node ./sitemap.js
+printf "\n"
+cd ..
