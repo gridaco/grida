d984e235540e98a5e0cde3a884710fa9fbb86258
GwonHeeJun <developer_gwon@naver.com>
2021-03-01 00:33:45 +0900

    modify :  create dummy header & footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Box, Flex, Text } from 'rebass';
 
 const Footer = () => {
     return (
-        <div>
-
-        </div>
+        <Flex alignItems="center" justifyContent="center" width="100%" height="700px" bg="#000" >
+            <Text textAlign="center" color="#fff">Footer</Text>
+        </Flex>
     )
 }
 
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Box, Flex, Text } from 'rebass';
 
 const Header = () => {
     return (
-        <div>
-
-        </div>
+        <Flex alignItems="center" justifyContent="center" width="100%" height="60px" bg="#000" >
+            <Text textAlign="center" color="#fff">Header</Text>
+        </Flex>
     )
 }
 
