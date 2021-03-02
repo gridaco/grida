4a5a5c41a444ee0442ce0138d685de359ea09949
GwonHeeJun <developer_gwon@naver.com>
2021-03-02 20:19:20 +0900

    modify : edit header expand style,

--- web/components/header/expand-header-item/index.tsx
+++ web/components/header/expand-header-item/index.tsx
@@ -2,12 +2,12 @@ import Icon from 'components/icon';
 import React, { useCallback, useState } from 'react'
 import { Text, Flex, Box } from 'rebass';
 import styled from '@emotion/styled';
+import Product from 'components/product';
 
-const ExpandHeaderItem = ({ item }) => {
-    const [isExpand, setIsExpand] = useState(false);
+const ExpandHeaderItem = ({ item, isExpand, onExpandHeader, onContractHeader }) => {
 
     const onClose = useCallback(() => {
-        setIsExpand(false)
+        onContractHeader()
     }, []);
 
     const onModalInnerClick = useCallback((e: React.MouseEvent) => {
@@ -16,16 +16,35 @@ const ExpandHeaderItem = ({ item }) => {
 
     return (
         <Flex >
-            <Label color={isExpand ? "#000" : "#8B8B8B"} fontWeight="bold" fontSize="16px" onClick={() => setIsExpand(!isExpand)}>{item.label} <Icon name={isExpand ? "arrowUp" : "arrowDown"} isVerticalMiddle /></Label>
-            {isExpand && <ModalBackground onClick={onClose}>
-                <Modal
-                    p={["10px", "10px", "15px 30px"]}
-                    bg="white"
-                    height="430px"
-                    onClick={onModalInnerClick}
-                >
-
-                </Modal>
+            <Label
+                color={isExpand ? "#000" : "#8B8B8B"}
+                isBorder={isExpand}
+                onMouseOver={onExpandHeader}
+                fontWeight="bold"
+                fontSize="16px"
+            >
+                {item.label}
+                <Icon name={isExpand ? "arrowUp" : "arrowDown"} isVerticalMiddle />
+            </Label>
+            {isExpand && <ModalBackground onClick={onClose} >
+                    <Modal
+                        p={["10px", "10px", "15px 30px"]}
+                        bg="white"
+                        height="430px"
+                        onClick={onModalInnerClick}
+                        onMouseOver={onExpandHeader}
+                        onMouseLeave={onContractHeader}
+                    >
+                        <ExpandHeaderContent width={["320px", "730px", "985px", "1040px"]} height="100%">
+                            <Product />
+                            <Product />
+                            <Product />
+                            <Product />
+                            <Product />
+                            <Product />
+                            <Product />
+                        </ExpandHeaderContent>
+                    </Modal>
             </ModalBackground>}
         </Flex>
     )
@@ -33,12 +52,21 @@ const ExpandHeaderItem = ({ item }) => {
 
 export default ExpandHeaderItem
 
-const Label = styled(Text)`
+const Label = styled(Text) <{ isBorder: boolean }>`
     margin: 0px 12px;
     font-weight: bold;
     font-size: 16px;
     display: flex;
     align-items: center;
+    padding: 16px 0px;
+
+    ${p => p.isBorder && `
+        border-bottom: 2px solid black;
+    `}
+
+    &:hover {
+        color: #000;
+    }
 `
 
 const ModalBackground = styled.div`
@@ -52,12 +80,25 @@ const ModalBackground = styled.div`
   z-index: 1050;
 `;
 
-const Modal = styled(Box)`
+const Modal = styled(Flex)`
   position: absolute;
-  top: 16%;
+  top: 210px;
   left: 50%;
   width: 100%;
   overflow-y: scroll;
   box-shadow: 4px 6px 20px 0 rgba(0, 0, 0, 0.09);
   transform: translate(-50%, -50%);
+  align-items: center;
+  justify-content: center;
 `;
+
+const ExpandHeaderContent = styled(Flex)`
+    display: grid;
+    grid-template-columns: 250px 250px;
+    grid-template-rows: 70px 70px;
+    grid-column-gap: 48px;
+    grid-row-gap: 21px;
+
+    max-height: 350px;
+
+`
---
+++ web/components/product/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box } from 'rebass';
+import styled from '@emotion/styled';
+
+
+const Product = () => {
+  return (
+    <ProductWrapper width="100%" height="100%" bg="red">
+      as
+    </ProductWrapper>
+  )
+}
+
+export default Product
+
+const ProductWrapper = styled(Box)`
+  max-width: 255px;
+  max-height: 70px;
+`
