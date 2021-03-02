c6c547763c4a7043d7aef71dc1da10834ce7a244
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-02 21:34:38 +0900

    add landingpage urls

---
+++ web/utils/landingpage/constants.js
@@ -0,0 +1,22 @@
+export const LandingpageUrls = {
+    login: "https://accounts.bridged.xyz/login",
+    signup: "https://accounts.bridged.xyz/signup",
+
+    // video
+    latest_promotion_video_youtube: "https://www.youtube.com/watch?v=RIZjZFoDhRc&ab_channel=Bridged",
+
+    // linked articles
+    article_how_do_design_to_code_work: "/docs/concepts/design-to-code",
+    article_how_engine_works: "/docs/concepts/detection",
+
+
+    // social
+    facebook: "https://facebook.com/bridgedxyz",
+    instagram: "https://instagram.com/bridged.xyz",
+    twitter: "https://twitter.com/bridgedxyz",
+    github: "https://github.com/bridgedxyz",
+
+    // misc
+    privacy_policy: "docs/support/privacy-policy",
+    terms_and_conditions: "docs/support/terms-and-conditins",
+}
