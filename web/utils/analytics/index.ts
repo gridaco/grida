8afc82e49a54af05b901729cbe83abef4fb49846
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-03 16:01:37 +0900

    logger function wrapped for dev purpose

--- web/utils/analytics/index.ts
+++ web/utils/analytics/index.ts
@@ -2,5 +2,13 @@ import { firebase } from "../firebase";
 import { LOG_CTA_GET_STARTED } from "./event-keys";
 
 export function logCtaGetStartedClick() {
-  firebase.analytics().logEvent(LOG_CTA_GET_STARTED);
+  safelyLogEvent(LOG_CTA_GET_STARTED);
+}
+
+export function safelyLogEvent(key: string, params?: { [key: string]: any }) {
+  try {
+    firebase.analytics().logEvent(key);
+  } catch (_) {
+    console.warn(_);
+  }
 }
