07d5a7daa7eee36ab8cdd9961a88d93e7b20f53b
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-03 15:51:17 +0900

    add firebase envs

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,18 @@
 
 const withTM = require("next-transpile-modules");
 
+
+const FIREBASE_ENV_VARS =  {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUKET: process.env.FIREBASE_STORAGE_BUKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+}
+
 module.exports = withTM({
     webpack: function(config, {
         isServer
@@ -28,4 +40,7 @@ module.exports = withTM({
         return config;
     },
     transpileModules: ["lodash-es"],
+    env:{
+        ...FIREBASE_ENV_VARS
+    }
 });
