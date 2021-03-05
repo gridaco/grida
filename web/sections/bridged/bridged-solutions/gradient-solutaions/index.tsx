4da4ff55d09e27ed5fcad8697d3ca233f18a44c5
GwonHeeJun <developer_gwon@naver.com>
2021-03-05 20:18:44 +0900

    modify : responsive mobile view

--- web/sections/bridged/bridged-solutions/gradient-solutaions/index.tsx
+++ web/sections/bridged/bridged-solutions/gradient-solutaions/index.tsx
@@ -1,15 +1,54 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Box, Flex, Text } from 'rebass';
 import styled from '@emotion/styled';
 
 
-const GradientSolutions = ({ list, currentSolution, changeSolution }) => {
+const GradientSolutions = ({ list, currentSolution, changeSolution, type }) => {
+  const scrollabelDiv = useRef(null);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(3);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      scrollabelDiv.current.scrollLeft = 0
+      changeSolution("code")
+    }
+
+    const countdown = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(countdown);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => clearInterval(countdown);
+  }, [minutes, seconds]);
 
   return (
     <Postioner>
-      <LeftFade />
-      <ScrollView>
-        {list.map(i => <span className="cursor" onClick={() => changeSolution(i.title)} style={currentSolution === i.title ? { backgroundImage: i.gradient } : { color: "#F1F1F1" }}>{i.title}</span>)}
+
+      <ScrollView ref={scrollabelDiv}>
+        <Desktop width="100%">
+          {list.map((i, ix) => <span className="cursor" onClick={() => {
+            changeSolution(i.title);
+            scrollabelDiv.current.scrollLeft = i.width[0];
+            setSeconds(3)
+          }} style={currentSolution === i.title ? { backgroundImage: i.gradient } : { color: "#F1F1F1" }}>{i.title}</span>)}
+        </Desktop>
+        <Mobile width="100%">
+        {list.map((i, ix) => <span className="cursor" onClick={() => {
+            changeSolution(i.title);
+            scrollabelDiv.current.scrollLeft = i.width[1];
+            setSeconds(3)
+          }} style={currentSolution === i.title ? { backgroundImage: i.gradient } : { color: "#F1F1F1" }}>{i.title}</span>)}
+        </Mobile>
       </ScrollView>
       <RightFade />
     </Postioner>
@@ -18,9 +57,22 @@ const GradientSolutions = ({ list, currentSolution, changeSolution }) => {
 
 export default GradientSolutions
 
+const Mobile = styled(Box)`
+  display: none;
+  @media(max-width: 768px) {
+    display: block;
+  }
+`
+
+const Desktop = styled(Box)`
+  @media(max-width: 768px) {
+    display: none;
+  }
+`
+
 const Postioner = styled(Flex)`
   position: relative;
-  width: 100%;
+  width: 150%;
   height: 100px;
 
   
@@ -31,7 +83,20 @@ const Postioner = styled(Flex)`
 `
 
 const ScrollView = styled(Flex)`
-  overflow-x: auto;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+
+  &:before {
+    left: 0;
+    background: linear-gradient(90deg,#fff,hsla(0,0%,100%,0));
+    content: "";
+    top: 0;
+    width: 8%;
+    height: 100%;
+    position: absolute;
+    z-index: 100;
+    pointer-events: none;
+  }
 
   span {
     height: auto;
@@ -48,23 +113,28 @@ const ScrollView = styled(Flex)`
    
 
     &:first-child {
-      padding-left: 100px;
+      padding-left: 240px;
     }
 
     &:last-child {
-      padding-right: 100px;
+      padding-right: 1000px;
     }
   }
 `
 
 const LeftFade = styled(Box)`
-  position: absolute;
-  width: 200px;
-  height: 100px;
-  background: linear-gradient(270deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 51.56%);
-  @media(max-width: 400px) {
-    width: 50px
-  }
+
+    position: absolute;
+    width: 200px;
+    height: 100px;
+    background: linear-gradient(270deg, rgba(255, 255, 255, 0) 0%, #FFFFFF 51.56%);
+    top: 0px;
+    left: -15%;
+    @media(max-width: 400px) {
+      width: 50px
+    }
+ 
+  
 `
 
 const RightFade = styled(Box)`
--- web/sections/bridged/bridged-solutions/index.tsx
+++ web/sections/bridged/bridged-solutions/index.tsx
@@ -7,41 +7,52 @@ import GradientSolutions from './gradient-solutaions';
 
 const SolutionList = [
     {
-        title: "idea",
-        subTitle: "Instantly create 'idea' from your design",
-        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included."
-    },
-    {
         title: "code",
         subTitle: "Instantly create 'code' from your design",
         desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included.",
-        gradient: "linear-gradient(99.57deg, #6268FF 0%, #9039FF 100%)"
+        gradient: "linear-gradient(99.57deg, #6268FF 0%, #9039FF 100%)",
+        width: [0, 0]
     },
     {
         title: "server",
         subTitle: "Instantly create 'server' from your design",
-        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included."
+        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included.",
+        gradient: "linear-gradient(99.57deg, #B062FF 0%, #9C39FF 100%)",
+        width: [215, 200]
     },
     {
         title: "translations",
         subTitle: "Instantly create 'translations' from your design",
-        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included."
+        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included.",
+        gradient: "linear-gradient(99.57deg, #FBA33C 0%, #FFC700 100%)",
+        width: [475, 340]
     },
     {
         title: "insight",
         subTitle: "Instantly create 'insight' from your design",
-        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included."
+        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included.",
+        gradient: "linear-gradient(99.57deg, #6BCBC5 0%, #79E8AC 100%)",
+        width: [935, 560]
+    },
+    {
+        title: "GIT",
+        subTitle: "Instantly create 'GIT' from your design",
+        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included.",
+        gradient: "linear-gradient(99.57deg, #0E1279 0%, #632655 100%)",
+        width: [1215, 710]
     },
     {
         title: "everything",
         subTitle: "Instantly create 'everything' from your design",
-        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included."
+        desc: "With powerful Design2Code Engine, Bridged generates production ready code that can also easily be used for existing projects. Supprt for components, various code styles, naming convention, fille & directory structure are included.",
+        gradient: "linear-gradient(99.57deg, #9FA3F7 0%, #C49AFA 100%)",
+        width: [1385, 800]
     },
 ]
 
 const BridgedSolutions = () => {
     const [currentSolution, setCurrentSolution] = useState("code");
-    const [currentIndex, setCurrentIndex] = useState(1);
+    const [currentIndex, setCurrentIndex] = useState(0);
     useEffect(() => {
         SolutionList.map((i, ix) => {
             if (i.title === currentSolution) {
@@ -54,7 +65,7 @@ const BridgedSolutions = () => {
         <SolutionsWrapper alignItems="center" justifyContent="center" mx="20px">
             <Flex width={["320px", "730px", "985px", "1040px"]} alignItems="center" justifyContent="center" flexDirection="column">
                 <Text fontSize={["36px", "36px", "64px"]} fontWeight="bold" mr="auto">Your design is your</Text>
-                <GradientSolutions list={SolutionList} currentSolution={currentSolution} changeSolution={title => setCurrentSolution(title)} />
+                <GradientSolutions type="desktop" list={SolutionList} currentSolution={currentSolution} changeSolution={title => setCurrentSolution(title)} />
 
                 <Box width="100%" height="650px" mt="90px" bg="#000" mb="100px" />
 
@@ -63,7 +74,7 @@ const BridgedSolutions = () => {
 
                 <Text fontSize="18px" fontWeight="bold" mr="auto" mt="40px" pb="12px" style={{ borderBottom: "1px solid black" }}>See also</Text>
                 <Solutions mr="auto" mt="9px">
-                <Link href="/">
+                    <Link href="/">
                         <span>idea</span>
                     </Link>
                     <Link href="/">
