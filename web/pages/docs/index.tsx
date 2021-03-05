c22b875325a625077af125da4499b24aad447476
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-05 10:12:29 +0900

    add : temporary docs redirection

--- web/pages/docs/index.tsx
+++ web/pages/docs/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 export default function() {
+  // temporary redirection
+  const router = useRouter();
+  if (typeof window !== "undefined") {
+    router.push("/docs/getting-started");
+  }
   return (
-    <div>
+    <div style={{ textAlign: "center", margin: 120 }}>
       <h1>Welcome to Bridged Docs</h1>
+      <h2>You are now being redirected</h2>
     </div>
   );
 }
