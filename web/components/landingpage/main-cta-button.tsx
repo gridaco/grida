ca4d4fbf822689d216ed04729a73eb9584ba3cbe
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-08 07:59:21 +0900

    cta action provided

--- web/components/landingpage/main-cta-button.tsx
+++ web/components/landingpage/main-cta-button.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Button } from "rebass";
+import { URLS } from "utils/landingpage/constants";
 
 export default function LandingMainCtaButton() {
+  const handleCta = () => {
+    open(URLS.downloads.download_figma_plugin);
+  };
   return (
-    <Button mt={["32px", "90px", "90px", "90px"]} mb="50px">
+    <Button mt={["32px", "90px", "90px", "90px"]} mb="50px" onClick={handleCta}>
       Start now
     </Button>
   );
