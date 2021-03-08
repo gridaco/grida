65b3a8facb896e097a240529df34247a6dd10150
GwonHeeJun <developer_gwon@naver.com>
2021-03-08 09:07:08 +0900

    restore : prcing & docs page

---
+++ web/pages/pricing/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+export default function PricingPage() {
+  return (
+    <>
+      <div
+        style={{
+          margin: 120,
+        }}
+      >
+        <span style={{ textAlign: "center" }}>
+          <h2>Bridged is free to use</h2>
+          <p>
+            Free for dreamers. Bridged is Free for now, we don't take any forms
+            of payment for now. Feel free to create your awesome products with
+            it.
+          </p>
+        </span>
+      </div>
+    </>
+  );
+}
