1d0bf1d9c8d5baf916f1738c3e1f6b4fd9a2dd93
dann1y <skypedanny@naver.com>
2021-03-10 19:56:40 +0900

    modify : publish free-plan section

--- web/sections/pricing/free-plan/index.tsx
+++ web/sections/pricing/free-plan/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { Flex, Heading } from "rebass";
+import { Flex, Heading, Text } from "rebass";
 import SectionLayout from "layout/section";
+import StartNow from "components/start-now";
+import BlankArea from "components/blank-area";
 
 const FreePlan = () => {
   return (
     <SectionLayout alignContent="center">
-      {/* <Flex flexDirection="column">
-        <FreeText>Free for dreamers</FreeText>
-      </Flex> */}
+      <Flex alignItems="center" flexDirection="column" width="100%">
+        <BlankArea height={221} />
+        <FreeText mb="43px">Free for dreamers</FreeText>
+        <Desc width="80%" mb="132px">
+          Unlock your possibility, express your ideas faster then ever. Bridged
+          is free forever.
+        </Desc>
+
+        <StartNow />
+      </Flex>
     </SectionLayout>
   );
 };
@@ -21,4 +30,13 @@ const FreeText = styled(Heading)`
 
   text-align: center;
   letter-spacing: -0.025em;
+`;
+
+const Desc = styled(Text)`
+  font-size: 24px;
+  line-height: 34px;
+  letter-spacing: 0em;
+  text-align: center;
+
+  color: #707070;
 `;
