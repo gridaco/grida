f7e3fde56a87a48d1a38b63f2663395e4f0af947
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-16 18:06:32 +0900

    add globalization page

---
+++ web/pages/globalization/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function GlobalizationPage() {
+  return (
+    <div>
+      <p>hi. i'm globalization</p>
+      <SectionFaq />
+    </div>
+  );
+}
+
+function SectionFaq() {
+  return <div>FAQs</div>;
+}
