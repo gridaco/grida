46d113dae394d7b156490ad307b6ac221b331df4
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-17 19:01:07 +0900

    init mdx components

---
+++ web/components/mdx/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "./button";
+export const _MDX_COMPONENTS = {
+  h1: props => <h1 style={{ color: "black" }} {...props} />,
+};
+
+const Mdx = {
+  Button,
+};
+
+export default Mdx;
