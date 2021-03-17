a7b6b83299d4d6e9696bad771988537834c7349a
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-17 19:51:31 +0900

    update content description

--- web/utils/seo/index.ts
+++ web/utils/seo/index.ts
@@ -8,9 +8,9 @@ export const SEO_OG_DEFAULTS = {
 export const SEO_DEFAULTS = {
   title: "Bridged",
   description:
-    "designs that are meant to be implemented. automate your frontend development process. no more boring.",
+    "Designs, come to live. automate your frontend development. Instantly convert your design to production ready code.",
   keywords:
-    "flutter, design to code, figma to code, flutter code generation, design handoff, design linting, code generation",
+    "figma to flutter, design to code, figma to code, flutter code generation, design handoff, design linting, code generation",
   author: "bridged.xyz team and community collaborators",
   og: SEO_OG_DEFAULTS,
 };
