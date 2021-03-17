267ac6ec46aa4eb61d4ebae244ffaea4d2e1d32b
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-17 20:41:18 +0900

    og gif test

--- web/utils/seo/index.ts
+++ web/utils/seo/index.ts
@@ -1,11 +1,15 @@
 import { OpenGraphMeta, PageSeoMeta } from "./interface";
 
+const OG_IMAGE_STATIC =
+  "https://bridged-service-static.s3-us-west-1.amazonaws.com/opengraph/og-image.png";
+const OG_IMAGE_GIF =
+  "https://bridged-service-static.s3-us-west-1.amazonaws.com/opengraph/og-image-gif.gif";
+
 export const SEO_OG_DEFAULTS: OpenGraphMeta = {
   title: "Designs, come to live | Bridged",
   type: "website",
   url: "https://bridged.xyz",
-  image:
-    "https://bridged-service-static.s3-us-west-1.amazonaws.com/opengraph/og-image.png",
+  image: OG_IMAGE_GIF,
 };
 
 export const SEO_DEFAULTS: PageSeoMeta = {
