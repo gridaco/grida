977ba1a1345e47cc3a4ce8d586fa39cd1ce5eb12
GwonHeeJun <developer_gwon@naver.com>
2021-03-18 00:50:00 +0900

    modify : pricing page title

--- web/pages/pricing/index.tsx
+++ web/pages/pricing/index.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import React from "react";
 
 import Sections from "sections/pricing";
 export default function PricingPage() {
   return (
     <>
+    <Head>
+      <title>Pricing - Bridged</title>
+    </Head>
       <Sections.Hero_TryFreePlan />
       <Sections.ComparePlans />
       <Sections.FeaturesAndPricingTable />
