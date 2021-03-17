0826c993adcdc40e61e1ee0ac9776a5f2623fa5f
softmarshmallow <woojoo@softmarshmallow.com>
2021-03-17 19:08:12 +0900

    add layout support for mdx page

--- web/components/mdx/index.tsx
+++ web/components/mdx/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "./button";
+import { SectionLayout } from "./layout";
 export const _MDX_COMPONENTS = {
   h1: props => <h1 style={{ color: "black" }} {...props} />,
 };
 
 const Mdx = {
   Button,
+  SectionLayout,
 };
 
 export default Mdx;
