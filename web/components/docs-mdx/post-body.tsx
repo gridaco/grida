1613798804d96809214b65ab4242f2fd0d0dc00f
GwonHeeJun <developer_gwon@naver.com>
2021-03-19 14:43:49 +0900

    modify: add prev, next, helpful, github buttons

--- web/components/docs-mdx/post-body.tsx
+++ web/components/docs-mdx/post-body.tsx
@@ -5,6 +5,8 @@ import renderToString from "next-mdx-remote/render-to-string";
 import useAsyncEffect from "utils/hooks/use-async-effect";
 import CodeBlock from "components/code";
 import styled from "@emotion/styled";
+import { Flex, Text } from "rebass";
+import Icon from "components/icon";
 
 const components = {
   pre: props => <div {...props} />,
@@ -26,9 +28,63 @@ export default function PostBody({ content }) {
           __html: docs,
         }}
       />
+      <Flex justifyContent="space-between" mt="90px">
+        <Flex className="cursor" alignItems="center">
+          <CustomIcon
+            name="arrowDown"
+            isVerticalMiddle
+            style={{ transform: "rotate(90deg)" }}
+          />
+          <Text fontSize="16px" color="#2562FF">
+            Prev
+          </Text>
+        </Flex>
+
+        <Flex className="cursor" alignItems="center">
+          <Text fontSize="16px" color="#2562FF">
+            Next
+          </Text>
+          <CustomIcon
+            name="arrowDown"
+            isVerticalMiddle
+            style={{ transform: "rotate(270deg)" }}
+          />
+        </Flex>
+      </Flex>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        mt="60px"
+        flexDirection="column"
+      >
+        <Text fontSize="18px" fontWeight="bold" color="#686868">
+          Was this page helpful?
+        </Text>
+        <Flex mt="16px">
+          <Icon className="cursor" name="thumbsup" mx="16px" />
+          <Icon className="cursor" name="thumbsdown" mx="16px" />
+        </Flex>
+      </Flex>
+      <Flex
+        mt="40px"
+        justifyContent="flex-end"
+        style={{ borderTop: "1px solid #F8F8F8" }}
+      >
+        <Text className="cursor" pt="24px" fontSize="16px" color="#2562FF">
+          Edit this page on Github
+        </Text>
+      </Flex>
     </div>
   );
 }
+
+const CustomIcon = styled(Icon)`
+  svg {
+    path {
+      fill: #2562ff;
+    }
+  }
+`;
 
 const Wrap = styled.div`
   width: 100%;
