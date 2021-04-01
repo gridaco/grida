1782ce51e464c3a8d9eb0c96277436838716cb78
GwonHeeJun <developer_gwon@naver.com>
2021-04-02 02:21:42 +0900

    init : d2c layout ( file-explorer, dev-tools )

---
+++ editor/layout/file-explorer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+function FileExplorer() {
+  return <Wrapper></Wrapper>;
+}
+
+export default FileExplorer;
+
+const Wrapper = styled.div`
+  flex: 1;
+  background-color: red;
+`;
---
+++ editor/pages/_app.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { Global, css } from "@emotion/react";
+import styled from "@emotion/styled";
+import Head from "next/head";
+import FileExplorer from "../layout/file-explorer";
+import DevTools from "../layout/dev-tools";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <React.Fragment>
+      <Global
+        styles={css`
+          body {
+            margin: 0px;
+            font-family: "Roboto", sans-serif;
+            background-color: #181a22;
+          }
+        `}
+      />
+      <Head>
+        <SeoMeta />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
+      <Template>
+        <FileExplorer />
+        <ContentWrapper>
+          <RenderComponentWrapper>
+            <Component {...pageProps} />
+          </RenderComponentWrapper>
+          <DevTools />
+        </ContentWrapper>
+      </Template>
+    </React.Fragment>
+  );
+}
+
+function SeoMeta() {
+  return (
+    <>
+      <meta property="title" content="Design to Codes" />
+      <meta property="description" content="Design to Codes description" />
+    </>
+  );
+}
+
+export default MyApp;
+
+const Template = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 5;
+`;
+
+const RenderComponentWrapper = styled.div`
+  flex: 3;
+`;
