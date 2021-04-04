84926f73ca81356932b4bd27e9d1eacdb8cc95b8
GwonHeeJun <developer_gwon@naver.com>
2021-04-04 17:12:57 +0900

    add skia canvas

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,10 +1,7 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Canvas from "components/canvas";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-test
-    </div>
-  )
+  return <Canvas />;
 }
