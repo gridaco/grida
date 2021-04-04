4e41887aaa28405b5390c330ec6121ade22442e3
GwonHeeJun <developer_gwon@naver.com>
2021-04-05 02:32:05 +0900

    genearete uuid & canvas mouse handers

--- web/components/canvas/index.tsx
+++ web/components/canvas/index.tsx
@@ -1,24 +1,80 @@
-import React, { CSSProperties, useCallback, useEffect, useRef } from "react";
-import { Stage } from "../../../packages/nothing/packages/skia-backend";
+import React, {
+  CSSProperties,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import {
+  CkSurface,
+  Stage,
+} from "../../../packages/nothing/packages/skia-backend";
 import styled from "@emotion/styled";
 import { useRecoilValue } from "recoil";
 import { currentInsetLayer } from "state/demo";
+import { toSkPaint } from "../../../packages/nothing/packages/skia-backend/src/skia-element-mapping";
 interface CanvasStyledProps {
   cursor?: CSSProperties["cursor"];
   left?: CSSProperties["left"];
 }
 
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); 
+
+  return windowSize;
+}
+
+function createRect(initialPoint, finalPoint) {
+  return {
+    width: Math.abs(finalPoint.x - initialPoint.x),
+    height: Math.abs(finalPoint.y - initialPoint.y),
+    x: Math.min(finalPoint.x, initialPoint.x),
+    y: Math.min(finalPoint.y, initialPoint.y),
+  };
+}
+
+function getXYLocation(event: MouseEvent) {
+  return { x: Math.round(event.offsetX), y: Math.round(event.offsetY) };
+}
+
 function SkiaComposition() {
   return (
-    <cg-canvas>
-      <cg-rect fTop={0} fRight={0} fBottom={50} fLeft={50} paint={{ color: "#fff" }} />
-    </cg-canvas>
+    <cg-rect
+      fTop={0}
+      fRight={0}
+      fBottom={50}
+      fLeft={50}
+      paint={{ color: "#fff" }}
+    />
   );
 }
 
+let startLocation = {};
+
 function Canvas() {
   const wrapperRef = useRef(undefined);
+
+  const [cursorRect, setCursorRect] = useState(null);
+  const [isClicked, setIsClicked] = useState(false);
   const currentLayer = useRecoilValue(currentInsetLayer);
+  const { width, height } = useWindowSize();
 
   const cursorType = useCallback((): CSSProperties["cursor"] => {
     switch (currentLayer) {
@@ -30,26 +86,24 @@ function Canvas() {
     }
   }, [currentLayer]);
 
-  const mouseUp = useCallback(
-    (e) => {
-      wrapperRef.current?.releasePointerCapture(e.pointerId);
-    },
-    [currentLayer]
-  );
+  const mouseUp = (e: React.PointerEvent) => {
+    const XYLocation = getXYLocation(e.nativeEvent);
+    setIsClicked(false);
+  };
 
-  const mouseDown = useCallback(
-    (e) => {
-      wrapperRef.current?.releasePointerCapture(e.pointerId);
-    },
-    [currentLayer]
-  );
+  const mouseDown = (e) => {
+    const XYLocation = getXYLocation(e.nativeEvent);
+    console.log("D", XYLocation);
+    startLocation = XYLocation;
+    setIsClicked(true);
+  };
 
-  const mouseMove = useCallback(
-    (e) => {
-      wrapperRef.current?.releasePointerCapture(e.pointerId);
-    },
-    [currentLayer]
-  );
+  const mouseMove = (e) => {
+    const XYLocation = getXYLocation(e.nativeEvent);
+    
+    wrapperRef.current?.setPointerCapture(e.pointerId);
+    isClicked && setCursorRect(createRect(startLocation, XYLocation));
+  };
 
   return (
     <Wrapper
@@ -59,8 +113,20 @@ function Canvas() {
       onPointerMove={mouseMove}
       onPointerUp={mouseUp}
     >
-      <SkiaCanvas width={1000} height={100}>
-        <SkiaComposition />
+      <SkiaCanvas width={width} height={height - 55}>
+        {/* <cg-canvas>
+          <SkiaComposition />
+        </cg-canvas> */}
+        <cg-canvas>
+          {cursorRect && (
+            <cg-rect
+              fBottom={cursorRect.width + cursorRect.y}
+              fTop={cursorRect.y}
+              fLeft={cursorRect.x}
+              fRight={cursorRect.height + cursorRect.x}
+            />
+          )}
+        </cg-canvas>
       </SkiaCanvas>
     </Wrapper>
   );
