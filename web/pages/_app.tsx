f8e4bb691450f0a51c59dbf962302e397324cf7a
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-09 18:29:57 +0900

    wip

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,79 +1,30 @@
-import Head from "next/head";
 import React from "react";
-import styled from "@emotion/styled";
-import { Menubar, Header } from "layouts";
-import { css, Global } from "@emotion/react";
+import { Suspense, StrictMode } from "react";
+import App from "components/app";
 import { RecoilRoot } from "recoil";
 
-function MyApp({ Component, pageProps }) {
+// enable SPA mode, supports react.Suspense; if you don't want to use Suspense, you can use NextJS' dynamic import instead. - on SSR mode
+// though, this app does not benefit from SSR.
+function SafeHydrate({ children }) {
   return (
-    <RecoilRoot>
-      <Global
-        styles={css`
-          body {
-            margin: 0px !important;
-            background-color: #212121;
-            font-family: "Roboto", sans-serif;
-          }
-        `}
-      />
-      <SeoMeta />
-      <Wrapper>
-        <Header />
-        <Content>
-          <Menubar />
-          <Component {...pageProps} />
-          <Right_PropertyList />
-        </Content>
-      </Wrapper>
-    </RecoilRoot>
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
   );
 }
 
-export default MyApp;
-
-const SeoMeta = () => {
+function BridgedRootWebApp() {
   return (
-    <Head>
-      <title>Bridged Studio</title>
-      <meta
-        name="description"
-        content="designs that are meant to be implemented. automate your frontend development process. no more boring."
-      />
-      <meta
-        name="keywords"
-        content="flutter, design to code, figma to code, flutter code generation, design handoff, design linting, code generation"
-      />
-      <meta
-        name="author"
-        content="bridged.xyz team and community collaborators"
-      />
-      <link rel="icon" href="/favicon.ico" />
-      {/* TEMPORARY FONT, ROBOTO */}
-      <link
-        href="https://fonts.googleapis.com/css?family=Roboto:400,100,300,100italic,300italic,400italic,500italic,500,700,700italic,900,900italic"
-        rel="stylesheet"
-        type="text/css"
-      />
-    </Head>
+    <SafeHydrate>
+      <StrictMode>
+        <Suspense fallback="Loading...">
+          <RecoilRoot>
+            <App />
+          </RecoilRoot>
+        </Suspense>
+      </StrictMode>
+    </SafeHydrate>
   );
-};
+}
 
-const Wrapper = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
-
-const Content = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  height: calc(100% - 55px);
-`;
-
-const Right_PropertyList = styled.div`
-  max-width: 250px;
-  width: 100%;
-  height: 100%;
-  background-color: #121212;
-`;
+export default BridgedRootWebApp;
