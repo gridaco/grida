d9418ad94e4b3312967b20692cfee0fb95417c45
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-01 08:20:41 +0900

    update with flutter builder

--- editor/next.config.js
+++ editor/next.config.js
@@ -1,7 +1,9 @@
 const withTM = require("next-transpile-modules")([
   "@designto.codes/core",
+  "@bridged.xyz/flutter-builder",
   "@bridged.xyz/design-sdk",
   "@reflect-ui/core",
+  "@reflect-ui/detection",
 ]);
 
 module.exports = withTM();
--- editor/pages/_development/figma.tsx
+++ editor/pages/_development/figma.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { figmacomp } from "../../components";
+import Editor from "@monaco-editor/react";
 
 export default function FigmaDeveloperPage() {
   return (
     <>
       <figmacomp.FigmaScreenImporter />
+      <Editor
+        width={500}
+        height={800}
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        onMount={(d) => {
+          console.log("d", d);
+        }}
+      />
     </>
   );
 }
