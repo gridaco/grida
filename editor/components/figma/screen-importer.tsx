3633f089a4fa679957903637ec4da8dc04a23be5
softmarshmallow <woojoo@softmarshmallow.com>
2021-04-30 19:47:36 +0900

    wip - figma api remote

--- editor/components/figma/screen-importer.tsx
+++ editor/components/figma/screen-importer.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import * as FigmaApi from "figma-js";
 import { TextField } from "@material-ui/core";
 import { utils_figma } from "../../utils";
+// import { convert } from "@bridged.xyz/design-sdk";
 
 async function fetchDemo() {
+  const _nid = utils_figma.FIGMA_BRIDGED_DEMO_APP_ENTRY_NODE_ID;
   const client = FigmaApi.Client({
     personalAccessToken: utils_figma.figmaPersonalAccessToken(),
   });
@@ -11,20 +13,25 @@ async function fetchDemo() {
   const nodesRes = await client.fileNodes(
     utils_figma.FIGMA_BRIDGED_DEMO_APP_FILE_ID,
     {
-      ids: [utils_figma.FIGMA_BRIDGED_DEMO_APP_ENTRY_NODE_ID],
+      ids: [_nid],
     }
   );
 
+  console.log("nodesRes.data", nodesRes.data);
+
   const nodes = nodesRes.data.nodes;
+  console.log("nodes", nodes);
 
-  const demoEntryNode = nodes[0];
+  const demoEntryNode = nodes[_nid];
 
-  return demoEntryNode;
+  return demoEntryNode.document;
 }
 
 export function FigmaScreenImporter() {
   fetchDemo().then((d) => {
     console.log(d);
+    // const cvted = convert.intoReflectNode(d as any);
+    // console.log("cvted", cvted);
   });
   return <></>;
 }
