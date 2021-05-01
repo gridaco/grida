130b6b5a6c40d348641bdd136eb48dcd7d38ca9e
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-01 14:26:34 +0900

    cleanup before orging

--- editor/pages/_development/figma.tsx
+++ editor/pages/_development/figma.tsx
@@ -1,18 +1,36 @@
 import React from "react";
 import { figmacomp } from "../../components";
-import Editor from "@monaco-editor/react";
+
+import dynamic from "next/dynamic";
+
+// const MonacoEditor = dynamic(import("@monaco-editor/react"), { ssr: false });
+import MonacoEditor from "@monaco-editor/react";
+
+// const DynamicComponentWithNoSSR = dynamic(
+//   () => import("@monaco-editor/react"),
+//   { ssr: false }
+// );
 
 export default function FigmaDeveloperPage() {
   return (
     <>
       <figmacomp.FigmaScreenImporter />
-      <Editor
+      {/* <DynamicComponentWithNoSSR
         width={500}
         height={800}
         defaultLanguage="javascript"
         defaultValue="// some comment"
         onMount={(d) => {
           console.log("d", d);
+        }}
+      /> */}
+      <MonacoEditor
+        language="dart"
+        theme="vs-dark"
+        value={"//source"}
+        options={{ unusualLineTerminators: "off" }}
+        onChange={(value: string) => {
+          // editingSource = value;
         }}
       />
     </>
