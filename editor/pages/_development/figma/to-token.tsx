77c69ad19cd2365c76055bdf58de4395aecf52b6
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-05 20:52:19 +0900

    add tokenize

--- editor/pages/_development/figma/to-token.tsx
+++ editor/pages/_development/figma/to-token.tsx
@@ -1,4 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { figmacomp, canvas } from "../../../components";
+import { ReflectSceneNode } from "@design-sdk/core/nodes";
+import JSONTree from "react-json-tree";
+import { tokenize } from "@designto/token";
+
 export default function FigmaToReflectWidgetTokenPage() {
-  return <></>;
+  const [reflect, setReflect] = useState<ReflectSceneNode>();
+  const [figmaNodeUrl, setFigmaNodeUrl] = useState<string>();
+
+  const handleOnDesignImported = (reflect: ReflectSceneNode) => {
+    setReflect(reflect);
+  };
+
+  const handleFigmaUrlEnter = (url: string) => {
+    setFigmaNodeUrl(url);
+  };
+
+  let tokenTree;
+  if (reflect) {
+    tokenTree = tokenize(reflect);
+  }
+
+  return (
+    <>
+      <canvas.FigmaEmbedCanvas url={figmaNodeUrl} />
+      <figmacomp.FigmaScreenImporter
+        onImported={handleOnDesignImported}
+        onUrlEnter={handleFigmaUrlEnter}
+      />
+      <JSONTree hideRoot data={tokenTree} />
+    </>
+  );
 }
