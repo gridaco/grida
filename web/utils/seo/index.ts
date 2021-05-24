9b4f8aa88f5d918abc953e36f803bf628fa6d422
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-25 03:20:18 +0900

    update coresponding seo data

--- web/utils/seo/index.ts
+++ web/utils/seo/index.ts
@@ -6,7 +6,7 @@ const OG_IMAGE_GIF =
   "https://bridged-service-static.s3-us-west-1.amazonaws.com/opengraph/og-image-gif.gif";
 
 export const SEO_OG_DEFAULTS: OpenGraphMeta = {
-  title: "Designs, come to live | Bridged",
+  title: "Figma to Flutter | Bridged",
   type: "website",
   url: "https://bridged.xyz",
   image: OG_IMAGE_GIF,
@@ -16,11 +16,14 @@ export const SEO_DEFAULTS: PageSeoMeta = {
   route: "/",
   title: "Bridged",
   description:
-    "Designs, come to live | automate your frontend development. Instantly convert your design to production ready code.",
+    "Figma to Flutter | automate your frontend development. Instantly convert your design to production ready code.",
   keywords: [
     "figma to flutter",
     "design to code",
     "figma to code",
+    "design nocode",
+    "flutter studio",
+    "flutter tools",
     "flutter code generation",
     "design handoff",
     "design linting",
