22245b1fb4555d1029c85ab1a93a1c232daf2f44
softmarshmallow <woojoo@softmarshmallow.com>
2021-05-31 01:48:40 +0900

    add boring embed ui dependency

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { HomeScaffold } from "../layouts/home-scaffold";
 import { ContentCard } from "../cards";
+import Embed from "@boring-ui/embed";
+
 export function Home() {
   return (
     <HomeScaffold navigation={<></>}>
       <>
         <ContentCard title="aha" description="hihi" />
+        <Embed url="https://www.youtube.com/watch?v=RIZjZFoDhRc" />
       </>
     </HomeScaffold>
   );
