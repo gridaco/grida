5725cb95dda81059fb2ba9b97bdd3dced3587333
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-05 21:27:28 +0900

    update layout

---
+++ editor/pages/_playground/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function () {
+  return (
+    <>
+      Playground. make your playground screen under this directory. it won't be
+      included in GIT. (name it *.play.tsx)
+    </>
+  );
+}
