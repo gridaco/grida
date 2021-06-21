068cb11a12711f85dc5a3187f93a379e4aa39d33
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-22 06:37:41 +0900

    update design-sdk

--- app/blocks/screen-preview-card-block/provider-figma-embed.tsx
+++ app/blocks/screen-preview-card-block/provider-figma-embed.tsx
@@ -1,5 +1,17 @@
 import React from "react";
-// import "@design-sdk/"
-export function ProviderFigmaEmbed() {
-  return <></>;
+import { url as urlutil } from "@design-sdk/figma";
+
+interface ProviderFigmaEmbedProps {
+  url: string;
+}
+export function ProviderFigmaEmbed(props: ProviderFigmaEmbedProps) {
+  const url = urlutil.buildFigmaEmbedUrl({
+    url: props.url,
+  });
+
+  return (
+    <>
+      <iframe src={url} />
+    </>
+  );
 }
