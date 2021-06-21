49af42e2b4e4b6d3163d08d4b173ae94e7fa5e0b
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-22 07:51:24 +0900

    update add page button style

--- app/components/add-page-button/add-page-button.tsx
+++ app/components/add-page-button/add-page-button.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback } from "react";
 import styled from "@emotion/styled";
 import { useDispatch } from "@core/app-state";
+import { PlusIcon } from "@radix-ui/react-icons";
+import { Spacer } from "@editor-ui/spacer";
 
 export function AddPageButton() {
   const dispatch = useDispatch();
@@ -8,14 +10,39 @@ export function AddPageButton() {
   const handleAddPage = useCallback(() => {
     const name = prompt("New page Name");
 
-    if (name !== null) dispatch("addPage", name);
+    if (name !== null)
+      dispatch({
+        type: "add-page",
+        name,
+      });
   }, [dispatch]);
 
-  return <Button onClick={handleAddPage}>+ Add Page</Button>;
+  return (
+    <Button onClick={handleAddPage}>
+      <PlusIcon />
+      <Spacer.Horizontal size={8} />
+      Add Page
+    </Button>
+  );
 }
 
 const Button = styled.button`
   width: 100%;
   border: none;
+  padding: 12px;
   background: none;
+  color: grey;
+
+  :hover {
+    background: #e1e1e1;
+  }
+
+  :active {
+    background: #d0d0d0;
+  }
+
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  justify-content: center;
 `;
