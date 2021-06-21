a14723b8c06a85557b15da58d80cc77952bbee16
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-22 06:35:05 +0900

    update figma embed url builder

--- editor/components/canvas/figma-embed-canvas.tsx
+++ editor/components/canvas/figma-embed-canvas.tsx
@@ -1,47 +1,16 @@
 import React from "react";
-
-type EmbedInput = { url: string } | { fileid: string; nodeid?: string };
+import { url as urlutil } from "@design-sdk/figma";
 export function FigmaEmbedCanvas(props: {
-  src: EmbedInput;
+  src: urlutil.FigmaEmbedInput;
   width?: string | number;
   height?: string | number;
 }) {
-  const url = builEmbedabledUrl(props.src);
+  const url = urlutil.buildFigmaEmbedUrl(props.src);
 
   if (url) {
-    /**
-     * build embedding url. - https://www.figma.com/developers/embed
-     */
-    const _embed_url = `https://www.figma.com/embed?embed_host=astra&url=${url}`;
     return (
-      <iframe
-        width={props.width ?? 375}
-        height={props.height}
-        src={_embed_url}
-      />
+      <iframe width={props.width ?? 375} height={props.height} src={url} />
     );
   }
   return <>NO FIGMA URL PROVIDED</>;
-}
-
-/**
- * e.g. - https://www.figma.com/file/HSozKEVWhh8saZa2vr1Nxd?node-id=111%3A0
- * @param src : ;
- * @returns
- */
-function builEmbedabledUrl(src?: EmbedInput): string | undefined {
-  if (!src) {
-    return;
-  }
-
-  if ("url" in src) {
-    return src.url;
-  } else if ("fileid" in src) {
-    /// WWW prefix is required. if non passed, figma embed won't accept it.
-    return `https://www.figma.com/file/${src.fileid}/${
-      src.nodeid && `?node-id=${src.nodeid}`
-    }`;
-  } else {
-    return undefined;
-  }
 }
