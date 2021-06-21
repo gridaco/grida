b7f1308a7bb8e98329c288fbbae5f85009f5478b
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-21 16:27:12 +0900

    sync-all

---
+++ app/built-in-pages/getting-started-components/import-design-with-url.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+export function ImportDesignWithUrl() {
+  return (
+    <_RootWrapper>
+      <input></input>
+      <button>Import</button>
+    </_RootWrapper>
+  );
+}
+
+const _RootWrapper = styled.div``;
---
+++ app/built-in-pages/getting-started/getting-started.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+import { ImportDesignWithUrl } from "../getting-started-components";
+export function BuiltIn_GettingStarted() {
+  return <ImportDesignWithUrl />;
+}
