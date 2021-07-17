a9029a3e060c5ccfa350d42d3683e4f2273ec950
softmarshmallow <woojoo@softmarshmallow.com>
2021-07-17 20:00:46 +0900

    update getting-started page with image support

--- app/built-in-pages/getting-started/getting-started.tsx
+++ app/built-in-pages/getting-started/getting-started.tsx
@@ -9,7 +9,9 @@ export const getting_started_document = new BoringDocument({
 <br/>
 <br/>
 <h1>Too much? - Quick start (30 Seconds)</h1>
-<p>Bridged App is an opensource software for Ultimate productivity for startups & application creation. With built-in powerful text editor engine and graphics engine based on Skia 2D Graphics library, we provide the most effecient way to design, to develop the app and to manage the contents Live.</p>
-<import-design-with-url/>
+<p>Grida is an open source software for Ultimate productivity for startups & application creation. With built-in powerful text editor engine and graphics engine based on Skia 2D Graphics library, we provide the most effecient way to design, to develop the app and to manage the contents Live.</p>
+<import-design-with-url></import-design-with-url>
+<br/>
+<img src="https://grida-app-builtin-pages.s3.us-west-1.amazonaws.com/getting-started/resources/how-to-copy-link-from-figma.png" />
   `,
 });
