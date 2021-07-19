54f2856d50e93ae5d3dcb6e6ee40ca1ca9ab93c0
softmarshmallow <woojoo@softmarshmallow.com>
2021-07-19 19:08:26 +0900

    add @design-sdk/figma-auth-store & nonsecure 3rd party auth store

---
+++ app/3rd-party-api/figma/token-store.ts
@@ -0,0 +1,12 @@
+import { oauth, FigmaAuthStore } from "@design-sdk/figma-auth-store"
+
+// FigmaAuthStore
+const prefix = '3rd-party-api/figma/'
+
+export function getToken(){
+
+}
+
+export function setOauthToken(){
+
+}
