ba8b48e5aacff16b54da38ec1dd1ce929007d527
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-07 06:12:20 +0900

    update boringdoc service

--- services/boring-service/src/boringdoc/boringdoc.service.ts
+++ services/boring-service/src/boringdoc/boringdoc.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../_prisma/prisma.service';
 
 @Injectable()
-export class BoringdocService {}
+export class BoringdocService {
+  constructor(private readonly prisma: PrismaService) {}
+  async findAll(p: { workspace: string }): Promise<any> {
+    //
+  }
+
+  async get(id: string) {
+    return await this.prisma.document.findUnique({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  async create(p: { title: string; content: string }) {
+    return await this.prisma.document.create({
+      data: {
+        title: p.title,
+        content: p.content,
+      },
+    });
+  }
+}
