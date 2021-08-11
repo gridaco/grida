2afc35c736f18250258b3377604aa4a5883bed8a
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-11 17:12:08 +0900

    prettify monaco editor

--- packages/app-blocks/scaffold-code-block/scaffold-code-block.tsx
+++ packages/app-blocks/scaffold-code-block/scaffold-code-block.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
 import { format } from "@base-sdk/functions-code-format";
 import Editor, { useMonaco } from "@monaco-editor/react";
-
 export interface ScaffoldCodeBlockProps {
   source: string;
   lang: "html" | "ts" | "js" | "dart";
@@ -59,36 +59,76 @@ export function ScaffoldCodeBlock(props: ScaffoldCodeBlockProps) {
     }
   }, [props.source]);
 
+  const TEXT_MARGIN_LINES = 2;
+  const addmargintoTextContent = (text: string): string => {
+    let textwithmargin = text;
+    const margin = "\n".repeat(TEXT_MARGIN_LINES);
+    if (!text.startsWith("\n")) {
+      textwithmargin = margin + textwithmargin;
+    }
+
+    if (!text.endsWith("\n")) {
+      textwithmargin = textwithmargin + margin;
+    }
+    return textwithmargin;
+  };
+
   const monacolang = monacolangmap[props.lang];
+
+  function marginAppliedLine(line: number): string {
+    const originline = line - TEXT_MARGIN_LINES;
+    if (originline <= 0) {
+      return "";
+    } else {
+      return originline.toString();
+    }
+  }
+
   return (
-    <>
+    <RootWrap>
       <Editor
         key={source}
         height="90vh"
-        // theme="vs-dark"
+        theme="vs-dark"
+        line={TEXT_MARGIN_LINES}
         options={{
+          fontFamily: `Menlo, Monaco, 'Courier New', monospace`,
+          fontSize: 14,
+          minimap: {
+            // disable minimap a.k.a preview
+            enabled: false,
+          },
+          renderIndentGuides: true, // need color customization
           scrollbar: {
+            // allow parent scoll
             alwaysConsumeMouseWheel: false,
+            useShadows: false,
+            verticalScrollbarSize: 8,
+            horizontalScrollbarSize: 8,
           },
           showFoldingControls: "mouseover",
-          lineNumbers: "off",
+          lineNumbers: "off", //marginAppliedLine,
+          lineDecorationsWidth: "12px",
           glyphMargin: true,
           scrollBeyondLastLine: false,
           readOnly: true,
           renderFinalNewline: true,
-          lineDecorationsWidth: "12px",
         }}
         defaultLanguage={monacolang}
-        defaultValue={source}
+        defaultValue={addmargintoTextContent(source)}
       />
       {/* <pre>
         <code>{source}</code>
       </pre> */}
       <FormatButton onClick={onformat} />
-    </>
+    </RootWrap>
   );
 }
 
 function FormatButton(props: { onClick: () => void }) {
   return <button onClick={props.onClick}>format</button>;
 }
+
+const RootWrap = styled.div`
+  margin-top: 12px;
+`;
