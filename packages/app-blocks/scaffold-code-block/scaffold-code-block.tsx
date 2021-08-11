cc821a468483543b7a0a5b3eaaef1fee4987fd2c
softmarshmallow <woojoo@softmarshmallow.com>
2021-08-11 16:26:10 +0900

    add monaco editor usage for default scaffold code view

--- packages/app-blocks/scaffold-code-block/scaffold-code-block.tsx
+++ packages/app-blocks/scaffold-code-block/scaffold-code-block.tsx
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { format } from "@base-sdk/functions-code-format";
+import Editor, { useMonaco } from "@monaco-editor/react";
 
 export interface ScaffoldCodeBlockProps {
   source: string;
   lang: "html" | "ts" | "js" | "dart";
 }
 
+const monacolangmap = {
+  html: "html",
+  ts: "typescript",
+  js: "javascript",
+  dart: "dart",
+};
+
 export function ScaffoldCodeBlock(props: ScaffoldCodeBlockProps) {
-  console.log("props", props);
+  const monaco = useMonaco();
   const [source, setSource] = useState(props.source);
   const onformat = () => {
     formatthis();
@@ -28,14 +36,54 @@ export function ScaffoldCodeBlock(props: ScaffoldCodeBlockProps) {
 
   useEffect(() => {
     formatthis();
+
+    if (monaco) {
+      monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+        target: monaco.languages.typescript.ScriptTarget.Latest,
+        allowNonTsExtensions: true,
+        moduleResolution:
+          monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+        module: monaco.languages.typescript.ModuleKind.CommonJS,
+        noEmit: true,
+        esModuleInterop: true,
+        jsx: monaco.languages.typescript.JsxEmit.React,
+        reactNamespace: "React",
+        allowJs: true,
+        typeRoots: ["node_modules/@types"],
+      });
+
+      monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+        noSemanticValidation: false,
+        noSyntaxValidation: false,
+      });
+    }
   }, [props.source]);
 
+  const monacolang = monacolangmap[props.lang];
   return (
     <>
-      ::TODO::
-      <pre>
+      <Editor
+        key={source}
+        height="90vh"
+        // theme="vs-dark"
+        options={{
+          scrollbar: {
+            alwaysConsumeMouseWheel: false,
+          },
+          showFoldingControls: "mouseover",
+          lineNumbers: "off",
+          glyphMargin: true,
+          scrollBeyondLastLine: false,
+          readOnly: true,
+          renderFinalNewline: true,
+          lineDecorationsWidth: "12px",
+        }}
+        defaultLanguage={monacolang}
+        defaultValue={source}
+      />
+      {/* <pre>
         <code>{source}</code>
-      </pre>
+      </pre> */}
       <FormatButton onClick={onformat} />
     </>
   );
