d33a072f7a932978a079e44e4cddf1d00c293095
You-j <ijyk3061@gmail.com>
2021-08-26 11:30:57 +0900

    fix typo: global style

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -23,7 +23,7 @@ function CssNormalized() {
         body {
           margin: 0px;
           padding: 0;
-          font-family: "Helvetica Nueue" "Roboto", sans-serif;
+          font-family: "Helvetica Nueue", "Roboto", sans-serif;
         }
         iframe {
           border: none;
@@ -36,13 +36,13 @@ function CssNormalized() {
 function HeadInjection() {
   return (
     <Head>
-      <CssNormalized />
-      <SeoMeta />
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&"
         rel="stylesheet"
         type="text/css"
       />
+      <CssNormalized />
+      <SeoMeta />
     </Head>
   );
 }
