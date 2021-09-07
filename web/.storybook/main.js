9ea61e8c1780bb155ee32fec0e62e300318348b4
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-07 17:31:12 +0900

    init storybook with minimal example

---
+++ web/.storybook/main.js
@@ -0,0 +1,13 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+module.exports = {
+  stories: [
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
+    return config;
+  },
+};
