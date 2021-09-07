e05854a0d6757203c38d815a33c6511bca3076cd
You-j <ijyk3061@gmail.com>
2021-09-07 15:39:24 +0900

    add code theme

--- web/components/code/index.tsx
+++ web/components/code/index.tsx
@@ -1,14 +1,31 @@
-import Highlight, { defaultProps } from "prism-react-renderer";
+import {
+  default as PrismHighlight,
+  defaultProps,
+  Language,
+  Prism,
+} from "prism-react-renderer";
+import vsdark from "prism-react-renderer/themes/vsDark";
 
 export default function CodeBlock({ children, className }) {
   const language = className.replace(/language-/, "");
 
   return (
-    <Highlight {...defaultProps} code={children} language={language}>
+    <PrismHighlight
+      {...defaultProps}
+      theme={vsdark}
+      Prism={Prism}
+      code={children}
+      language={language}
+    >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre
           className={className + " font-firacode"}
-          style={{ ...style, padding: "20px", borderRadius: "5px", whiteSpace: "break-spaces" }}
+          style={{
+            ...style,
+            padding: "20px",
+            borderRadius: "5px",
+            whiteSpace: "break-spaces",
+          }}
         >
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
@@ -19,6 +36,6 @@ export default function CodeBlock({ children, className }) {
           ))}
         </pre>
       )}
-    </Highlight>
+    </PrismHighlight>
   );
 }
