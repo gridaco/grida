a977666b3f1328c496879aed59c19aa14d1e8894
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-15 13:09:56 +0900

    use use-design from token inspector

--- editor/pages/figma/to-token.tsx
+++ editor/pages/figma/to-token.tsx
@@ -1,80 +1,38 @@
 import React, { useEffect, useState } from "react";
-import { figmacomp, canvas } from "../../components";
-import { ReflectSceneNode } from "@design-sdk/core/nodes";
+import { canvas } from "../../components";
 import { tokenize } from "@designto/token";
 import {
   JsonTree,
   WidgetTree,
 } from "../../components/visualization/json-visualization/json-tree";
-import {
-  FigmaTargetNodeConfig,
-  parseFileAndNodeId,
-} from "@design-sdk/figma-url";
-import { useRouter } from "next/router";
-import { extractFromFigmaQueryParams } from "../../query/from-figma";
-import { Figma } from "@design-sdk/figma";
-import { fetch } from "@design-sdk/figma-remote";
-import { personal } from "@design-sdk/figma-auth-store";
 import { DefaultEditorWorkspaceLayout } from "../../layout/default-editor-workspace-layout";
 import { LayerHierarchy } from "../../components/editor-hierarchy";
 import { WorkspaceContentPanelGridLayout } from "../../layout/panel/workspace-content-panel-grid-layout";
 import { WorkspaceContentPanel } from "../../layout/panel";
 import { WorkspaceBottomPanelDockLayout } from "../../layout/panel/workspace-bottom-panel-dock-layout";
+import { useDesign } from "../../query/to-code";
 
 export default function FigmaToReflectWidgetTokenPage() {
-  const [figmaNode, setFigmaNode] = useState<Figma.SceneNode>();
-  const [reflect, setReflect] = useState<ReflectSceneNode>();
-  const [figmaNodeUrl, setFigmaNodeUrl] = useState<string>();
-  const [
-    targetnodeConfig,
-    setTargetnodeConfig,
-  ] = useState<FigmaTargetNodeConfig>();
+  const design = useDesign();
 
-  const router = useRouter();
-
-  useEffect(() => {
-    const params = extractFromFigmaQueryParams(router);
-    if (params.figma_target_url) {
-      setFigmaNodeUrl(params.figma_target_url);
-      const targetnodeconfig = parseFileAndNodeId(params.figma_target_url);
-      setTargetnodeConfig(targetnodeconfig);
-      fetch
-        .fetchTargetAsReflect(targetnodeconfig.file, targetnodeconfig.node, {
-          personalAccessToken: personal.get_safe(),
-        })
-        .then((res) => {
-          setReflect(res.reflect);
-          setFigmaNode(res.figma);
-        });
-    }
-  }, [router]);
-
-  const handleOnDesignImported = (reflect: ReflectSceneNode) => {
-    setReflect(reflect);
-  };
-
-  const handleFigmaUrlEnter = (target: FigmaTargetNodeConfig) => {
-    setFigmaNodeUrl(target.url);
-  };
+  if (!design) {
+    return <>Loading..</>;
+  }
 
   let tokenTree;
-  if (reflect) {
-    tokenTree = tokenize(reflect);
+  if (design.reflect) {
+    tokenTree = tokenize(design.reflect);
   }
 
   return (
     <>
-      {!figmaNodeUrl && (
-        <figmacomp.FigmaScreenImporter
-          onImported={handleOnDesignImported}
-          onTargetEnter={handleFigmaUrlEnter}
-        />
-      )}
-      <DefaultEditorWorkspaceLayout leftbar={<LayerHierarchy data={reflect} />}>
+      <DefaultEditorWorkspaceLayout
+        leftbar={<LayerHierarchy data={design.reflect} />}
+      >
         <WorkspaceContentPanelGridLayout>
           <WorkspaceContentPanel>
             <canvas.FigmaEmbedCanvas
-              src={{ url: figmaNodeUrl }}
+              src={{ url: design.url }}
               width="100%"
               height="100%"
             />
@@ -90,10 +48,10 @@ export default function FigmaToReflectWidgetTokenPage() {
                 }}
               >
                 <div style={{ flex: 1 }}>
-                  <WidgetTree data={figmaNode} />
+                  <WidgetTree data={design.figma} />
                 </div>
                 <div style={{ flex: 1 }}>
-                  <WidgetTree data={reflect} />
+                  <WidgetTree data={design.reflect} />
                 </div>
                 <div style={{ flex: 1 }}>
                   <JsonTree hideRoot data={tokenTree} />
