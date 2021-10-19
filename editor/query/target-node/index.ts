c7dadfcb2e87ffabdf4ad1e7f14e8d0716cd5b67
softmarshmallow <woojoo@softmarshmallow.com>
2021-10-19 16:57:23 +0900

    add raw response type def

--- editor/query/target-node/index.ts
+++ editor/query/target-node/index.ts
@@ -1,4 +1,5 @@
 import { nodes } from "@design-sdk/core";
+import { RawNodeResponse } from "@design-sdk/figma-remote";
 
 export interface TargetNodeConfig<R = any, P = any> {
   remote: R;
@@ -7,5 +8,5 @@ export interface TargetNodeConfig<R = any, P = any> {
   url: string;
   file: string;
   node: string;
-  raw: any;
+  raw: RawNodeResponse;
 }
