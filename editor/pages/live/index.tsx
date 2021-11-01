0ad2763664397c79a264c1caebc3ea0de41cb892
UZU, J <woojoo@softmarshmallow.com>
2021-11-02 01:04:45 +0900

    add server auth

--- editor/pages/live/index.tsx
+++ editor/pages/live/index.tsx
@@ -1,28 +1,30 @@
 import React from "react";
 import Pusher from "pusher-js";
 import { useState } from "react";
+import { useEffect } from "react";
 
-const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {});
+const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+  // 'live-session-from-assistant'
+  cluster: "us3",
+  authEndpoint: "https://assistant-live-session.grida.cc/pusher/auth",
+});
 
 export default function LiveSessionPage() {
-  const [channel, setChannel] = useState<string>();
+  // const [channel, setChannel] = useState<string>();
   const [lastmessage, setLastmessage] = useState<string>();
 
-  const connect = () => {
-    pusher.channel(channel);
-  };
+  useEffect(() => {
+    // TODO: - add auth guard
+
+    // subscribe once wheb the page is loaded
+    const subscription = pusher.subscribe("private-live-session"); // channel
+    subscription.bind("client-select", (d) => {
+      setLastmessage(JSON.stringify(d));
+    });
+  }, []);
 
   return (
     <div style={{ margin: 24 }}>
-      <input
-        placeholder="channel"
-        onChange={(e) => {
-          setChannel(e.target.value);
-        }}
-      />
-      <button disabled={!!!channel} onClick={connect}>
-        connect
-      </button>
       {lastmessage ? (
         <>
           <p>{lastmessage}</p>
