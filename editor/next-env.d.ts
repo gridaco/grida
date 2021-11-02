a0cb1c7887404bb7ccefcc86eb36f1d294f93d2b
softmarshmallow <woojoo@softmarshmallow.com>
2021-11-03 00:41:29 +0900

    nextjs12

--- editor/next-env.d.ts
+++ editor/next-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-declare module "*.txt" {
-  const content: string;
-  export default content;
-}
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
