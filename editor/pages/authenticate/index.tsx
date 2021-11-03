13a67b6f0103e686a0a72337ae551e96eebf89d0
softmarshmallow <woojoo@softmarshmallow.com>
2021-11-03 13:50:25 +0900

    wip auth page

---
+++ editor/pages/authenticate/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useAuthState } from "@base-sdk-fp/auth-components-react";
+import { useRouter } from "next/router";
+
+export default function AuthenticationPage() {
+  const router = useRouter();
+  const authstate = useAuthState();
+  switch (authstate) {
+    case "loading": {
+      return (
+        <>
+          <h6>Loading...</h6>
+        </>
+      );
+    }
+    case "signedin": {
+      router.replace("/");
+      break;
+    }
+    case "expired":
+    case "unauthorized": {
+      return (
+        <>
+          <button
+            onClick={() => {
+              router.replace(
+                "https://accounts.grida.co/signin?redirect_uri=" +
+                  window.location.href
+              );
+            }}
+          >
+            sign in
+          </button>
+        </>
+      );
+    }
+  }
+}
