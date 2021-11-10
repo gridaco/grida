b20f14e9b611a06d11b40f39e98e2e8a4753e8de
softmarshmallow <woojoo@softmarshmallow.com>
2021-11-10 16:00:07 +0900

    initialized scaling vanilla preview page with `@code-editor/vanilla-preview`

--- editor/pages/embed/vscode/grida-explorer-preview/index.tsx
+++ editor/pages/embed/vscode/grida-explorer-preview/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import VanillaPreview from "@code-editor/vanilla-preview";
 export default function VSCodeEmbedGridaExplorerPreview() {
-  const router = useRouter();
-  const [isEmpty, setIsEmpty] = useState(true);
+  const router = useRouter(); // use router only for loading initial params.
+  const [isEmpty, setIsEmpty] = useState(false); // TODO: set to false when publishing - true on only dev.
 
   useEffect(() => {
     // subscribes to user's message
@@ -20,11 +21,27 @@ export default function VSCodeEmbedGridaExplorerPreview() {
     return <EmptyState />;
   }
 
-  return <div>VSCodeEmbedGridaExplorerPreview</div>;
+  return <PreviewState srcDoc={`<div>Hello</div>`} />;
 }
 
-function EmptyState() {
-  return <div>EmptyState</div>;
+function EmptyState({ message = "Nothing is selected" }: { message?: string }) {
+  return <div>{message}</div>;
+}
+
+function PreviewState({ srcDoc }: { srcDoc: string }) {
+  return (
+    <VanillaPreview
+      type={"responsive"}
+      id={""}
+      data={srcDoc}
+      margin={0}
+      borderRadius={0}
+      origin_size={{
+        width: 375,
+        height: 812,
+      }}
+    />
+  );
 }
 
 interface EventFromClient<T = any> {
