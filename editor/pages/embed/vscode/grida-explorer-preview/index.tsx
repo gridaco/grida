376f4787f42ba531b22cad2baa840ebb2d40be16
softmarshmallow <woojoo@softmarshmallow.com>
2021-11-11 19:15:45 +0900

    qf - missing break line

--- editor/pages/embed/vscode/grida-explorer-preview/index.tsx
+++ editor/pages/embed/vscode/grida-explorer-preview/index.tsx
@@ -34,6 +34,7 @@ export default function VSCodeEmbedGridaExplorerPreview() {
           }
           case "set-loading": {
             setLoading(event.payload.isLoading);
+            break;
           }
         }
       }
