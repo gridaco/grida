88174c80f57d0fa6611426e30a29470e90da16f3
PACMAN <ijyk3061@gmail.com>
2021-11-17 20:00:28 +0900

    stash add section storybook

---
+++ web/sections/landingpage/online-app/storybook-style.ts
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+import LandingpageText from "components/landingpage/text";
+
+export const Description = styled(LandingpageText)`
+  max-width: 520px;
+  margin-top: 20px;
+  z-index: 99;
+`;
+
+export const OnlineTitle = styled(LandingpageText)`
+  display: flex;
+  align-items: center;
+`;
