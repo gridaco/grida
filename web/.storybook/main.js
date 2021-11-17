fc3543090b23cdaec31122cd3e9b851eb9d66fdb
PACMAN <ijyk3061@gmail.com>
2021-11-17 17:46:07 +0900

    setup storybook main

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -4,8 +4,14 @@ module.exports = {
   stories: [
     "../components/**/*.stories.mdx",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../sections/landingpage/**/*.stories.mdx",
+    "../sections/landingpage/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-actions",
+  ],
   webpackFinal: async (config, { configType }) => {
     config.resolve.plugins = [new TsconfigPathsPlugin()];
     return config;
