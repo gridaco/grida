66086d9e84c2a6f4d435174ffb3eaf7f623a9d52
softmarshmallow <woojoo@softmarshmallow.com>
2021-12-05 03:58:20 +0900

    rm unused

--- web/sections/landingpage/cta-see-the-magic/index.tsx
+++ web/sections/landingpage/cta-see-the-magic/index.tsx
@@ -1,32 +1,7 @@
 import React from "react";
 
-import { BreakPoints } from "../_breakpoints";
-import LG from "./lg";
-import MD from "./md";
 import SectionCtaLastSeeTheMagicScaffold from "./scaffold";
-import SM from "./sm";
-import XL from "./xl";
-import XS from "./xs";
 
-const Section__CTASeeTheMagic = () => (
-  <div>
-    {/* <BreakPoints.xl>
-      <XL />
-    </BreakPoints.xl>
-    <BreakPoints.lg>
-      <LG />
-    </BreakPoints.lg>
-    <BreakPoints.md>
-      <MD />
-    </BreakPoints.md>
-    <BreakPoints.sm>
-      <SM />
-    </BreakPoints.sm>
-    <BreakPoints.xs>
-      <XS />
-    </BreakPoints.xs> */}
-    <SectionCtaLastSeeTheMagicScaffold />
-  </div>
-);
+const Section__CTASeeTheMagic = () => <SectionCtaLastSeeTheMagicScaffold />;
 
 export default Section__CTASeeTheMagic;
--- web/sections/landingpage/hero/index.tsx
+++ web/sections/landingpage/hero/index.tsx
@@ -1,32 +1,7 @@
 import React from "react";
 
-import { BreakPoints } from "../_breakpoints";
-import LG from "./lg";
-import MD from "./md";
 import HeroResponsive from "./scaffold";
-import SM from "./sm";
-import XL from "./xl";
-import XS from "./xs";
 
-const Hero = () => (
-  <div>
-    <HeroResponsive />
-    {/* <BreakPoints.xl>
-      <XL />
-    </BreakPoints.xl>
-    <BreakPoints.lg>
-      <LG />
-    </BreakPoints.lg>
-    <BreakPoints.md>
-      <MD />
-    </BreakPoints.md>
-    <BreakPoints.sm>
-      <SM />
-    </BreakPoints.sm>
-    <BreakPoints.xs>
-      <XS />
-    </BreakPoints.xs> */}
-  </div>
-);
+const Hero = () => <HeroResponsive />;
 
 export default Hero;
