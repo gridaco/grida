083493087cb1a9dd4d2f35b2ffb62ecdff30c18e
softmarshmallow <woojoo@softmarshmallow.com>
2021-12-07 02:24:24 +0900

    update public env prefix for nextjs / vercel

--- web/next.config.js
+++ web/next.config.js
@@ -5,17 +5,6 @@ const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 const withTM = require("next-transpile-modules");
 const withVideos = require("next-videos");
 
-const FIREBASE_ENV_VARS = {
-  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-  FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
-  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-  FIREBASE_STORAGE_BUKET: process.env.FIREBASE_STORAGE_BUKET,
-  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-  FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
-};
-
 module.exports = withBundleAnalyzer(
   withVideos(
     withMDX(
@@ -47,9 +36,6 @@ module.exports = withBundleAnalyzer(
         transpileModules: ["lodash-es"],
         images: {
           domains: ["img.youtube.com", "via.placeholder.com"],
-        },
-        env: {
-          ...FIREBASE_ENV_VARS,
         },
         analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
         analyzeBrowser: ["browser", "both"].includes(
