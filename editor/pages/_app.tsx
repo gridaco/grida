bfddd03cebb871584e6c7d935fb127e25a809d13
softmarshmallow <woojoo@softmarshmallow.com>
2021-12-06 19:34:04 +0900

    (editor) add app title for browser

--- editor/pages/_app.tsx
+++ editor/pages/_app.tsx
@@ -38,6 +38,7 @@ function HeadInjection() {
     <Head>
       <GlobalCss />
       <SeoMeta />
+      <title>Grida: Design to Code</title>
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&"
         rel="stylesheet"
