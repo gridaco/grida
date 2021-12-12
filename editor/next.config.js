58db39d5054958881af4b4f3b2188841e2dab6bd
softmarshmallow <woojoo@softmarshmallow.com>
2021-12-13 00:56:57 +0900

    add framer motion & buildconfig update

--- editor/next.config.js
+++ editor/next.config.js
@@ -92,6 +92,11 @@ const withTM = require("next-transpile-modules")([
 
 module.exports = withTM({
   webpack: (config) => {
+    config.module.rules.push({
+      type: "javascript/auto",
+      test: /\.mjs$/,
+      include: /node_modules/,
+    });
     return config;
   },
   async redirects() {
