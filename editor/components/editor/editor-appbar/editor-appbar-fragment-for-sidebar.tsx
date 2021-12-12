2e24d2b5e99ecd298fb4fdbee4e259cf574ffeb4
softmarshmallow <woojoo@softmarshmallow.com>
2021-12-13 04:24:33 +0900

    update editor sidebar appbar

--- editor/components/editor/editor-appbar/editor-appbar-fragment-for-sidebar.tsx
+++ editor/components/editor/editor-appbar/editor-appbar-fragment-for-sidebar.tsx
@@ -4,11 +4,17 @@ import styled from "@emotion/styled";
 export function AppbarFragmentForSidebar() {
   return (
     <RootWrapperAppbarFragmentForSidebar>
-      <IconsMdiMenu
+      <div
+        style={{
+          width: 24,
+          height: 24,
+        }}
+      ></div>
+      {/* <IconsMdiMenu
         // TODO: replace resource
         src="https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/333b/8550/4bdd6a7ceffe5b23b37bc68c1fb7a4ab"
         alt="image of IconsMdiMenu"
-      ></IconsMdiMenu>
+      ></IconsMdiMenu> */}
     </RootWrapperAppbarFragmentForSidebar>
   );
 }
