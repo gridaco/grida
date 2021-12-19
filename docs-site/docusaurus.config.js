518efad32dfe3c0aee60b5d66447da8d2c8348e4
softmarshmallow <woojoo@softmarshmallow.com>
2021-12-19 18:29:54 +0900

    update docs site appearance

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -23,14 +23,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          // Please change this to your repo.
           editUrl: "https://github.com/gridaco/grida.co/edit/main/docs/",
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            "https://github.com/facebook/docusaurus/edit/main/website/blog/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -43,74 +36,35 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: "Grida Docs",
         logo: {
           alt: "Grida Logo",
-          src: "img/logo.png",
+          src: "img/logo.svg",
+          href: "https://grida.co/",
+          target: "_self",
         },
         items: [
           {
             type: "doc",
-            docId: "intro",
+            docId: "getting-started/intro",
             position: "left",
-            label: "Tutorial",
+            label: "Getting Started",
           },
-          { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/gridaco",
             label: "GitHub",
             position: "right",
           },
         ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Docs",
-            items: [
-              {
-                label: "Tutorial",
-                to: "/docs/intro",
-              },
-            ],
-          },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
-          {
-            title: "More",
-            items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
-              {
-                label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        hideOnScroll: true,
       },
       prism: {
+        additionalLanguages: ["dart", "kotlin", "swift"],
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      colorMode: {
+        defaultMode: "light",
+        disableSwitch: true,
       },
     }),
 };
