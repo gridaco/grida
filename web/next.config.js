60bc60e615ebceb35c3f93911252833e94e37709
softmarshmallow <woojoo@softmarshmallow.com>
2021-12-22 07:49:48 +0900

    add docs entry redirection (tmp)

--- web/next.config.js
+++ web/next.config.js
@@ -69,6 +69,11 @@ module.exports = withBundleAnalyzer(
         },
         async redirects() {
           return [
+            {
+              source: "/docs",
+              destination: "/docs/getting-started/intro",
+              permanent: false,
+            },
             // disabling globalization page access since it's not fully implemented. (temporary)
             {
               source: "/globalization",
