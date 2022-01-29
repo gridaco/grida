3c03f1298a6393a0abf9a76aceeb6793a449c33c
softmarshmallow <woojoo@softmarshmallow.com>
2022-01-30 03:07:22 +0900

    fix state error

--- editor/scaffolds/code/index.tsx
+++ editor/scaffolds/code/index.tsx
@@ -6,14 +6,12 @@ import { EditorAppbarFragments } from "components/editor";
 import { get_framework_config } from "query/to-code-options-from-query";
 import { CodeOptionsControl } from "components/codeui-code-options-control";
 import { designToCode, Result } from "@designto/code";
-
 import { config } from "@designto/config";
 import { MainImageRepository } from "@design-sdk/core/assets-repository";
-
 import { DesignInput } from "@designto/config/input";
 import { useEditorState, useWorkspaceState } from "core/states";
-
 import { utils_dart } from "utils";
+import type { ReflectSceneNode } from "@design-sdk/core";
 
 import { utils as _design_utils } from "@design-sdk/core";
 const designq = _design_utils.query;
@@ -57,13 +55,19 @@ export function CodeSegment() {
   const targetted =
     designq.find_node_by_id_under_entry(targetId, root?.entry) ?? root?.entry;
 
+  const targetStateRef = useRef<ReflectSceneNode>();
+  targetStateRef.current = targetted;
+
   const on_result = (result: Result) => {
     if (framework_config.language == "dart") {
       // special formatter support for dartlang
       result.code.raw = utils_dart.format(result.code.raw);
       result.scaffold.raw = utils_dart.format(result.scaffold.raw);
     }
-    setResult(result);
+
+    if (result.id == targetStateRef?.current?.id) {
+      setResult(result);
+    }
   };
 
   useEffect(() => {
