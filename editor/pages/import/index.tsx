ddf476ad72ae0fbd6680c157247c95c6da5c8ee4
softmarshmallow <woojoo@softmarshmallow.com>
2022-02-20 15:07:48 +0900

    add meta data to import page

--- editor/pages/import/index.tsx
+++ editor/pages/import/index.tsx
@@ -1,6 +1,15 @@
+import Head from "next/head";
 import React from "react";
 import { HomeInput } from "scaffolds/home-input";
 
 export default function ImportPage() {
-  return <HomeInput />;
+  return (
+    <>
+      <Head>
+        <title>Grida - Import Design</title>
+        <meta name="description" content="Import your design from Figma" />
+      </Head>
+      <HomeInput />
+    </>
+  );
 }
