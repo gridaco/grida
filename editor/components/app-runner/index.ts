aad8f109b8d05c4f042312793468e2d3c757013a
UZU, J <woojoo@softmarshmallow.com>
2022-04-06 22:55:17 +0900

    add vanilla esbuild runner comp

--- editor/components/app-runner/index.ts
+++ editor/components/app-runner/index.ts
@@ -3,3 +3,4 @@ export * from "./csb-runner";
 export * from "./flutter-app-runner";
 export * from "./react-app-runner";
 export * from "./loading-indicator";
+export * from "./vanilla-esbuild-app-runner";
