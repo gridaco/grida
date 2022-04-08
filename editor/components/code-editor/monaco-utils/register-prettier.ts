91949c6e22cc3d5ef89ce89ead41fe6f287a35cc
UZU, J <woojoo@softmarshmallow.com>
2022-04-08 09:18:02 +0900

    add dartlang formatting support for monaco (without webworkers)

--- editor/components/code-editor/monaco-utils/register-prettier.ts
+++ editor/components/code-editor/monaco-utils/register-prettier.ts
@@ -1,11 +1,26 @@
 import * as monaco from "monaco-editor";
+import { formatCode as formatDartCode } from "dart-style";
 import { createWorkerQueue } from "../../../workers";
 
 export function registerDocumentPrettier(editor, monaco) {
   const disposables: monaco.IDisposable[] = [];
   let prettierWorker;
 
-  const formattingEditProvider = {
+  const dartFormattingEditProvider = {
+    provideDocumentFormattingEdits: (model, options, token) => {
+      const raw = model.getValue();
+      const { code, error } = formatDartCode(raw);
+      if (error) return [];
+      return [
+        {
+          range: model.getFullModelRange(),
+          text: code,
+        },
+      ];
+    },
+  };
+
+  const prettierFormattingEditProvider = {
     async provideDocumentFormattingEdits(model, _options, _token) {
       if (!prettierWorker) {
         prettierWorker = createWorkerQueue(
@@ -37,27 +52,35 @@ export function registerDocumentPrettier(editor, monaco) {
   disposables.push(
     monaco.languages.registerDocumentFormattingEditProvider(
       "javascript",
-      formattingEditProvider
+      prettierFormattingEditProvider
+    )
+  );
+
+  disposables.push(
+    monaco.languages.registerDocumentFormattingEditProvider(
+      "typescript",
+      prettierFormattingEditProvider
+    )
+  );
+
+  disposables.push(
+    monaco.languages.registerDocumentFormattingEditProvider(
+      "dart",
+      dartFormattingEditProvider
     )
   );
 
   disposables.push(
     monaco.languages.registerDocumentFormattingEditProvider(
       "html",
-      formattingEditProvider
+      prettierFormattingEditProvider
     )
   );
 
   disposables.push(
     monaco.languages.registerDocumentFormattingEditProvider(
       "css",
-      formattingEditProvider
-    )
-  );
-  disposables.push(
-    monaco.languages.registerDocumentFormattingEditProvider(
-      "typescript",
-      formattingEditProvider
+      prettierFormattingEditProvider
     )
   );
 
