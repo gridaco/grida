2f86ba0857d195103004b6e2a53db4e2b3c7cc94
UZU, J <woojoo@softmarshmallow.com>
2022-04-15 21:32:39 +0900

    wip

--- editor/scaffolds/devtools/index.tsx
+++ editor/scaffolds/devtools/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { DevtoolsTab } from "@code-editor/devtools";
 import { WindowConsoleFeed } from "@code-editor/devtools";
 import { colors } from "theme";
 
 export function Devtools() {
+  const [expanded, setExpended] = useState(false);
   return (
     <div
       style={{
@@ -17,24 +18,52 @@ export function Devtools() {
         background: colors.color_editor_bg_on_dark,
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          gap: 12,
-          padding: 12,
-        }}
-      >
-        <DevtoolsTab label="Console" selected badge={0} />
-        <DevtoolsTab label="Problems" badge={0} />
-        <DevtoolsTab label="React DevTools" badge={0} />
-        <DevtoolsTab label="Properties" badge={0} />
-      </div>
+      <ControllerBar
+        expanded={expanded}
+        onToggleExpand={() => setExpended(!expanded)}
+      />
       <WindowConsoleFeed
         style={{
           height: 200,
           overflow: "scroll",
         }}
       />
+    </div>
+  );
+}
+
+function ControllerBar({
+  onToggleExpand,
+  expanded,
+}: {
+  onToggleExpand: () => void;
+  expanded: boolean;
+}) {
+  return (
+    <>
+      <Tabs />
+      <ControllerBarActionArea>
+        {/* TODO: add toggle */}
+      </ControllerBarActionArea>
+    </>
+  );
+}
+
+const ControllerBarActionArea = styled.div``;
+
+function Tabs() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: 12,
+        padding: 12,
+      }}
+    >
+      <DevtoolsTab label="Console" selected badge={0} />
+      <DevtoolsTab label="Problems" badge={0} />
+      <DevtoolsTab label="React DevTools" badge={0} />
+      <DevtoolsTab label="Properties" badge={0} />
     </div>
   );
 }
