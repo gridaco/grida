017f74de89f088a363cf530fddffdd313059edd8
softmarshmallow <woojoo@softmarshmallow.com>
2022-05-11 12:02:43 +0900

    bump next12 react18

--- web/next-env.d.ts
+++ web/next-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
-/// <reference types="next-images" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
