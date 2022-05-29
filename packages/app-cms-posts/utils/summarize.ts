9dbe7b417e995204ef63f27d4852d156d3758b36
softmarshmallow <woojoo@softmarshmallow.com>
2022-05-29 21:50:47 +0900

    update summarize func

---
+++ packages/app-cms-posts/utils/summarize.ts
@@ -0,0 +1,52 @@
+import jsdom from "jsdom";
+
+/**
+ * get the summary from the body html.
+ *
+ *
+ * e.g. from
+ * ```html
+ * <p><b>SERVES: 1</b></p>
+ * <p>Legend has it that the Ice Cream Float was invented on a particularly hot Philadelphia day by a soda vendor who had run out of ice. To cool his drinks</p>
+ * ```
+ *
+ * => returns the second paragraph
+ *
+ *
+ * @param body
+ * @returns
+ */
+export function summarize(body: { html: string }): string {
+  try {
+    const { html } = body;
+
+    if (!html) return undefined;
+
+    const doc = new jsdom.JSDOM(html, {
+      contentType: "text/html",
+    }).window.document; // new DOMParser().parseFromString(html, "text/html");
+    const paragraphs = doc.querySelectorAll("p");
+    if (paragraphs.length) {
+      // list first 3 paragraphs, get the longest one.
+      const longest = Array.from(paragraphs)
+        .slice(0, 3)
+        .reduce(
+          (acc, p) => {
+            const text = p.textContent;
+            if (text.length > acc.length) {
+              return text;
+            }
+            return acc;
+          },
+          //
+          ""
+        );
+      return longest.substring(0, 200);
+    } else {
+      // extract any text from html doc
+      return doc.textContent?.substring(0, 200) ?? "";
+    }
+  } catch (e) {
+    return "";
+  }
+}
