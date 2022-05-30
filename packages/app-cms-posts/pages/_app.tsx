ffd7eb05c62c2ac4992456189d655dd394067b23
softmarshmallow <woojoo@softmarshmallow.com>
2022-05-30 09:19:42 +0900

    fix

--- packages/app-cms-posts/pages/_app.tsx
+++ packages/app-cms-posts/pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalHelpButton />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
