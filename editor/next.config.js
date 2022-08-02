43c0e8ecd314bf76e728b6288645b0961f642921
softmarshmallow <woojoo@softmarshmallow.com>
2022-08-03 01:42:47 +0900

    setup turbo & add build scripts to each packages - might have major impact

--- editor/next.config.js
+++ editor/next.config.js
@@ -1,7 +1,5 @@
 const TerserPlugin = require("terser-webpack-plugin");
 const withTM = require("next-transpile-modules")([
-  "dirty",
-
   // region @editor-app
   "@editor-app/live-session",
   "@code-editor/preview-pip", // TODO: remove me. this is for development. for production, use npm ver instead.
