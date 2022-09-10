e959184b96f644f4cfb677e4f4649630c19ff9f1
softmarshmallow <woojoo@softmarshmallow.com>
2022-09-10 11:52:30 +0900

    update hovering effect for module item

--- web/components/header/modules/index.tsx
+++ web/components/header/modules/index.tsx
@@ -3,18 +3,43 @@ import Image from "next/image";
 import React from "react";
 
 export function ModuleItem({ label, icon }: { label: string; icon: string }) {
+  const [hovering, setHovering] = React.useState(false);
+
   return (
-    <Wrapper>
-      <Image width={28} height={28} src={icon} alt={label} />
+    <Wrapper
+      onMouseEnter={() => setHovering(true)}
+      onMouseLeave={() => setHovering(false)}
+    >
+      <IconSet>
+        <Icon opacity={0.8} data-hidden={hovering}>
+          <Image
+            id="icon-black"
+            width={21}
+            height={21}
+            src={icon + "/black.svg"}
+            alt={label}
+          />
+        </Icon>
+        <Icon opacity={1} data-hidden={!hovering}>
+          <Image
+            id="icon-color"
+            width={21}
+            height={21}
+            src={icon + "/default.svg"}
+            alt={label}
+          />
+        </Icon>
+      </IconSet>
       <Name>{label}</Name>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
-  max-width: 210px;
+  min-width: 200px;
   width: fit-content;
   padding: 8px;
+  padding-right: 12px;
   cursor: pointer;
   display: flex;
   justify-content: flex-start;
@@ -26,6 +51,23 @@ const Wrapper = styled.div`
 
   :hover {
     background-color: rgba(0, 0, 0, 0.05);
+  }
+
+  transition: all 0.1s ease-in-out;
+`;
+
+const IconSet = styled.div`
+  position: relative;
+  width: 21px;
+  height: 21px;
+`;
+
+const Icon = styled.div<{ opacity: number }>`
+  position: absolute;
+  opacity: ${props => props.opacity};
+
+  &[data-hidden="true"] {
+    opacity: 0;
   }
 
   transition: all 0.1s ease-in-out;
