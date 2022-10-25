a19cb412679429fca7f012a5f9d7aaad04307bfc
softmarshmallow <woojoo@softmarshmallow.com>
2022-10-25 18:31:13 +0900

    update guide math to nearest value

--- editor-packages/editor-canvas/math/guide-spacing.ts
+++ editor-packages/editor-canvas/math/guide-spacing.ts
@@ -18,7 +18,7 @@ interface SpacingGuide {
 
 /**
  *
- * calculates the base box and the spacing of t, r, b, l with givven a and b boxes.
+ * calculates the base box and the spacing of **nearest** t, r, b, l with givven a and b boxes.
  *
  * the a and b boxes are formed with [x, y, x2, y2] format.
  *
@@ -28,22 +28,20 @@ interface SpacingGuide {
  *
  * the top, right, bottom, left spacing is relative to the base box.
  *
- * [t, r, b, l]
- * t = abs(t1 - t2)
- * r = abs(r1 - r2)
- * b = abs(b1 - b2)
- * l = abs(l1 - l2)
- *
  * For example
- * - a = [10, 10, 20, 20], b = [20, 20, 30, 30], then the spacing is [10, 10, 10, 10] and the base box is [20, 20, 20, 20]
+ * - a = [10, 10, 20, 20], b = [20, 20, 30, 30], then the spacing is [10, 0, 0, 10] and the base box is [20, 20, 20, 20]
  * - a = [10, 10, 20, 20], b = [15, 15, 25, 25], then the spacing is [5, 5, 5, 5] and the base box is [15, 15, 20, 20]
  * - a = [450, 450, 550, 550], b = [0, 0, 1000, 1000], then the spacing is [450, 450, 450, 450] and the base box is [450, 450, 550, 550]
  * - a = [0, 0, 1000, 1000], b = [450, 450, 550, 550], then the spacing is [450, 450, 450, 450] and the base box is [450, 450, 550, 550]
  * - a = [0, 0, 50, 50], b = [0, 0, 20, 20], then the spacing is [0, 30, 30, 0] and the base box is [0, 0, 20, 20]
+ * - a = [10, 10, 20, 20], b = [30, 30, 40, 40], then the spacing is [20, 10, 10, 20] and the base box is [10, 10, 20, 20]
  *
  */
 export function spacing_guide(a: Box, b: Box): SpacingGuide {
   let box: Box;
+
+  const [a_x, a_y, a_x2, a_y2] = a;
+  const [b_x, b_y, b_x2, b_y2] = b;
 
   // no intersection (if the interecting space is 0, it is also considered as no intersection)
   if (a[0] > b[2] || a[2] < b[0] || a[1] > b[3] || a[3] < b[1]) {
@@ -68,11 +66,15 @@ export function spacing_guide(a: Box, b: Box): SpacingGuide {
 
   // calculate the spacing of t, r, b, l
   const spacing: [number, number, number, number] = [
-    Math.abs(a[1] - b[1]),
-    Math.abs(a[2] - b[2]),
-    Math.abs(a[3] - b[3]),
-    Math.abs(a[0] - b[0]),
+    nearest(a_y, b_y, b_y2),
+    nearest(a_x2, b_x, b_x2),
+    nearest(a_y2, b_y, b_y2),
+    nearest(a_x, b_x, b_x2),
   ];
 
   return { box, spacing };
 }
+
+const nearest = (a: number, ...b: number[]) => {
+  return Math.min(...b.map((v) => Math.abs(v - a)));
+};
