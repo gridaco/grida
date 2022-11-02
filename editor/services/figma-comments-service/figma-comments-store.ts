473b01482ff323381d0b2ddae57d97f7435e1dfe
softmarshmallow <woojoo@softmarshmallow.com>
2022-11-02 20:31:16 +0900

    update comment store with index

--- editor/services/figma-comments-service/figma-comments-store.ts
+++ editor/services/figma-comments-service/figma-comments-store.ts
@@ -1,25 +1,29 @@
 import { openDB, IDBPDatabase } from "idb";
 import type { Comment } from "@design-sdk/figma-remote-types";
 
-type IndexedComment = Comment;
+type IndexedComment = Comment & {
+  [__index]: string;
+};
 
 // #region global db initialization
-const __db_pref = { name: "fimga-comments-store", version: 1 };
+const __db_pref = { name: "fimga-comments-store", version: 3 };
 const __pk = "id";
+const __table = "comments";
+const __index = "filekey";
 
-export const db = (filekey: string): Promise<IDBPDatabase<IndexedComment>> =>
+const connect = (): Promise<IDBPDatabase<IndexedComment>> =>
   new Promise((resolve) => {
     // disable on ssr
     if (typeof window === "undefined") {
-      resolve(null);
       return;
     }
 
     openDB<IndexedComment>(__db_pref.name, __db_pref.version, {
       upgrade(db) {
-        db.createObjectStore(filekey, {
+        const store = db.createObjectStore(__table, {
           keyPath: __pk,
         });
+        store.createIndex(__index, __index, { unique: false });
       },
     }).then((_db) => {
       resolve(_db);
@@ -33,36 +37,49 @@ export class FigmaCommentsStore {
     return new FigmaCommentsStore(filekey);
   };
 
+  private _db: IDBPDatabase<IndexedComment>;
   private get db(): Promise<IDBPDatabase<IndexedComment>> {
-    return db(this.filekey);
+    if (this._db) {
+      return Promise.resolve(this._db);
+    } else {
+      return connect().then((db) => {
+        this._db = db;
+        return db;
+      });
+    }
   }
 
   async clear() {
-    await (await this.db).clear(this.filekey);
+    // clear with index
+    const pdestroy = await (
+      await this.db
+    ).getAllKeysFromIndex(__table, __index, this.filekey);
+    await (await this.db).delete(__table, pdestroy);
   }
 
   async upsert(image: IndexedComment) {
-    try {
-      await (
-        await this.db
-      ).put(this.filekey, <IndexedComment>{
-        ...image,
-        [__pk]: image.id,
-      });
-    } catch (e) {}
+    await (
+      await this.db
+    ).put(__table, <IndexedComment>{
+      ...image,
+      [__pk]: image.id,
+      filekey: this.filekey,
+    });
   }
 
   async getAll(): Promise<ReadonlyArray<IndexedComment>> {
-    const recs = await (await this.db).getAll(this.filekey);
+    const recs = await (
+      await this.db
+    ).getAllFromIndex(__table, __index, this.filekey);
     return recs;
   }
 
   async get(query: { id: string }): Promise<IndexedComment> {
-    const rec = await (await this.db).get(this.filekey, query.id);
+    const rec = await (await this.db).get(__table, query.id);
     return rec;
   }
 
   async delete(query: { id: string }) {
-    await (await this.db).delete(this.filekey, query.id);
+    await (await this.db).delete(__table, query.id);
   }
 }
