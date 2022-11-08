7995594c1acb3c38db1db27c8a6ab79666c276e9
softmarshmallow <woojoo@softmarshmallow.com>
2022-11-08 14:41:21 +0900

    add cli snippet view on section-code

--- editor/scaffolds/inspector/section-code.tsx
+++ editor/scaffolds/inspector/section-code.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
-  PropertyLine,
+  PropertyLines,
   PropertyGroup,
   PropertyGroupHeader,
 } from "@editor-ui/property";
@@ -8,11 +8,14 @@ import { Result } from "@designto/code";
 import { useTargetContainer } from "hooks/use-target-node";
 import { useWorkspaceState } from "core/states";
 import { MonacoEditor } from "components/code-editor";
-import { InspectorSection } from "components/inspector";
+import { ClipboardBox } from "components/inspector";
 import { Button } from "@editor-ui/button";
 import { useDispatch } from "core/dispatch";
 import { code as wwcode } from "../code/code-worker-messenger";
 import { CircularProgress } from "@mui/material";
+import { ReflectSceneNode } from "@design-sdk/figma-node";
+import { copy } from "utils/clipboard";
+import styled from "@emotion/styled";
 
 export function CodeSection() {
   const wstate = useWorkspaceState();
@@ -53,6 +56,7 @@ export function CodeSection() {
           Open Code editor
         </Button>
       </PropertyGroupHeader>
+      <CliIntegrationSnippet node={target} />
       {code ? (
         <>
           <MonacoEditor
@@ -83,3 +87,38 @@ export function CodeSection() {
     </PropertyGroup>
   );
 }
+
+function CliIntegrationSnippet({ node }: { node: ReflectSceneNode }) {
+  const { id, filekey } = node;
+  const snippet = `grida add "${String(filekey).substring(0, 5)}/${id}"`;
+  const actualcmd = `grida add "https://www.figma.com/file/${String(
+    filekey
+  )}/${id}"`;
+
+  const onclick = () => {
+    copy(actualcmd, { notify: true });
+  };
+
+  return (
+    <PropertyLines>
+      <ClipboardBox
+        background="black"
+        borderRadius={8}
+        onClick={onclick}
+        singleline
+      >
+        <CodeLine
+          title={actualcmd}
+          style={{ color: "white", wordBreak: "keep-all" }}
+        >
+          $ {snippet}
+        </CodeLine>
+      </ClipboardBox>
+    </PropertyLines>
+  );
+}
+
+const CodeLine = styled.span`
+  display: inline-block;
+  font-family: "Courier New", Courier, monospace;
+`;
