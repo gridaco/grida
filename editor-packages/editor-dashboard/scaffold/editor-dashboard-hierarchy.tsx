86799f6ad59287b56307228fb4ae330e18ff227b
Universe <woojoo@softmarshmallow.com>
2022-11-20 21:55:24 +0900

    update hash routing (dirty fix)

--- editor-packages/editor-dashboard/scaffold/editor-dashboard-hierarchy.tsx
+++ editor-packages/editor-dashboard/scaffold/editor-dashboard-hierarchy.tsx
@@ -2,9 +2,11 @@ import React, { useCallback } from "react";
 import { TreeView } from "@editor-ui/editor";
 import { useDashboard } from "../core/provider";
 import { HierarchyRow, IconContainer, LayerIcon } from "../components";
+import { NextRouter, useRouter } from "next/router";
 import type { DashboardItem } from "../core/state";
 
 export function DashboardHierarchy() {
+  const router = useRouter();
   const { hierarchy, selectNode, selection } = useDashboard();
   const { sections } = hierarchy;
 
@@ -16,6 +18,7 @@ export function DashboardHierarchy() {
     (item: DashboardItem, i: number) => {
       const selected = selection.includes(item.id);
       const depth = item.path.split("/").length - 1;
+
       return (
         <HierarchyRow
           key={item.path} // todo: update - this needs to be a unique path
@@ -23,11 +26,18 @@ export function DashboardHierarchy() {
           depth={depth}
           name={item.name}
           onPress={() => {
+            if (item.$type === "folder") {
+              // this is dirty workaround for awaiting the router to update first from the editor-reducer caused by the select node action.
+              setTimeout(() => {
+                pushhash(router, item.path);
+              }, 10);
+            }
+
             selectNode(item.id);
           }}
           icon={
             <IconContainer>
-              <LayerIcon type={item.type as any} selected={selected} />
+              <LayerIcon type={item.$type as any} selected={selected} />
             </IconContainer>
           }
           onMenuClick={() => {
@@ -46,4 +56,24 @@ export function DashboardHierarchy() {
       renderItem={renderItem}
     />
   );
+}
+
+function pushhash(router: NextRouter, hash: string) {
+  router
+    .push(
+      {
+        // add anchor to url
+        hash: hash,
+      },
+      null,
+      {
+        shallow: true,
+      }
+    )
+    .catch((e) => {
+      // workaround for https://github.com/vercel/next.js/issues/37362
+      if (!e.cancelled) {
+        throw e;
+      }
+    });
 }
