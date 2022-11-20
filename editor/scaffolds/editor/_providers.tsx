213a03af0f85c9ec72c3e0ed115f209f5e2f9e93
Universe <woojoo@softmarshmallow.com>
2022-11-20 22:13:36 +0900

    fix build

--- editor/scaffolds/editor/_providers.tsx
+++ editor/scaffolds/editor/_providers.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useCallback } from "react";
 import { EditorShortcutsProvider } from "./editor-shortcuts-provider";
 import { EditorImageRepositoryProvider } from "./editor-image-repository-provider";
 import { EditorPreviewDataProvider } from "./editor-preview-provider";
@@ -7,14 +7,26 @@ import { EditorToastProvider } from "./editor-toast-provider";
 import { FigmaImageServiceProvider } from "./editor-figma-image-service-provider";
 import { FigmaImageServiceProviderForCanvasRenderer } from "./editor-figma-image-service-for-canvas-provider";
 import { DashboardStateProvider } from "@code-editor/dashboard";
-import { useEditorState } from "core/states";
+import { EditorState, useEditorState } from "core/states";
 
 export function EditorDefaultProviders(props: { children: React.ReactNode }) {
   const [state] = useEditorState();
 
-  const DashboardProvider = useMemo(() => {
-    return state.design ? DashboardStateProvider : React.Fragment;
-  }, [state.design?.version]);
+  const DashboardProvider = useCallback(
+    ({
+      children,
+      design,
+    }: React.PropsWithChildren<{ design: EditorState["design"] }>) => {
+      return state.design ? (
+        <DashboardStateProvider design={design}>
+          {children}
+        </DashboardStateProvider>
+      ) : (
+        <React.Fragment>{children}</React.Fragment>
+      );
+    },
+    [state.design?.version]
+  );
 
   return (
     <EditorToastProvider>
