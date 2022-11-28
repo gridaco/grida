1d16d3b267222dcd668d0a8a6cc23246368379bc
Universe <woojoo@softmarshmallow.com>
2022-11-29 03:16:15 +0900

    update meta desc

--- editor/pages/_document.tsx
+++ editor/pages/_document.tsx
@@ -11,7 +11,10 @@ export default function Document() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="PWA App" />
-        <meta name="description" content="Best PWA App in the world" />
+        <meta
+          name="description"
+          content="Grida Code - Manage your micro-frontend architecture with Componets cloud from Figma designs"
+        />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
