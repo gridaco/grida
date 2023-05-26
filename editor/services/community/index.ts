6a44a6a90ad1f2622b81dc24afe266d22c156f7d
softmarshmallow <woojoo@softmarshmallow.com>
2023-05-26 18:26:20 +0900

    add internal community api client

---
+++ editor/services/community/index.ts
@@ -0,0 +1,18 @@
+import Axios from "axios";
+import {
+  FigmaCommunityFileQueryParams,
+  FigmaCommunityFileMeta,
+} from "ssg/community";
+
+const client = Axios.create({
+  baseURL: "/api/community",
+});
+
+export async function query(p: FigmaCommunityFileQueryParams) {
+  return await client.get<ReadonlyArray<Partial<FigmaCommunityFileMeta>>>("/", {
+    params: {
+      origin: "figma",
+      ...p,
+    },
+  });
+}
