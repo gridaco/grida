0758afc10e5a62c9091316b860de38ddf78f99f8
softmarshmallow <woojoo@softmarshmallow.com>
2023-05-26 14:31:42 +0900

    qa route placeholding

---
+++ editor/pages/qa/files/[key]/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+
+export default function QAEditor() {
+  return (
+    <>
+      <Head>
+        <title>QA</title>
+        {/*  */}
+      </Head>
+      <main>
+        {/*  */}
+        {/*  */}
+      </main>
+    </>
+  );
+}
---
+++ editor/pages/qa/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+
+export default function QAIndexPage() {
+  return (
+    <>
+      <Head>
+        <title>QA - Home</title>
+        <meta name="description" content="QA Home" />
+      </Head>
+      <main>
+        {/*  */}
+        {/*  */}
+      </main>
+    </>
+  );
+}
