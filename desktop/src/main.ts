1da39e81039193b46d57a2eccee4694351aed64d
Universe <universe@grida.co>
2023-12-17 00:06:55 +0900

    size

--- desktop/src/main.ts
+++ desktop/src/main.ts
@@ -10,8 +10,9 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1400,
+    height: 900,
+    titleBarStyle: 'hidden',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
