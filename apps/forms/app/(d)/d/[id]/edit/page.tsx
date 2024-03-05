73a80036618c4927564a69cf14aba0052545e089
Universe <universe@grida.co>
2024-03-05 17:18:44 +0900

    add document layout

--- apps/forms/app/(d)/d/[id]/edit/page.tsx
+++ apps/forms/app/(d)/d/[id]/edit/page.tsx
@@ -9,45 +9,5 @@ export default async function EditFormPage({
 }: {
   params: { id: string };
 }) {
-  const cookieStore = cookies();
-  const supabase = createServerClient(cookieStore);
-  const id = params.id;
-
-  const { data, error } = await supabase
-    .from("form")
-    .select()
-    .eq("id", id)
-    .single();
-
-  if (!data) {
-    console.error(id, error);
-    return notFound();
-  }
-
-  return (
-    <main className="p-4">
-      <header className="flex w-full gap-4">
-        <div className="justify-self-start">
-          <EditableFormTitle value={data.title} />
-        </div>
-        <div className="justify-self-center flex gap-4">
-          <Link href={`/d/${id}/responses`}>
-            <button className="px-4 py-2 border-b-2 border-transparent hover:border-black">
-              Create
-            </button>
-          </Link>
-          <Link href={`/d/${id}/responses`}>
-            <button className="px-4 py-2 border-b-2 border-transparent hover:border-black">
-              Results
-            </button>
-          </Link>
-          <Link href={`/d/${id}/developer`}>
-            <button className="px-4 py-2 border-b-2 border-transparent hover:border-black">
-              Developer
-            </button>
-          </Link>
-        </div>
-      </header>
-    </main>
-  );
+  return <main className="p-4"></main>;
 }
