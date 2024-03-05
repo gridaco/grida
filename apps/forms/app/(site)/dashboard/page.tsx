b2e7e3650678b51fba23be7815a7a6e8799b6666
Universe <universe@grida.co>
2024-03-05 16:16:39 +0900

    style dashboard

--- apps/forms/app/(site)/dashboard/page.tsx
+++ apps/forms/app/(site)/dashboard/page.tsx
@@ -1,9 +1,11 @@
-import { DashboardFormCard } from "@/components/dashboard-form-card";
-import { createServerClient } from "@/lib/supabase";
-import { cookies } from "next/headers";
+import React from "react";
 import Link from "next/link";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-
+import { createServerClient } from "@/lib/supabase";
+import { DashboardFormCard } from "@/components/dashboard-form-card";
+import { GridaLogo } from "@/components/grida-logo";
+import { PlusIcon } from "@radix-ui/react-icons";
 export default async function FormsDashboardPage() {
   const cookieStore = cookies();
   const supabase = createServerClient(cookieStore);
@@ -16,17 +18,43 @@ export default async function FormsDashboardPage() {
 
   const { data: forms, error } = await supabase.from("form").select();
 
+  const count = forms?.length || 0;
+
   return (
-    <main>
-      <h1>Dashboard</h1>
-      <section>
+    <main className="container mx-auto px-4">
+      <header className="py-10">
+        <Link href="/dashboard">
+          <span className="flex items-center gap-2 text-2xl font-black select-none">
+            <GridaLogo />
+            Forms
+          </span>
+        </Link>
+        <h1 className="text-5xl font-mono font-black py-10 flex items-center gap-4">
+          Dashboard
+          <span className="text-2xl bg-neutral-500 text-white rounded-full py-2 px-3">
+            {count}
+          </span>
+        </h1>
+      </header>
+      <section className="flex justify-end py-4">
         <Link href="/new">
-          <button className="bg-blue">Create Form</button>
+          <button className="flex justify-center items-center gap-2 px-4 py-2 bg-black text-white dark:invert rounded">
+            <PlusIcon />
+            Create new Form
+          </button>
         </Link>
       </section>
-      {forms?.map((form, i) => (
-        <DashboardFormCard key={i} />
-      ))}
+      <hr className="my-10" />
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+        {forms?.map((form, i) => (
+          <Link key={i} href={`/d/${form.id}`}>
+            <DashboardFormCard
+              title={form.title}
+              thumbnail="/assets/placeholder-image.png"
+            />
+          </Link>
+        ))}
+      </div>
     </main>
   );
 }
--- apps/forms/components/dashboard-form-card.tsx
+++ apps/forms/components/dashboard-form-card.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import React from "react";
 
-export function DashboardFormCard() {
+export function DashboardFormCard({
+  title,
+  thumbnail,
+}: {
+  title: string;
+  thumbnail: string;
+}) {
   return (
-    <div>
-      <Image src="" alt="thumbnail" />
-      <span>Form</span>
+    <div className="rounded border border-neutral-500/10 shadow-md">
+      <Image width={240} height={300} src={thumbnail} alt="thumbnail" />
+      <div className="px-4 py-2">
+        <span>{title}</span>
+      </div>
     </div>
   );
 }
