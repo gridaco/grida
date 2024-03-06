2dd59a1dfc15d0de028541fcc2c09830ec444892
Universe <universe@grida.co>
2024-03-07 01:39:34 +0900

    wip homepage

--- apps/forms/app/(index)/page.tsx
+++ apps/forms/app/(index)/page.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col p-24">
+      <header></header>
+      <div>
+        <h1 className="text-5xl font-black py-4">Grida Forms</h1>
+        <p className="text-sm opacity-50">
+          Grida Forms is a headless api-first forms builder for developers
+        </p>
+      </div>
+    </main>
   );
 }
