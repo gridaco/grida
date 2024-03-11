f229def94435bd8647869c3c5d9f799e42891787
Universe <universe@grida.co>
2024-03-12 03:40:22 +0900

    bump

--- apps/posts/next.config.js
+++ apps/posts/next.config.js
@@ -1,4 +1,20 @@
-module.exports = {
+const packages = [
+  // boring
+  "@boring.so/store",
+  "@boring.so/loader",
+  "@boring.so/config",
+  "@boringso/react-core",
+  "@boring.so/document-model",
+  "@boring.so/template-provider",
+];
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
   experimental: { esmExternals: "loose" },
   basePath: "/posts",
+  transpilePackages: packages,
 };
+
+module.exports = config;
