b5923d511d5a98c962019fddc900e4fac9fab254
Universe <universe@grida.co>
2024-03-12 01:03:36 +0900

    pnpm

--- packages/app-cms-posts/next.config.js
+++ packages/app-cms-posts/next.config.js
@@ -1,16 +1,4 @@
-const TerserPlugin = require("terser-webpack-plugin");
-const config = require("../../next.config.base");
-const path = require("path");
-const { withTM, webpack } = config;
-
-module.exports = withTM({
-  webpack: webpack(path.resolve(__dirname, "..")),
-
+module.exports = {
   experimental: { esmExternals: "loose" },
-
   basePath: "/posts",
-
-  async rewrites() {
-    return [];
-  },
-});
+};
