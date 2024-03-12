1ad48fe33b8bda2b348fd92fa30cb7574659eb47
Universe <universe@grida.co>
2024-03-12 16:55:03 +0900

    add home redirect

--- apps/forms/app/(index)/layout.tsx
+++ apps/forms/app/(index)/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { createServerClient } from "@/lib/supabase/server";
+import { cookies } from "next/headers";
 import "../globals.css";
+import { redirect } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,11 +11,22 @@ export const metadata: Metadata = {
   title: "Grida Forms",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = cookies();
+  const supabase = createServerClient(cookieStore);
+
+  const { data } = await supabase.auth.getSession();
+
+  const isLoggedIn = !!data.session?.user;
+
+  if (isLoggedIn) {
+    redirect("/dashboard");
+  }
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
