f7ce65dae8549c2ef7ea985642abcb4960ca3e0d
Universe <universe@grida.co>
2024-03-13 12:38:26 +0900

    add state tab

--- apps/forms/app/(d)/d/[id]/layout.tsx
+++ apps/forms/app/(d)/d/[id]/layout.tsx
@@ -6,6 +6,7 @@ import { createServerClient } from "@/lib/supabase/server";
 import { GridaLogo } from "@/components/grida-logo";
 import { EyeOpenIcon, SlashIcon } from "@radix-ui/react-icons";
 import { Toaster } from "react-hot-toast";
+import { Tabs } from "@/scaffolds/d/tabs";
 
 export default async function Layout({
   params,
@@ -30,13 +31,13 @@ export default async function Layout({
   }
 
   return (
-    <main className="p-4 min-h-screen h-full">
+    <main className="px-4 min-h-screen h-full">
       <Toaster position="bottom-center" />
-      <header className="flex w-full gap-4">
+      <header className="flex w-full gap-4 border-b">
         <div className="w-1/3 flex gap-2 items-center justify-start">
           <Link href="/dashboard">
-            <span className="flex items-center gap-2 text-2xl font-black select-none">
-              <GridaLogo />
+            <span className="flex items-center gap-2 text-xl font-black select-none">
+              <GridaLogo size={20} />
               Forms
             </span>
           </Link>
@@ -44,21 +45,7 @@ export default async function Layout({
           <EditableFormTitle form_id={id} defaultValue={data.title} />
         </div>
         <div className="w-1/3 flex items-center justify-center gap-4">
-          <Link href={`/d/${id}/design`}>
-            <button className="px-4 py-2 border-b-2 border-transparent hover:border-black">
-              Design
-            </button>
-          </Link>
-          <Link href={`/d/${id}/data`}>
-            <button className="px-4 py-2 border-b-2 border-transparent hover:border-black">
-              Data
-            </button>
-          </Link>
-          <Link href={`/d/${id}/share`}>
-            <button className="px-4 py-2 border-b-2 border-transparent hover:border-black">
-              Share
-            </button>
-          </Link>
+          <Tabs form_id={id} />
         </div>
         <div className="w-1/3 flex gap-4 items-center justify-end">
           <Link href={"preview"} target="_blank">
---
+++ apps/forms/scaffolds/d/tabs.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+
+export function Tabs({ form_id: id }: { form_id: string }) {
+  const router = useRouter();
+  const selectedId = usePathname();
+
+  const tab = selectedId.split("/").pop();
+
+  return (
+    <>
+      <Link href={`/d/${id}/design`}>
+        <Tab selected={tab === "design"}>Design</Tab>
+      </Link>
+      <Link href={`/d/${id}/data`}>
+        <Tab selected={tab === "data"}>Data</Tab>
+      </Link>
+      <Link href={`/d/${id}/share`}>
+        <Tab selected={tab === "share"}>Share</Tab>
+      </Link>
+    </>
+  );
+}
+
+function Tab({
+  selected,
+  children,
+}: React.PropsWithChildren<{
+  selected: boolean;
+}>) {
+  return (
+    <button
+      data-selected={selected}
+      className="
+        mx-2 px-2 py-4 border-b-2 border-transparent opacity-50 hover:border-black
+        data-[selected='true']:border-black
+        data-[selected='true']:opacity-100
+        transition-all
+        font-medium text-sm
+      "
+    >
+      {children}
+    </button>
+  );
+}
