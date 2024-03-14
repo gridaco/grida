6f0f8a8f603825acdc4b0ca81e6ca38f6166af71
Universe <universe@grida.co>
2024-03-14 20:11:21 +0900

    add GA

--- apps/forms/app/(index)/layout.tsx
+++ apps/forms/app/(index)/layout.tsx
@@ -2,13 +2,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { createServerClient } from "@/lib/supabase/server";
 import { cookies } from "next/headers";
-import "../globals.css";
 import { redirect } from "next/navigation";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Grida Forms",
+  description:
+    "Grida Forms is a headless & api-first form builder for developers",
 };
 
 export default async function RootLayout({
@@ -30,6 +33,9 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      {process.env.NEXT_PUBLIC_GAID && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GAID} />
+      )}
     </html>
   );
 }
