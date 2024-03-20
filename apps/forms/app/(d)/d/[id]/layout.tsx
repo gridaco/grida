8e487d2870eac6124b94d6ad4267caf4f7bc289b
Universe <universe@grida.co>
2024-03-20 16:18:27 +0900

    split layout

--- apps/forms/app/(d)/d/[id]/layout.tsx
+++ apps/forms/app/(d)/d/[id]/layout.tsx
@@ -8,6 +8,16 @@ import { EyeOpenIcon, SlashIcon } from "@radix-ui/react-icons";
 import { Toaster } from "react-hot-toast";
 import { Tabs } from "@/scaffolds/d/tabs";
 import { FormEditorProvider } from "@/scaffolds/editor";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import "../../../editor.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Grida Forms",
+};
 
 export const revalidate = 0;
 
@@ -43,48 +53,56 @@ export default async function Layout({
   }
 
   return (
-    <main className="min-h-screen h-screen flex flex-col">
-      <Toaster position="bottom-center" />
-      <header className="sticky top-0 px-4 flex w-full gap-4 border-b dark:border-neutral-900 bg-white dark:bg-black z-10">
-        <div className="w-1/3 flex items-center justify-start">
-          <Link href="/dashboard">
-            <span className="flex items-center gap-2 text-md font-black select-none">
-              <GridaLogo size={15} />
-              Forms
-            </span>
-          </Link>
-          <SlashIcon className="min-w-[20px] ml-2" width={15} height={15} />
-          <EditableFormTitle form_id={id} defaultValue={data.title} />
-        </div>
-        <div className="w-1/3 flex items-center justify-center gap-4">
-          <Tabs form_id={id} />
-        </div>
-        <div className="w-1/3 flex gap-4 items-center justify-end">
-          <Link href={"preview"} target="_blank">
-            <button
-              className="p-2 h-10 w-10 rounded bg-neutral-200"
-              title="Preview"
-            >
-              <EyeOpenIcon className="mx-auto" width={20} height={20} />
-            </button>
-          </Link>
-          <button
-            className="px-4 py-2 h-10 rounded bg-neutral-200"
-            title="Publish"
+    <html lang="en">
+      <body className={inter.className}>
+        {process.env.NEXT_PUBLIC_GAID && (
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GAID} />
+        )}
+
+        <main className="min-h-screen h-screen flex flex-col">
+          <Toaster position="bottom-center" />
+          <header className="sticky top-0 px-4 flex w-full gap-4 border-b dark:border-neutral-900 bg-white z-10">
+            <div className="w-1/3 flex items-center justify-start">
+              <Link href="/dashboard">
+                <span className="flex items-center gap-2 text-md font-black select-none">
+                  <GridaLogo size={15} />
+                  Forms
+                </span>
+              </Link>
+              <SlashIcon className="min-w-[20px] ml-2" width={15} height={15} />
+              <EditableFormTitle form_id={id} defaultValue={data.title} />
+            </div>
+            <div className="w-1/3 flex items-center justify-center gap-4">
+              <Tabs form_id={id} />
+            </div>
+            <div className="w-1/3 flex gap-4 items-center justify-end">
+              <Link href={"preview"} target="_blank">
+                <button
+                  className="p-2 h-10 w-10 rounded bg-neutral-200"
+                  title="Preview"
+                >
+                  <EyeOpenIcon className="mx-auto" width={20} height={20} />
+                </button>
+              </Link>
+              <button
+                className="px-4 py-2 h-10 rounded bg-neutral-200"
+                title="Publish"
+              >
+                Publish
+              </button>
+            </div>
+          </header>
+          <FormEditorProvider
+            initial={{
+              form_id: id,
+              fields: data.fields,
+              blocks: data.blocks,
+            }}
           >
-            Publish
-          </button>
-        </div>
-      </header>
-      <FormEditorProvider
-        initial={{
-          form_id: id,
-          fields: data.fields,
-          blocks: data.blocks,
-        }}
-      >
-        {children}
-      </FormEditorProvider>
-    </main>
+            {children}
+          </FormEditorProvider>
+        </main>
+      </body>
+    </html>
   );
 }
--- apps/forms/app/(d)/d/e/layout.tsx
+++ apps/forms/app/(d)/d/e/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import "../editor.css";
+import "../../../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
