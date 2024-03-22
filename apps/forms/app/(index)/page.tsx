821e08abb812d0f39661e74c3e29c1170bea1720
ryujunghy3on <ryu@grida.co>
2024-03-22 20:36:58 +0900

    .

--- apps/forms/app/(index)/page.tsx
+++ apps/forms/app/(index)/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
                 builder for developers
               </p>
             </div>
-            <button className="mt-20 px-3 py-2 bg-neutral-800 rounded border border-neutral-800">
+            <button className="mt-20 px-3 py-2 bg-neutral-800 rounded border border-neutral-800 hover:invert transition-all">
               Start your project
             </button>
           </div>
@@ -96,6 +96,16 @@ export default function Home() {
               Start building for free, collaborate with a team, then scale to
               millions of users.
             </p>
+            <label className="inline-flex items-center cursor-pointer">
+              <input type="checkbox" value="" className="sr-only peer" />
+              <span className="me-3 text-sm text-gray-900 dark:text-gray-300">
+                Monthly
+              </span>
+              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-blue  rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              <span className="ms-3 text-sm text-gray-900 dark:text-gray-300">
+                Yearly (00% save)
+              </span>
+            </label>
           </div>
           <div className="columns-1 xl:columns-4 gap-10 w-full">
             <PricingCard
@@ -131,14 +141,55 @@ export default function Home() {
           </div>
         </section>
         <section>
-          <div className="py-20 flex flex-col gap-4">
-            <div>
-              <p className="text-lg font-semibold">Free</p>
-              <p className=" text-lg opacity-50">$0 / month</p>
+          <div className="flex flex-row">
+            <div className="columns-1 py-20 pl-20 gap-4">
+              <div>
+                <p className="text-lg font-semibold">Free</p>
+                <p className=" text-lg opacity-50">$0 / month</p>
+              </div>
+              <button
+                className="mt-8 px-11 py-2 bg-neutral-800 rounded border border-neutral-700 hover:invert
+        transition-all"
+              >
+                Start for free
+              </button>
             </div>
-            <button className=" px-6 py-2 bg-neutral-800 rounded border border-neutral-700">
-              Start for free
-            </button>
+            <div className="columns-1 py-20 pl-20 gap-4">
+              <div>
+                <p className="text-lg font-semibold">Pro</p>
+                <p className=" text-lg opacity-50">$0 / month</p>
+              </div>
+              <button
+                className="mt-8 px-11 py-2 bg-neutral-800 rounded border border-neutral-700 hover:invert
+        transition-all"
+              >
+                Start for free
+              </button>
+            </div>
+            <div className="columns-1 py-20 pl-20 gap-4">
+              <div>
+                <p className="text-lg font-semibold">Business</p>
+                <p className=" text-lg opacity-50">$0 / month</p>
+              </div>
+              <button
+                className="mt-8 px-11 py-2 bg-neutral-800 rounded border border-neutral-700 hover:invert
+        transition-all"
+              >
+                Start for free
+              </button>
+            </div>
+            <div className="columns-1 py-20 pl-20 gap-4">
+              <div>
+                <p className="text-lg font-semibold">Enterprise</p>
+                <p className=" text-lg opacity-50">$0 / month</p>
+              </div>
+              <button
+                className="mt-8 px-11 py-2 bg-neutral-800 rounded border border-neutral-700 hover:invert
+        transition-all"
+              >
+                Start for free
+              </button>
+            </div>
           </div>
         </section>
         <section>
@@ -147,9 +198,56 @@ export default function Home() {
             <h2 className="text-4xl font-semibold">
               Build in a weekend, scale to millions
             </h2>
-            <button className="mt-10 px-3 py-2 bg-neutral-800 rounded border border-neutral-800">
+            <button
+              className="mt-10 px-3 py-2 bg-neutral-800 rounded border border-neutral-800 hover:invert
+        transition-all"
+            >
               Start your project
             </button>
+          </div>
+        </section>
+        <section>
+          <div id="accordion-collapse" data-accordion="collapse">
+            <h2 id="accordion-collapse-heading-1">
+              <button
+                type="button"
+                className="flex items-center justify-between w-full p-5 font-sm rtl:text-right rounded-xl dark:focus:ring-gray-800 dark:hover:bg-neutral-800 gap-3"
+                data-accordion-target="#accordion-collapse-body-1"
+                aria-expanded="true"
+                aria-controls="accordion-collapse-body-1"
+              >
+                <span>What is Grida Forms?</span>
+                <svg
+                  data-accordion-icon
+                  className="w-3 h-3 rotate-180 shrink-0"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 5 5 1 1 5"
+                  />
+                </svg>
+              </button>
+            </h2>
+          </div>
+          <div
+            id="accordion-collapse-body-1"
+            className="hidden"
+            aria-labelledby="accordion-collapse-heading-1"
+          >
+            <div className="p-5 dark:bg-gray-900">
+              <p className="mb-2 text-gray-500 dark:text-gray-400">
+                Flowbite is an open-source library of interactive components
+                built on top of Tailwind CSS including buttons, dropdowns,
+                modals, navbars, and more.
+              </p>
+            </div>
           </div>
         </section>
       </div>
