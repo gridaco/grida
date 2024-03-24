dec0d0ab832889bca890632a4963d4aa1b46046c
Universe <universe@grida.co>
2024-03-25 00:24:11 +0900

    postcss nesting

--- apps/forms/postcss.config.js
+++ apps/forms/postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: {
+    // https://github.com/adazzle/react-data-grid/issues/3460#issuecomment-2016837753
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
   },
