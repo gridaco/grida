4a8e5bb08174ed5430da02d55dcb38fb0309271e
Universe <universe@grida.co>
2024-03-30 04:26:19 +0900

    handle region

--- apps/forms/scaffolds/blocks-editor/blocks/index.tsx
+++ apps/forms/scaffolds/blocks-editor/blocks/index.tsx
@@ -98,7 +98,7 @@ export function Block(props: EditorFlatFormBlock) {
           {...listeners}
           {...attributes}
           className={clsx(
-            "absolute left-0 right-0 top-3 flex items-center justify-center",
+            "absolute left-44 right-44 top-3 flex items-center justify-center",
             "cursor-move",
             "opacity-0 p-1 group-hover:opacity-100"
           )}
