93d2c5241abea1c64bb0163744c01c8b4994f828
Universe <universe@grida.co>
2024-04-01 04:39:54 +0900

    add themes route

--- apps/forms/app/(theme)/theme/backgrounds/globe/page.tsx
+++ apps/forms/app/(theme)/theme/backgrounds/globe/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 
 const World = dynamic(
-  () => import("../../../../theme/backgrounds/globe").then((m) => m.World),
+  () => import("../../../../../theme/backgrounds/globe").then((m) => m.World),
   {
     ssr: false,
   }
