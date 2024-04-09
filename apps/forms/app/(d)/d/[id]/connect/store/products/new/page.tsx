39409934e7b22c80e0c4f3fe9a8df4b948c5b166
Universe <universe@grida.co>
2024-04-10 04:24:27 +0900

    man

--- apps/forms/app/(d)/d/[id]/connect/store/products/new/page.tsx
+++ apps/forms/app/(d)/d/[id]/connect/store/products/new/page.tsx
@@ -13,3 +13,48 @@ export default function StoreProductsNewPage() {
     </main>
   );
 }
+
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+// import { Textarea } from "@/components/ui/textarea";
+
+// export default function Component() {
+//   return (
+//     <Card>
+//       <CardHeader>
+//         <CardTitle>Product Details</CardTitle>
+//         <CardDescription>
+//           Lipsum dolor sit amet, consectetur adipiscing elit
+//         </CardDescription>
+//       </CardHeader>
+//       <CardContent>
+//         <div className="grid gap-6">
+//           <div className="grid gap-3">
+//             <Label htmlFor="name">Name</Label>
+//             <Input
+//               id="name"
+//               type="text"
+//               className="w-full"
+//               defaultValue="Gamer Gear Pro Controller"
+//             />
+//           </div>
+//           <div className="grid gap-3">
+//             <Label htmlFor="description">Description</Label>
+//             <Textarea
+//               id="description"
+//               defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies ultricies, nunc nisl ultricies nunc, nec ultricies nunc nisl nec nunc."
+//               className="min-h-32"
+//             />
+//           </div>
+//         </div>
+//       </CardContent>
+//     </Card>
+//   );
+// }
