37a3c9b2d5dcc022757dcbb320d0c675bd850886
Universe <universe@grida.co>
2024-04-11 17:08:23 +0900

    page

--- apps/forms/app/(d)/d/[id]/connect/store/products/page.tsx
+++ apps/forms/app/(d)/d/[id]/connect/store/products/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function StoreProductsPage() {
   return (
@@ -10,9 +11,19 @@ export default function StoreProductsPage() {
           <li>Orders</li>
         </ul>
       </nav>
-      <Link href="./products/new">
-        <button>Add product</button>
-      </Link>
+      <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+        <div className="flex flex-col items-center gap-1 text-center">
+          <h3 className="text-2xl font-bold tracking-tight">
+            You have no products
+          </h3>
+          <p className="text-sm text-muted-foreground">
+            You can start selling as soon as you add a product.
+          </p>
+          <Link href="./products/new">
+            <Button className="mt-4">Add Product</Button>
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
