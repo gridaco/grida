aaa8a1512c6247900e453e8961d342bc6dabcea6
Universe <universe@grida.co>
2024-04-26 17:56:33 +0900

    store connect prep

---
+++ apps/forms/app/(api)/private/editor/connect/store/connection/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  // create new store if no store_id provided
+
+  const store_name = "[generated]";
+
+  // create new connection record
+  return;
+}
