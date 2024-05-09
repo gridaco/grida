5aed6b09eb6dac2b18f6fc9f79b9b3df69c01cdf
Universe <universe@grida.co>
2024-05-09 23:41:16 +0900

    playground monaco schema

--- apps/forms/app/(dev)/playground/page.tsx
+++ apps/forms/app/(dev)/playground/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 
-import { Select } from "@/components/vanilla/select";
-import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Editor, useMonaco } from "@monaco-editor/react";
+import { useEffect, useState } from "react";
 
 export default function FormsPlayground() {
   const [action, setAction] = useState<string>("");
-  const [method, setMethod] = useState<string>("GET");
+  const [method, setMethod] = useState<string>("get");
 
   return (
     <main className="container mx-auto">
       <div className="p-10">
         <header className="py-4 flex flex-col">
           <h1 className="text-xl font-bold">Grida Forms Playground</h1>
+          <FormJsonEditor />
           <div>
             <input
               type="text"
@@ -19,11 +27,16 @@ export default function FormsPlayground() {
               value={action}
               onChange={(e) => setAction(e.target.value)}
             />
-            <Select value={method} onChange={(e) => setMethod(e.target.value)}>
-              <option>GET</option>
-              <option>POST</option>
-              <option>PUT</option>
-              <option>DELETE</option>
+            <Select value={method} onValueChange={(value) => setMethod(value)}>
+              <SelectTrigger id="method" aria-label="select method">
+                <SelectValue placeholder="Select method" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="get">GET</SelectItem>
+                <SelectItem value="post">POST</SelectItem>
+                <SelectItem value="put">PUT</SelectItem>
+                <SelectItem value="delete">DELETE</SelectItem>
+              </SelectContent>
             </Select>
           </div>
         </header>
@@ -37,4 +50,22 @@ export default function FormsPlayground() {
       </div>
     </main>
   );
+}
+
+const schema = {
+  uri: "https://forms.grida.co/schema/form.schema.json",
+  fileMatch: ["*"], // Associate with all JSON files
+};
+
+function FormJsonEditor() {
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    monaco?.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      schemas: [schema],
+    });
+  }, [monaco]);
+
+  return <Editor height={500} defaultLanguage="json" />;
 }
