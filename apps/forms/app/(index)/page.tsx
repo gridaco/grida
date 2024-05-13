ceee147e78558417ede961cb98b2c97436ab3ef2
Universe <universe@grida.co>
2024-05-13 13:00:47 +0900

    ai page

--- apps/forms/app/(index)/page.tsx
+++ apps/forms/app/(index)/page.tsx
@@ -13,6 +13,7 @@ import {
   VisualStudioIcon,
 } from "@/www/icons";
 import { plans } from "@/www/data/plans";
+import { Header } from "../(site)/header";
 
 export default function Home() {
   return (
@@ -238,46 +239,5 @@ function FeatureCard({
         </div>
       </div>
     </div>
-  );
-}
-
-async function Header() {
-  return (
-    <header className="absolute top-0 left-0 right-0 py-4 px-4 md:py-14 md:px-24 flex justify-between items-center z-50">
-      <div className="flex">
-        <span className="flex items-center gap-2">
-          <Link href="https://grida.co" target="_blank">
-            <GridaLogo />
-          </Link>
-          <SlashIcon width={20} height={20} />
-          <Link href="/">
-            <span className="text-2xl font-bold dark:text-white">
-              Grida Forms
-            </span>
-          </Link>
-        </span>
-      </div>
-      <div className="flex gap-10 items-center">
-        <Link href="https://github.com/gridaco/grida/tree/main/apps/forms">
-          <button className="flex justify-center items-center">
-            <GitHubLogoIcon className="fill-black" width={24} height={24} />
-          </button>
-        </Link>
-        <Link href="/ai" className="hidden md:block">
-          <button className="hover:underline">AI</button>
-        </Link>
-        <Link href="/playground" className="hidden md:block">
-          <button className="hover:underline">Playground</button>
-        </Link>
-        <Link href="/sign-in" className="hidden md:block">
-          <button>Sign in</button>
-        </Link>
-        <Link href="/sign-in">
-          <button className="px-4 py-2 rounded bg-black text-white dark:bg-white dark:text-black">
-            Get Started
-          </button>
-        </Link>
-      </div>
-    </header>
   );
 }
--- apps/forms/app/(site)/ai/page.tsx
+++ apps/forms/app/(site)/ai/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import { Prompt } from "./prompt";
-
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Header } from "@/app/(site)/header";
 export const metadata: Metadata = {
   title: "AI Forms Builder | Grida Forms",
   description: "Grida Forms AI Forms Builder",
@@ -9,13 +11,19 @@ export const metadata: Metadata = {
 export default function AIHome() {
   return (
     <main className="flex flex-col w-full min-h-screen items-center justify-center">
+      <Header />
       <div className="text-center mb-5">
         <h2 className="text-5xl font-bold">Build Forms with AI</h2>
         <p className="mt-2 text-lg text-gray-500 dark:text-gray-400">
-          Enter your AI prompt and let the magic happen.
+          Describe your form and see the magic happen
         </p>
       </div>
       <Prompt />
+      <div className="mt-10">
+        <Link href="/playground">
+          <Button variant="link">Jump to playground</Button>
+        </Link>
+      </div>
     </main>
   );
 }
