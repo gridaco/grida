5bb550f61f8e8998ffff88c3ba16aa1756cf78ae
Universe <universe@grida.co>
2024-05-17 20:50:15 +0900

    grid editor initial

--- apps/forms/app/(dev)/dev/grid/page.tsx
+++ apps/forms/app/(dev)/dev/grid/page.tsx
@@ -1,8 +1,8 @@
-import { GridEditor } from "@/grid";
+import GridEditor from "@/grid";
 
 export default function GridEditorDevPage() {
   return (
-    <main className="w-screen h-screen">
+    <main className="w-screen h-screen prose mx-auto">
       <GridEditor />
     </main>
   );
--- apps/forms/grid/editor.tsx
+++ apps/forms/grid/editor.tsx
@@ -1,28 +1,367 @@
 "use client";
-
+import { produce } from "immer";
 import { PlusIcon } from "@radix-ui/react-icons";
-import React from "react";
+import React, {
+  createContext,
+  memo,
+  useCallback,
+  useContext,
+  useMemo,
+} from "react";
+import { useGesture } from "@use-gesture/react";
+import { Dialog, DialogClose, DialogContent } from "@/components/ui/dialog";
+import { nanoid } from "nanoid";
+import { Button } from "@/components/ui/button";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
-export function GridEditor() {
+const blockpresets = [
+  { type: "button", label: "Button" },
+  { type: "text", label: "Text" },
+  { type: "link", label: "Link" },
+  { type: "image", label: "Image" },
+  { type: "video", label: "Video" },
+  { type: "youtue", label: "YouTube" },
+] as const;
+
+type GridaBlock =
+  | GridaGridImageBlock
+  | GridaGridTypographyBlock
+  | GridaGridButtonBlock
+  | GridaGridVideoBlock;
+
+type GridaBlockType = GridaBlock["type"];
+
+type GridaGridImageBlock = {
+  type: "image";
+  src: string;
+};
+
+type GridaGridTypographyBlock = {
+  type: "typography";
+  element: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
+  data: string;
+};
+
+type GridaGridButtonBlock = {
+  type: "button";
+  label: string;
+  href?: string;
+};
+
+type GridaGridVideoBlock = {
+  type: "video";
+  src: string;
+};
+
+type Area = [number, number, number, number];
+type Position = [number, number];
+type Size = [number, number];
+type BlockId = string;
+
+type Block<T = any> = {
+  id: BlockId;
+  x: Position;
+  y: Position;
+  z?: number;
+  element?: T;
+};
+
+interface State {
+  pos: Position;
+  dragging: boolean;
+  start?: Position;
+  end?: Position;
+  controls: {
+    insert_panel_open: boolean;
+  };
+  selected?: BlockId;
+  marquee?: Area;
+  debug?: boolean;
+  size: Size;
+  blocks: Block[];
+}
+
+type Action =
+  | CellPonterMoveAction
+  | CellPonterDownrAction
+  | OpenChangeInsertBlockPanel
+  | CellPonterUpAction
+  | InsertBlock;
+
+interface CellPonterMoveAction {
+  type: "pointermove";
+  pos: [number, number];
+}
+
+interface CellPonterDownrAction {
+  type: "pointerdown";
+}
+
+interface CellPonterUpAction {
+  type: "pointerup";
+}
+
+interface OpenChangeInsertBlockPanel {
+  type: "controls/insert_panel_open";
+  open: boolean;
+}
+
+interface InsertBlock<T = any> {
+  type: "blocks/new";
+  block: T;
+}
+
+const Context = React.createContext<State | undefined>(undefined);
+
+type Dispatcher = (action: Action) => void;
+type FlatDispatcher = (action: Action) => void;
+const __noop = () => {};
+
+const DispatchContext = createContext<Dispatcher>(__noop);
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "pointerdown": {
+      return produce(state, (draft) => {
+        draft.dragging = true;
+        draft.start = state.pos;
+        draft.marquee = [...state.pos, ...state.pos];
+      });
+    }
+    case "pointerup": {
+      return produce(state, (draft) => {
+        draft.dragging = false;
+        draft.end = state.pos;
+
+        // open insert panel
+        draft.controls.insert_panel_open = true;
+      });
+    }
+    case "pointermove": {
+      const { pos } = action;
+      return produce(state, (draft) => {
+        draft.pos = pos;
+
+        if (state.dragging && state.start) {
+          const [startX, startY] = state.start;
+          const [endX, endY] = pos;
+
+          const [minX, maxX] = startX < endX ? [startX, endX] : [endX, startX];
+          const [minY, maxY] = startY < endY ? [startY, endY] : [endY, startY];
+
+          draft.end = pos;
+          draft.marquee = [minX, minY, maxX, maxY];
+        }
+      });
+    }
+    case "controls/insert_panel_open": {
+      return produce(state, (draft) => {
+        draft.controls.insert_panel_open = action.open;
+      });
+    }
+    case "blocks/new": {
+      return produce(state, (draft) => {
+        const id = nanoid();
+        const [_mx1, _my1, _mx2, _my2] = state.marquee ?? [0, 0, 1, 1];
+
+        const x: Position = [_mx1 + 1, _mx2 + 2]; // +1 to endX for exclusive end in grid-area
+        const y: Position = [_my1 + 1, _my2 + 2]; // +1 to endY for exclusive end in grid-area
+
+        const el = create_initial_grida_block(action.block);
+        draft.blocks.push({
+          id,
+          x: x,
+          y: y,
+          z: 0,
+          element: el,
+        });
+
+        // close insert panel
+        draft.controls.insert_panel_open = false;
+      });
+    }
+  }
+  return produce(state, (draft) => {});
+}
+
+function create_initial_grida_block(block: GridaBlockType): GridaBlock {
+  switch (block) {
+    case "button": {
+      return {
+        type: "button",
+        label: "Button",
+      };
+    }
+    case "typography": {
+      return {
+        type: "typography",
+        element: "h1",
+        data: "Hello World",
+      };
+    }
+    // case 'link': {
+    //   return {
+    //     type: 'link',
+    //     label: 'Link'
+    //   }
+    // }
+    case "image": {
+      return {
+        type: "image",
+        src: "https://placehold.co/600x400",
+      };
+    }
+    case "video": {
+      return {
+        type: "video",
+        src: "",
+      };
+    }
+    // case 'youtube': {
+    //   return {
+    //     type: 'youtube',
+    //     src: ''
+    //   }
+    // }
+  }
+}
+
+export const useDispatch = (): FlatDispatcher => {
+  const dispatch = useContext(DispatchContext);
+  return useCallback(
+    (action: Action) => {
+      dispatch(action);
+    },
+    [dispatch]
+  );
+};
+
+export const StateProvider = memo(function StateProvider({
+  state,
+  dispatch,
+  children,
+}: {
+  state: State;
+  dispatch?: Dispatcher;
+  children?: React.ReactNode;
+}) {
+  return (
+    <Context.Provider value={state}>
+      <DispatchContext.Provider value={dispatch ?? __noop}>
+        {children}
+      </DispatchContext.Provider>
+    </Context.Provider>
+  );
+});
+
+const useGrid = (): [State, FlatDispatcher] => {
+  const state = useContext(Context);
+
+  if (!state) {
+    throw new Error(`No StateProvider: this is a logical error.`);
+  }
+
+  const dispatch = useDispatch();
+
+  return useMemo(() => [state, dispatch], [state, dispatch]);
+};
+
+const initial: State = {
+  pos: [0, 0],
+  dragging: false,
+  selected: undefined,
+  marquee: undefined,
+  controls: {
+    insert_panel_open: false,
+  },
+  size: [6, 12],
+  blocks: [],
+};
+
+function Provider({ children }: React.PropsWithChildren<{}>) {
+  const [state, dispatch] = React.useReducer(reducer, initial);
+  return (
+    <StateProvider state={state} dispatch={dispatch}>
+      {children}
+    </StateProvider>
+  );
+}
+
+export default function Editor() {
+  return (
+    <Provider>
+      <GridEditor />
+    </Provider>
+  );
+}
+
+function GridEditor() {
+  const [state, dispatch] = useGrid();
   return (
     <div className="relative w-full h-full">
+      <Dialog
+        open={state.controls.insert_panel_open}
+        onOpenChange={(open) => {
+          dispatch({ type: "controls/insert_panel_open", open: open });
+        }}
+      >
+        <DialogContent>
+          {blockpresets.map((block) => (
+            <button
+              key={block.type}
+              onClick={() => {
+                switch (block.type) {
+                  case "text": {
+                    dispatch({
+                      type: "blocks/new",
+                      block: "typography",
+                    });
+                    return;
+                  }
+                }
+                dispatch({
+                  type: "blocks/new",
+                  // @ts-ignore TODO: handle presets
+                  block: block.type,
+                });
+              }}
+            >
+              {block.label}
+            </button>
+          ))}
+          <div className="bg-red-50 w-10 h-10" />
+        </DialogContent>
+      </Dialog>
       <GridGuide col={6} row={12} />
       <Grid col={6} row={12}>
-        <GridAreaBlock x={[1, 7]} y={[4, 8]} z={1} debug>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor
-          dui eu risus sodales scelerisque. Pellentesque dui neque, scelerisque
-          in faucibus a, ullamcorper sed orci. Etiam libero urna, sodales luctus
-          lobortis et, porta in tellus.
-        </GridAreaBlock>
-        <GridAreaBlock x={[1, 7]} y={[12, 9]} z={2}>
-          Lorem ipsum dolor sit amet
-        </GridAreaBlock>
+        {state.blocks.map((block) => (
+          <GridAreaBlock x={block.x} y={block.y} z={block.z} key={block.id}>
+            {GridaBlockRenderer(block.element)}
+          </GridAreaBlock>
+        ))}
       </Grid>
     </div>
   );
 }
 
-const gridpos = (col: number, row: number, i: number) => {
+function GridaBlockRenderer(block: GridaBlock) {
+  const { type } = block;
+  switch (type) {
+    case "image":
+      const { src } = block as GridaGridImageBlock;
+      return <ImageBlock src={src} />;
+    case "typography":
+      const { element, data } = block as GridaGridTypographyBlock;
+      return React.createElement(element, {}, data);
+    case "button":
+      const { label } = block as GridaGridButtonBlock;
+      return <Button>{label}</Button>;
+    // case 'video'
+  }
+}
+
+const gridpos = (col: number, row: number, i: number): Position => {
   const x = i % col;
   const y = Math.floor(i / col);
   return [x, y] as const;
@@ -54,45 +393,93 @@ function GridGuide({
   //     backgroundSize: "71px 71px",
   //   }}
   // ></div>
+
+  const [state, dispatch] = useGrid();
+
+  return (
+    <>
+      <div
+        className="w-full h-full z-10"
+        style={{
+          position: "absolute",
+          display: "grid",
+          gridTemplateColumns: `repeat(${col}, 1fr)`,
+          gridTemplateRows: `repeat(${row}, 1fr)`,
+          // height: "calc(var(--scale-factor) * 750)",
+          // width: "calc(var(--scale-factor) * 375)",
+          margin: "0px auto",
+          border: "1px solid rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        {Array.from({ length: col * row }).map((_, i) => (
+          <Cell pos={gridpos(col, row, i)} key={i} index={i} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function Cell({ pos, index }: { pos: Position; index: number }) {
+  const [state, dispatch] = useGrid();
+  const [col, row] = state.size;
+  const [_mx1, _my1, _mx2, _my2] = state.marquee ?? [-1, -1, -1, -1];
+
+  const bind = useGesture(
+    {
+      onPointerDown: () => {
+        dispatch({ type: "pointerdown" });
+      },
+      onPointerUp: () => {
+        dispatch({ type: "pointerup" });
+      },
+      onPointerMove: () => {
+        dispatch({ type: "pointermove", pos: pos });
+      },
+    },
+    {
+      drag: {
+        enabled: false,
+      },
+    }
+  );
+
+  const is_in_marquee =
+    pos[0] >= _mx1 && pos[0] <= _mx2 && pos[1] >= _my1 && pos[1] <= _my2;
+
   return (
     <div
-      className="w-full h-full z-10"
+      {...bind()}
+      data-marqueed={is_in_marquee}
+      className="hover:bg-pink-200/15 data-[marqueed='true']:bg-pink-200/15"
       style={{
-        position: "absolute",
-        display: "grid",
-        gridTemplateColumns: `repeat(${col}, 1fr)`,
-        gridTemplateRows: `repeat(${row}, 1fr)`,
-        // height: "calc(var(--scale-factor) * 750)",
-        // width: "calc(var(--scale-factor) * 375)",
-        margin: "0px auto",
-        border: "1px solid rgba(0, 0, 0, 0.1)",
+        touchAction: "none",
+        border: "0.1px solid rgba(0, 0, 0, 0.1)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      {Array.from({ length: col * row }).map((_, i) => (
-        <div
-          key={i}
-          className="hover:bg-pink-200/15"
-          style={{
-            border: "0.1px solid rgba(0, 0, 0, 0.1)",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          {debug ? (
-            <>
-              <span className="text-xs font-mono opacity-20">
-                {gridpos(col, row, i).join(",")}
-              </span>
-            </>
-          ) : (
-            <>
-              <PlusIcon className="opacity-20" />
-            </>
-          )}
-        </div>
-      ))}
+      {state.debug ? (
+        <>
+          <span className="text-xs font-mono opacity-20">
+            {gridpos(col, row, index).join(",")}
+          </span>
+        </>
+      ) : (
+        <>
+          <PlusIcon className="opacity-20" />
+        </>
+      )}
     </div>
+  );
+}
+
+function ImageBlock({ src }: { src: string }) {
+  return (
+    <picture className="w-full h-full not-prose">
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <img className="object-cover w-full h-full" src={src} />
+    </picture>
   );
 }
 
