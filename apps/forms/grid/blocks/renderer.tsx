d53a0b1cf674090540a988899ab7c2eeca98a6a8
Universe <universe@grida.co>
2024-05-19 14:22:52 +0900

    block style

--- apps/forms/grid/blocks/renderer.tsx
+++ apps/forms/grid/blocks/renderer.tsx
@@ -61,8 +61,8 @@ function GridaGridTypographyBlock({ element, data }: GridaGridTypographyBlock) {
 
 function GridaGridButtonBlock({ label }: GridaGridButtonBlock) {
   return (
-    <div className="flex w-full h-full items-center justify-center">
-      <Button>{label}</Button>
+    <div className="flex w-full h-full items-center justify-center px-4">
+      <Button className="w-full">{label}</Button>
     </div>
   );
 }
