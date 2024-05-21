befd712e2b3f6a40d5cdd363a48ca1b5a9cd847d
Universe <universe@grida.co>
2024-05-22 00:56:29 +0900

    alpha disable store

--- apps/forms/app/(d)/d/[id]/connect/store/products/page.tsx
+++ apps/forms/app/(d)/d/[id]/connect/store/products/page.tsx
@@ -7,31 +7,31 @@ import { AlphaDisabledFeature } from "@/components/beta/comming-soon-overlay";
 
 export default function StoreProductsPage() {
   return (
-    // <AlphaDisabledFeature>
-    <main className="h-full flex flex-col">
-      <Tabs className="h-full" defaultValue="products">
-        <TabsList>
-          <TabsTrigger value="products">Products</TabsTrigger>
-          <TabsTrigger value="inventory">Inventory</TabsTrigger>
-          <TabsTrigger value="orders">Orders</TabsTrigger>
-        </TabsList>
-        <TabsContent className="w-full h-full" value="products">
-          <div className="h-full flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
-            <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-2xl font-bold tracking-tight">
-                You have no products
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                Let&apos;s start by adding your first product
-              </p>
-              <Link href="./products/new">
-                <Button className="mt-4">Add Product</Button>
-              </Link>
+    <AlphaDisabledFeature>
+      <main className="h-full flex flex-col">
+        <Tabs className="h-full" defaultValue="products">
+          <TabsList>
+            <TabsTrigger value="products">Products</TabsTrigger>
+            <TabsTrigger value="inventory">Inventory</TabsTrigger>
+            <TabsTrigger value="orders">Orders</TabsTrigger>
+          </TabsList>
+          <TabsContent className="w-full h-full" value="products">
+            <div className="h-full flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+              <div className="flex flex-col items-center gap-1 text-center">
+                <h3 className="text-2xl font-bold tracking-tight">
+                  You have no products
+                </h3>
+                <p className="text-sm text-muted-foreground">
+                  Let&apos;s start by adding your first product
+                </p>
+                <Link href="./products/new">
+                  <Button className="mt-4">Add Product</Button>
+                </Link>
+              </div>
             </div>
-          </div>
-        </TabsContent>
-      </Tabs>
-    </main>
-    // </AlphaDisabledFeature>
+          </TabsContent>
+        </Tabs>
+      </main>
+    </AlphaDisabledFeature>
   );
 }
