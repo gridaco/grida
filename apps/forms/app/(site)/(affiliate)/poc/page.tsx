b65fde069fd08e88ea1bee15689f5f600bbbdea2
ryujunghy3on <ryu@grida.co>
2024-05-27 17:44:23 +0900

    init affiliate

---
+++ apps/forms/app/(site)/(affiliate)/poc/page.tsx
@@ -0,0 +1,18 @@
+export default function PartnerPOC() {
+  return (
+    <main>
+      {/*  */}
+      ....
+      <header>
+        {/*  */}
+        ...
+      </header>
+      <div className="container mx-auto bg-gray-300">
+        <section>
+          {/*  */}
+          ...
+        </section>
+      </div>
+    </main>
+  );
+}
