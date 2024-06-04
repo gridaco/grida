31496bab962ef144a24c8ceff2148f98eb16eae7
Universe <universe@grida.co>
2024-06-05 01:31:56 +0900

    supabase connect page init

--- apps/forms/app/(d)/d/[id]/connect/database/supabase/page.tsx
+++ apps/forms/app/(d)/d/[id]/connect/database/supabase/page.tsx
@@ -1,11 +1,7 @@
 export default function ConnectDB() {
   return (
     <main className="max-w-2xl mx-auto">
-      <ul>
-        <li>Postgres</li>
-        <li>Supabase</li>
-        <li>Firebase</li>
-      </ul>
+      <h1>Connect Supabase</h1>
     </main>
   );
 }
