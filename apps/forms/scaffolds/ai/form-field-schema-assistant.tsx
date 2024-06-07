68d720a15af94b2bb49c22d8a710a20be22dd380
Universe <universe@grida.co>
2024-06-07 23:21:56 +0900

    update ui

--- apps/forms/scaffolds/ai/form-field-schema-assistant.tsx
+++ apps/forms/scaffolds/ai/form-field-schema-assistant.tsx
@@ -2,8 +2,11 @@
 
 import React, { useEffect, useState } from "react";
 import { FormFieldInit } from "@/types";
-import { LightningBoltIcon } from "@radix-ui/react-icons";
+import { LightningBoltIcon, MagicWandIcon } from "@radix-ui/react-icons";
 import { draftid } from "@/utils/id";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
 export function FormFieldAssistant({
   onSuggestion,
@@ -71,47 +74,25 @@ export function FormFieldAssistant({
           Ask AI
         </span>
       </div>
-      <textarea
-        autoFocus
-        className="block p-2.5 w-full text-sm text-neutral-900 bg-neutral-50 rounded-lg border border-neutral-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-neutral-700 dark:border-neutral-600 dark:placeholder-neutral-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        value={description}
-        placeholder="Describe the field..."
-        onChange={(e) => setDescription(e.target.value)}
-        onKeyDown={onKeyDown}
-        rows={4}
-        disabled={isLoading}
-      />
-      <button
-        className={`mt-3 w-full inline-flex justify-center items-center gap-2 rounded-md p-2 text-white ${isLoading ? "bg-neutral-400" : "bg-blue-600 hover:bg-blue-700"} focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`}
-        onClick={assist}
-        disabled={isLoading}
-      >
-        {isLoading ? (
-          <svg
-            className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              className="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              strokeWidth="4"
-            ></circle>
-            <path
-              className="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            ></path>
-          </svg>
-        ) : (
-          <LightningBoltIcon className="w-5 h-5" />
-        )}
-        Generate
-      </button>
+      <div className="grid gap-3">
+        <Textarea
+          autoFocus
+          value={description}
+          placeholder="Describe the field..."
+          onChange={(e) => setDescription(e.target.value)}
+          onKeyDown={onKeyDown}
+          rows={4}
+          disabled={isLoading}
+        />
+        <Button className={`w-full`} onClick={assist} disabled={isLoading}>
+          {isLoading ? (
+            <Spinner className="w-5 h-5 fill-background text-foreground" />
+          ) : (
+            <MagicWandIcon className="w-5 h-5" />
+          )}
+          <span className="ml-2">Generate</span>
+        </Button>
+      </div>
     </div>
   );
 }
