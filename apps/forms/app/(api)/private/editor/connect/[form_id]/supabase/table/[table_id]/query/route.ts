f83fddd5379b28934ef56e9d0ad0e415a6c6e8b4
Universe <universe@grida.co>
2024-06-26 02:14:57 +0900

    refactor query utils

--- apps/forms/app/(api)/private/editor/connect/[form_id]/supabase/table/[table_id]/query/route.ts
+++ apps/forms/app/(api)/private/editor/connect/[form_id]/supabase/table/[table_id]/query/route.ts
@@ -13,49 +13,24 @@ import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  req: NextRequest,
-  context: {
-    params: {
-      form_id: string;
-      table_id: string;
-    };
-  }
-) {
-  const { form_id, table_id: _table_id } = context.params;
-  const table_id = parseInt(_table_id);
-  const cookieStore = cookies();
-  const grida_forms_client = createRouteHandlerClient(cookieStore);
+type Context = {
+  params: {
+    form_id: string;
+    table_id: string;
+  };
+};
 
+export async function GET(req: NextRequest, context: Context) {
   const _q_limit = req.nextUrl.searchParams.get("limit");
   const limit = _q_limit ? parseInt(_q_limit) : undefined;
+  const { form_id, table_id: _table_id } = context.params;
+  const table_id = parseInt(_table_id);
 
-  const { data, error } = await grida_forms_client
-    .from("form")
-    .select(`id, supabase_connection:connection_supabase(*)`)
-    .eq("id", form_id)
-    .single();
-
-  if (!data) {
-    return notFound();
-  }
-
-  const { supabase_connection } = data;
-  assert(supabase_connection, "supabase_connection is required");
-
-  const x = new GridaXSupabaseClient();
-  const conn = await x.getConnection(supabase_connection);
-  assert(conn, "connection fetch failed");
-  const { main_supabase_table } = conn;
-
-  assert(main_supabase_table?.id === table_id, "only supports main table atm");
-
-  const x_client = await createXSupabaseClient(
-    supabase_connection.supabase_project_id,
-    {
-      service_role: true,
-    }
-  );
+  const { main_supabase_table, x_client } =
+    await get_forms_x_supabase_table_connector({
+      form_id,
+      table_id,
+    });
 
   const query = new XSupabaseQueryBuilder(x_client);
 
@@ -68,17 +43,49 @@ export async function GET(
   return NextResponse.json(res);
 }
 
-export async function DELETE(
-  req: NextRequest,
-  context: {
-    params: {
-      form_id: string;
-      table_id: string;
-    };
-  }
-) {
+export async function PATCH(req: NextRequest, context: Context) {
   const { form_id, table_id: _table_id } = context.params;
   const table_id = parseInt(_table_id);
+
+  const { main_supabase_table, x_client } =
+    await get_forms_x_supabase_table_connector({
+      form_id,
+      table_id,
+    });
+
+  // TODO:
+}
+
+export async function DELETE(req: NextRequest, context: Context) {
+  const { form_id, table_id: _table_id } = context.params;
+  const table_id = parseInt(_table_id);
+
+  const { main_supabase_table, x_client } =
+    await get_forms_x_supabase_table_connector({
+      form_id,
+      table_id,
+    });
+
+  const q: ReadonlyArray<XSupabaseQuery.Filter> = await req.json();
+
+  const query = new XSupabaseQueryBuilder(x_client);
+
+  const res = await query
+    .from(main_supabase_table.sb_table_name)
+    .delete()
+    .fromFilters(q)
+    .done();
+
+  return NextResponse.json(res);
+}
+
+async function get_forms_x_supabase_table_connector({
+  form_id,
+  table_id,
+}: {
+  form_id: string;
+  table_id: number;
+}) {
   const cookieStore = cookies();
   const grida_forms_client = createRouteHandlerClient(cookieStore);
 
@@ -109,15 +116,5 @@ export async function DELETE(
     }
   );
 
-  const q: ReadonlyArray<XSupabaseQuery.Filter> = await req.json();
-
-  const query = new XSupabaseQueryBuilder(x_client);
-
-  const res = await query
-    .from(main_supabase_table.sb_table_name)
-    .delete()
-    .fromFilters(q)
-    .done();
-
-  return NextResponse.json(res);
+  return { main_supabase_table, x_client };
 }
