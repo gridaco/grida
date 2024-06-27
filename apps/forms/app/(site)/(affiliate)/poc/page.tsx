241ce8dcdd549d8aa284bc3b280b43c54b6c1bfb
ryujunghy3on <ryu@grida.co>
2024-06-27 21:21:21 +0900

    maincard

--- apps/forms/app/(site)/(affiliate)/poc/page.tsx
+++ apps/forms/app/(site)/(affiliate)/poc/page.tsx
@@ -27,32 +27,37 @@ const demo_2_categories = [
 export default function PartnerPOC() {
   return (
     <main>
-      <header className="p-10 w-full">
-        <div className="flex justify-end w-full">
-          <button className="px-8 py-2 rounded bg-black font-semibold text-white dark:invert">
-            시작하기
-          </button>
-        </div>
-      </header>
-      <div>
-        <section className="min-h-screen flex">
-          {/*  */}
-          <div className="flex-1 w-full flex items-center justify-center">
-            <div className="flex flex-col items-center gap-10">
-              <div className="flex items-center justify-center gap-2">
-                <GridaLogo />
-                <span className="font-bold text-lg">Affiliate</span>
-              </div>
-              <h1 className="text-4xl font-extrabold text-center">
-                POC와 함께하는 <br />
-                편리한 이벤트 준비
-              </h1>
-              <button className=" px-8 py-2 rounded-full bg-black font-semibold text-white dark:invert">
-                문의하기
+      <ImageGrid className="z-0" />
+      <div className=" relative z-10">
+        <div className="py-10 px-20 w-full">
+          <header>
+            <div className="flex justify-end w-full">
+              <button className="px-8 py-2 rounded bg-black font-semibold text-white dark:invert">
+                시작하기
               </button>
             </div>
+          </header>
+          <div>
+            <section className="min-h-screen flex">
+              {/*  */}
+              <div className="flex-1 flex items-center justify-center sm:justify-start">
+                <div className="flex flex-col items-center sm:items-start gap-10">
+                  <div className="flex items-center justify-center gap-2">
+                    <GridaLogo />
+                    <span className="font-bold text-lg">Affiliate</span>
+                  </div>
+                  <h1 className="text-4xl font-extrabold text-start">
+                    POC와 함께하는 <br />
+                    편리한 이벤트 준비
+                  </h1>
+                  <button className=" px-8 py-2 rounded-full bg-black font-semibold text-white dark:invert">
+                    문의하기
+                  </button>
+                </div>
+              </div>
+            </section>
           </div>
-        </section>
+        </div>
         <section className="container my-40">
           <div className="gap-10 flex flex-col items-center justify-center">
             <h2 className=" text-lg font-bold">POC와 함께한 브랜드</h2>
@@ -380,3 +385,54 @@ function SolutionCard({
     </div>
   );
 }
+
+const imageUrls = [
+  [
+    "/affiliate/poc/images/main-card-image-1.png",
+    "/affiliate/poc/images/main-card-image-2.png",
+  ],
+  [
+    "/affiliate/poc/images/main-card-image-3.png",
+    "/affiliate/poc/images/main-card-image-4.png",
+    "/affiliate/poc/images/main-card-image-5.png",
+  ],
+  [
+    "/affiliate/poc/images/main-card-image-1.png",
+    "/affiliate/poc/images/main-card-image-2.png",
+  ],
+];
+
+const ImageGrid = ({ className = "" }) => {
+  return (
+    <div
+      aria-hidden="true"
+      className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
+    >
+      <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-[250px]">
+        <div className="flex items-center space-x-6 lg:space-x-8">
+          {imageUrls.map((column, colIndex) => (
+            <div
+              key={colIndex}
+              className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8"
+            >
+              {column.map((src, index) => (
+                <div
+                  key={index}
+                  className=" h-96 w-72 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100"
+                >
+                  <Image
+                    src={src}
+                    alt=""
+                    className="h-full w-full object-cover object-center"
+                    width={320}
+                    height={520}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
