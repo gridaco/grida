44876d4d1ace86df6dbb3d7a8e23cffdb9745f11
ryujunghy3on <ryu@grida.co>
2024-07-03 20:08:27 +0900

    .

--- apps/forms/app/(site)/(affiliate)/poc/demo.tsx
+++ apps/forms/app/(site)/(affiliate)/poc/demo.tsx
@@ -96,7 +96,7 @@ function DemoSubCard({
     <div className={className}>
       <div className="flex flex-col gap-3 p-8">
         <h6 className="text-4xl font-bold">{text1}</h6>
-        <span className="max-w-sm text-muted-foreground">{text2}</span>
+        <span className="max-w-sm text-sm text-muted-foreground">{text2}</span>
       </div>
       <Image
         className="absolute inset-0 overflow-hidden object-right-bottom object-cover w-full h-full -z-10"
