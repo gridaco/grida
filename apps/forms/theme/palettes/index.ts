da55f4227af1383e930764ea377d42d27313a877
Universe <universe@grida.co>
2024-07-05 22:46:43 +0900

    add high constrast palettes

--- apps/forms/theme/palettes/index.ts
+++ apps/forms/theme/palettes/index.ts
@@ -10,7 +10,14 @@ import stone from "./stone";
 import violet from "./violet";
 import yellow from "./yellow";
 import zinc from "./zinc";
+import * as hc from "./v-highcontrast";
 
+const highcontrast_blue = hc.blue;
+const highcontrast_green = hc.green;
+const highcontrast_violet = hc.violet;
+const highcontrast_orange = hc.orange;
+
+// NOTE: do not change the export name as this name is saved in the database
 export {
   blue,
   gray,
@@ -24,4 +31,9 @@ export {
   violet,
   yellow,
   zinc,
+  // hc
+  highcontrast_blue,
+  highcontrast_green,
+  highcontrast_orange,
+  highcontrast_violet,
 };
---
+++ apps/forms/theme/palettes/v-highcontrast/index.ts
@@ -0,0 +1,27 @@
+import blue from "./blue";
+// import gray from "./gray";
+import green from "./green";
+// import neutral from "./neutral";
+import orange from "./orange";
+// import red from "./red";
+// import rose from "./rose";
+// import slate from "./slate";
+// import stone from "./stone";
+import violet from "./violet";
+// import yellow from "./yellow";
+// import zinc from "./zinc";
+
+export {
+  blue,
+  // gray,
+  green,
+  // neutral,
+  orange,
+  // red,
+  // rose,
+  // slate,
+  // stone,
+  violet,
+  // yellow,
+  // zinc,
+};
