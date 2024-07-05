90660eae35771ce61d70ce6699b3e66226cc4aa3
Universe <universe@grida.co>
2024-07-05 19:51:19 +0900

    fix scrolling

--- apps/forms/app/(d)/d/[id]/blocks/page.tsx
+++ apps/forms/app/(d)/d/[id]/blocks/page.tsx
@@ -7,13 +7,15 @@ export default async function EditFormPage() {
       <aside className="hidden lg:flex h-full">
         <Siebar mode="blocks" />
       </aside>
-      <div className="py-4 px-8 md:py-10 md:px-16 container mx-auto max-w-screen-md">
-        <BlocksEditor />
-        <div
-          style={{
-            height: 100,
-          }}
-        />
+      <div className="overflow-y-auto w-full">
+        <div className="py-4 px-8 md:py-10 md:px-16 container mx-auto max-w-screen-md">
+          <BlocksEditor />
+          <div
+            style={{
+              height: 100,
+            }}
+          />
+        </div>
       </div>
     </main>
   );
