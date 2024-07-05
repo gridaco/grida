27d578e8fe1dfacb9f4e3c4a036b1c49fd5f4173
Universe <universe@grida.co>
2024-07-05 23:38:36 +0900

    variants mapping

--- apps/forms/scaffolds/settings/custom-page-palette-preference.tsx
+++ apps/forms/scaffolds/settings/custom-page-palette-preference.tsx
@@ -10,6 +10,7 @@ import {
 import { FormStyleSheetV1Schema } from "@/types";
 import { Button } from "@/components/ui/button";
 import palettes from "@/theme/palettes";
+import * as variants from "@/theme/palettes";
 import clsx from "clsx";
 import { Badge } from "@/components/ui/badge";
 import { useEditorState } from "../editor";
@@ -51,7 +52,44 @@ export function CustomPagePalettePreferences({
               <Badge variant="outline">{palette}</Badge>
             </div>
           )}
-          <div className="flex flex-wrap gap-1">
+          <div className="flex flex-col gap-4">
+            {Object.keys(variants).map((variant) => {
+              const palettes = variants[variant as keyof typeof variants];
+              return (
+                <>
+                  <h2 className="text-sm font-mono text-muted-foreground">
+                    {variant}
+                  </h2>
+                  <div className="flex flex-wrap gap-1">
+                    {Object.keys(palettes).map((key) => {
+                      const colors = palettes[key as keyof typeof palettes];
+                      const primary: any = colors["light"]["--primary"];
+                      return (
+                        <button
+                          type="button"
+                          key={key}
+                          data-selected={key === palette}
+                          onClick={() => {
+                            setPalette(key as keyof typeof palettes);
+                            // onPresetChange?.(key);
+                          }}
+                          className={clsx(
+                            "w-6 h-6 border-2 rounded-full",
+                            "data-[selected='true']:outline data-[selected='true']:outline-foreground data-[selected='true']:border-background"
+                          )}
+                          style={{
+                            backgroundColor: `hsl(${primary.h}, ${primary.s}%, ${primary.l}%)`,
+                          }}
+                        />
+                      );
+                    })}
+                  </div>
+                </>
+              );
+            })}
+          </div>
+
+          {/* <div className="flex flex-wrap gap-1">
             {Object.keys(palettes).map((key) => {
               const colors = palettes[key as keyof typeof palettes];
               const primary = colors["light"]["--primary"];
@@ -74,7 +112,7 @@ export function CustomPagePalettePreferences({
                 />
               );
             })}
-          </div>
+          </div> */}
         </form>
         {/* {palette && (
           <div className="mt-4 flex items-center justify-center select-none">
