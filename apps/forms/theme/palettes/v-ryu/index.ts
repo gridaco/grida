2a9f6436e866a630ce08f3536ee587d32add1eba
ryujunghy3on <ryu@grida.co>
2024-07-09 02:17:20 +0900

    .

--- apps/forms/theme/palettes/v-ryu/index.ts
+++ apps/forms/theme/palettes/v-ryu/index.ts
@@ -1,4 +1,5 @@
 import _001_teal from "./001-teal";
 import _002_soft_nuetrals from "./002-soft-neutrals";
+import _003_lemon_blue from "./003-lemon-blue";
 
-export { _001_teal, _002_soft_nuetrals };
+export { _001_teal, _002_soft_nuetrals, _003_lemon_blue };
