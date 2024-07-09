bf6bebaf9937e71e59ef5cb80a829b6be341dfb4
Universe <universe@grida.co>
2024-07-09 19:31:47 +0900

    add basic section style

--- apps/forms/theme/section/css.ts
+++ apps/forms/theme/section/css.ts
@@ -1,2 +1,5 @@
+export const section_style_basic =
+  "p-5 md:p-8 bg-background rounded-xl bg-clip-padding border shadow-lg";
+
 export const section_style_glass_morphism =
   "p-5 md:p-8 bg-background/15 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-lg border shadow-lg";
--- apps/forms/theme/section/index.ts
+++ apps/forms/theme/section/index.ts
@@ -1,6 +1,10 @@
-import { section_style_glass_morphism } from "./css";
+import { section_style_basic, section_style_glass_morphism } from "./css";
 
 export const sections = [
+  {
+    name: "Basic",
+    css: section_style_basic,
+  },
   {
     name: "Glass Morphism",
     css: section_style_glass_morphism,
