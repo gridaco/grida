6ba501c1a04bab53d7bd7aa260f14350edb17a89
Universe <universe@grida.co>
2024-07-10 00:32:33 +0900

    fix: isolate csstree

--- apps/forms/theme/palettes/utils/parser.ts
+++ apps/forms/theme/palettes/utils/parser.ts
@@ -1,46 +1,4 @@
-import { Palette, Theme } from "./types";
 import * as csstree from "css-tree";
-import { z } from "zod";
-
-export function stringfyThemeVariables(theme: z.infer<typeof Theme>) {
-  // Convert values
-
-  const { light, dark } = theme;
-
-  // Construct the CSS
-  const css_1 = stringfyPalette(light);
-  const css_2 = stringfyPalette(dark);
-  return `:root {
-${indent(css_1)}
-}
-
-.dark {
-${indent(css_2)}
-}`;
-}
-
-const indent = (str: string) =>
-  str
-    .split("\n")
-    .map((line) => `  ${line}`)
-    .join("\n");
-
-const stringifyHSL = (value: any) => {
-  return `${value.h} ${value.s}% ${value.l}%`;
-};
-
-const stringifyREM = (value: any) => value;
-
-export function stringfyPalette(palette: z.infer<typeof Palette>) {
-  return Object.entries(palette)
-    .map(([key, value]) => {
-      if (key === "--radius") {
-        return `${key}: ${stringifyREM(value)};`;
-      }
-      return `${key}: ${stringifyHSL(value)};`;
-    })
-    .join("\n");
-}
 
 export function parseThemeVariables(css: string) {
   // Parse CSS
