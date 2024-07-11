56dcdabe753c587e62cfc4edb62c1ae571c61440
Universe <universe@grida.co>
2024-07-11 18:09:08 +0900

    add cards

--- apps/forms/theme/templates/formcollection/page.tsx
+++ apps/forms/theme/templates/formcollection/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
 import React from "react";
 import { cn } from "@/utils";
+import { SlashIcon } from "@radix-ui/react-icons";
 
 export default function FormCollectionPage() {
   return (
@@ -45,7 +46,7 @@ export default function FormCollectionPage() {
             <div className="grid gap-6 grid-cols-1 @3xl/preview:grid-cols-2 @5xl/preview:grid-cols-3 @7xl/preview:grid-cols-4">
               {[0, 1, 2, 3].map((i) => (
                 <TemplateComponent key={i}>
-                  <Card_001 key={i} />
+                  <Card_002 key={i} />
                 </TemplateComponent>
               ))}
             </div>
@@ -93,6 +94,46 @@ function Card_001() {
         <div className="text-sm text-muted-foreground">
           Hosted at The Innovation Hub
         </div>
+      </div>
+    </Card>
+  );
+}
+
+function Card_002() {
+  return (
+    <Card className="relative overflow-hidden flex-1 flex flex-col justify-end gap-6 text-foreground w-auto aspect-[4/4]">
+      <Image
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        src="/images/abstract-placeholder.jpg"
+        alt={""}
+        layout="fill"
+      />
+      <div
+        className="absolute bottom-0 left-0 w-full h-1/2 z-10"
+        style={{
+          background:
+            "linear-gradient(to top, hsl(var(--background)), transparent)",
+        }}
+      />
+      <div className="absolute top-0 left-0 py-4 px-4">
+        <span className="bg-background backdrop-blur-md rounded-md px-2 py-1 uppercase text-sm font-bold">
+          <>Opens 1:11</>
+        </span>
+      </div>
+      <div className="flex flex-col gap-1 z-20 py-8 px-4 pr-10">
+        <div className="flex flex-row items-center gap-2">
+          <div className="flex gap-2 items-center justify-between">
+            <span>24.1.02</span>
+          </div>
+          <SlashIcon />
+          <span>
+            <strong>200</strong>
+          </span>
+        </div>
+        <h1 className="text-3xl font-bold break-keep max-w-[80%]">
+          Tech meetup
+        </h1>
+        <p className="text-xs font-regular opacity-80">register now</p>
       </div>
     </Card>
   );
