f09569fba5323186495c79f27c0e630b0818c632
Universe <universe@grida.co>
2024-08-13 18:54:02 +0900

    workspace provider

---
+++ apps/forms/app/(workbench)/[org]/layout.tsx
@@ -0,0 +1,38 @@
+import {
+  createServerComponentClient,
+  createServerComponentWorkspaceClient,
+} from "@/lib/supabase/server";
+import { Workspace } from "@/scaffolds/workspace";
+import { cookies } from "next/headers";
+import { notFound, redirect } from "next/navigation";
+
+export default async function Layout({
+  params,
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+  params: { org: string };
+}>) {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient(cookieStore);
+  const wsclient = createServerComponentWorkspaceClient(cookieStore);
+
+  const { data: auth } = await supabase.auth.getUser();
+
+  if (!auth.user) {
+    return redirect("/sign-in");
+  }
+
+  const { data: organization, error: err } = await wsclient
+    .from("organization")
+    .select(`*`)
+    .eq("name", params.org)
+    .single();
+
+  if (err) console.error(err);
+  if (!organization) {
+    return notFound();
+  }
+
+  return <Workspace organization={organization}>{children}</Workspace>;
+}
