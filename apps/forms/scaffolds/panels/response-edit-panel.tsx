664d087f9752789ec5aff6b71261dd7246e14b21
Universe <universe@grida.co>
2024-08-20 17:46:28 +0900

    update response edit panel with tables

--- apps/forms/scaffolds/panels/response-edit-panel.tsx
+++ apps/forms/scaffolds/panels/response-edit-panel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import {
   PanelClose,
   PanelContent,
@@ -24,6 +24,17 @@ import { fmt_local_index } from "@/utils/fmt";
 import { useTheme } from "next-themes";
 import { useMonacoTheme } from "@/components/monaco";
 import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { UAParser } from "ua-parser-js";
+import { AvatarIcon } from "@radix-ui/react-icons";
+import { useEditorState } from "../editor";
 
 export function RowEditPanel({
   onSave,
@@ -37,6 +48,7 @@ export function RowEditPanel({
   }>;
   onSave?: (field: FormFieldInit) => void;
 }) {
+  const [state, dispatch] = useEditorState();
   const { response, response_fields, field_defs } = init ?? {};
 
   const { resolvedTheme } = useTheme();
@@ -46,6 +58,18 @@ export function RowEditPanel({
 
   const onSaveClick = () => {};
 
+  const onViewCustomerDetailsClick = useCallback(
+    (customer_id: string) => {
+      dispatch({
+        type: "editor/customers/edit",
+        customer_id: customer_id,
+      });
+    },
+    [dispatch]
+  );
+
+  if (!response) return <></>;
+
   return (
     <SidePanel {...props}>
       <PanelHeader>{`Response ${init?.response?.local_index ? fmt_local_index(init.response?.local_index) : ""}`}</PanelHeader>
@@ -53,41 +77,32 @@ export function RowEditPanel({
         <PanelPropertySection>
           <PanelPropertySectionTitle>General</PanelPropertySectionTitle>
           <PanelPropertyFields>
-            <PanelPropertyField label={"Local Index"}>
-              <PropertyTextInput value={response?.local_index} disabled />
-            </PanelPropertyField>
-            <PanelPropertyField label={"IDX"}>
-              <PropertyTextInput
-                value={fmt_local_index(response?.local_index ?? NaN)}
-                disabled
-              />
-            </PanelPropertyField>
-            <PanelPropertyField label={"Local ID"}>
-              <PropertyTextInput value={response?.local_id ?? ""} disabled />
-            </PanelPropertyField>
-            <PanelPropertyField label={"UUID"}>
-              <PropertyTextInput value={response?.id} disabled />
-            </PanelPropertyField>
-            <PanelPropertyField label={"Created At"}>
-              <PropertyTextInput value={response?.created_at} disabled />
-            </PanelPropertyField>
+            {response && (
+              <>
+                <ResponseIdTable {...response} />
+                <ResponsePropertiesTable {...response} />
+              </>
+            )}
           </PanelPropertyFields>
         </PanelPropertySection>
         <PanelPropertySection>
           <PanelPropertySectionTitle>Customer</PanelPropertySectionTitle>
           <PanelPropertyFields>
-            <PanelPropertyField label={"UUID"}>
-              <PropertyTextInput value={response?.customer_id ?? ""} disabled />
-            </PanelPropertyField>
-            <PanelPropertyField label={"Browser"}>
-              <PropertyTextInput value={response?.browser ?? ""} disabled />
-            </PanelPropertyField>
-            <PanelPropertyField label={"Platform"}>
-              <PropertyTextInput
-                value={response?.platform_powered_by ?? ""}
-                disabled
-              />
-            </PanelPropertyField>
+            {response.customer_id && (
+              <>
+                <ResponseCustomerMetaTable {...response} />
+                <Button
+                  type="button"
+                  onClick={() =>
+                    onViewCustomerDetailsClick(response.customer_id!)
+                  }
+                  variant="secondary"
+                >
+                  <AvatarIcon className="me-2 inline-flex align-middle" />
+                  More about this customer
+                </Button>
+              </>
+            )}
           </PanelPropertyFields>
         </PanelPropertySection>
         <form>
@@ -118,7 +133,7 @@ export function RowEditPanel({
         <PanelPropertySection>
           <PanelPropertySectionTitle>RAW</PanelPropertySectionTitle>
           <PanelPropertyFields>
-            <PanelPropertyField label={"JSON"}>
+            <PanelPropertyField label={"raw.json (readonly)"}>
               <Editor
                 className="rounded overflow-hidden shadow-sm border"
                 height={400}
@@ -151,5 +166,170 @@ export function RowEditPanel({
         </button> */}
       </PanelFooter>
     </SidePanel>
+  );
+}
+
+function ResponseCustomerMetaTable({
+  customer_id,
+  platform_powered_by,
+  x_ipinfo,
+  x_useragent,
+}: Pick<
+  FormResponse,
+  "x_useragent" | "customer_id" | "platform_powered_by" | "x_ipinfo"
+>) {
+  const ua = useMemo(() => {
+    return x_useragent ? new UAParser(x_useragent).getResult() : undefined;
+  }, []);
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Property</TableHead>
+          <TableHead>Value</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody className="font-mono prose prose-sm dark:prose-invert prose-pre:my-1">
+        <TableRow>
+          <TableCell>
+            <code>customer_id</code>
+          </TableCell>
+          <TableCell>
+            <pre>{customer_id}</pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>client</code>
+          </TableCell>
+          <TableCell>
+            <pre>{platform_powered_by}</pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>browser</code>
+          </TableCell>
+          <TableCell>
+            <pre>{ua?.browser.name}</pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>device</code>
+          </TableCell>
+          <TableCell>
+            <pre>
+              {ua?.device.vendor} / {ua?.device.model}
+            </pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>os</code>
+          </TableCell>
+          <TableCell>
+            <pre>
+              {ua?.os.name} / {ua?.os.version}
+            </pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>location</code>
+          </TableCell>
+          <TableCell>
+            <pre>
+              {x_ipinfo?.country} / {x_ipinfo?.city}
+            </pre>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+}
+
+function ResponsePropertiesTable({
+  form_id,
+  created_at,
+}: Pick<FormResponse, "created_at" | "form_id">) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Property</TableHead>
+          <TableHead>Value</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody className="font-mono prose prose-sm dark:prose-invert prose-pre:my-1">
+        <TableRow>
+          <TableCell>
+            <code>form_id</code>
+          </TableCell>
+          <TableCell>
+            <pre>{form_id}</pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>created_at</code>
+          </TableCell>
+          <TableCell>
+            <pre>{created_at}</pre>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+}
+function ResponseIdTable({
+  id,
+  local_id,
+  local_index,
+}: Pick<FormResponse, "id" | "local_id" | "local_index">) {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Identifier</TableHead>
+          <TableHead>Value</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody className="font-mono prose prose-sm dark:prose-invert prose-pre:my-1">
+        <TableRow>
+          <TableCell>
+            <code>idx</code>
+          </TableCell>
+          <TableCell>
+            <pre>{fmt_local_index(local_index ?? NaN)}</pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>id</code>
+          </TableCell>
+          <TableCell>
+            <pre>{id}</pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>local_id</code>
+          </TableCell>
+          <TableCell>
+            <pre>{local_id ?? ""}</pre>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <code>local_index</code>
+          </TableCell>
+          <TableCell>
+            <pre>{local_index}</pre>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   );
 }
