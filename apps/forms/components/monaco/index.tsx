c8b6168d63303d3410530a0c36e5ea25ad06f09d
Universe <universe@grida.co>
2024-08-29 00:36:50 +0900

    monaco theme

--- apps/forms/components/monaco/index.tsx
+++ apps/forms/components/monaco/index.tsx
@@ -1,24 +1,29 @@
 "use client";
 
 import { useMonaco } from "@monaco-editor/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function useMonacoTheme(
   monaco: ReturnType<typeof useMonaco>,
   theme: "dark" | "light" | (string | {})
 ) {
+  const [themeloaded, setThemeLoaded] = useState(false);
   // load theme initially
   useEffect(() => {
+    if (!monaco) return;
     import("monaco-themes/themes/Blackboard.json").then((data) => {
       data.colors["editor.background"] = "#0D0D0D";
-      monaco?.editor.defineTheme("dark", data as any);
-      monaco?.editor.setTheme(theme === "dark" ? "dark" : "light");
+      monaco.editor.defineTheme("dark", data as any);
+      monaco.editor.setTheme(theme === "dark" ? "dark" : "light");
+      setThemeLoaded(true);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [monaco]);
 
   // update theme when it changes
   useEffect(() => {
-    monaco?.editor.setTheme(theme === "dark" ? "dark" : "light");
-  }, [monaco, theme]);
+    if (!themeloaded) return;
+    if (!monaco) return;
+    monaco.editor.setTheme(theme === "dark" ? "dark" : "light");
+  }, [monaco, theme, themeloaded]);
 }
