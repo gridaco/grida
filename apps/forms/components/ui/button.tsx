87eff1f4278da2672e53ceef1b41e57d34fe6b86
Universe <universe@grida.co>
2024-09-21 23:04:43 +0900

    button xs

--- apps/forms/components/ui/button.tsx
+++ apps/forms/components/ui/button.tsx
@@ -23,6 +23,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2",
+        xs: "h-6 px-2.5 text-xs",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
