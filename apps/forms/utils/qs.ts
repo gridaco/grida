664a0b3249466075fec0e2e2cff0f35b7954ba65
Universe <universe@grida.co>
2024-09-24 20:55:51 +0900

    qs omit

--- apps/forms/utils/qs.ts
+++ apps/forms/utils/qs.ts
@@ -22,3 +22,22 @@ export function queryorbody(
 ) {
   return b.searchParams.get(key) || b.body?.[key];
 }
+
+/**
+ * Removes specified keys from the given URLSearchParams object.
+ *
+ * @param {URLSearchParams} search - The URLSearchParams object to modify.
+ * @param {...string} omit - The list of keys to be removed from the URLSearchParams.
+ * @returns {URLSearchParams} - The modified URLSearchParams object with the specified keys omitted.
+ *
+ * @example
+ * const params = new URLSearchParams('foo=1&bar=2&baz=3');
+ * omit(params, 'foo', 'baz'); // Result: 'bar=2'
+ */
+export function omit(search: URLSearchParams, ...omit: string[]) {
+  // delete the keys
+  for (const key of omit) {
+    search.delete(key);
+  }
+  return search;
+}
