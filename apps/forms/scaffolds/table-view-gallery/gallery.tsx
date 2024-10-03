bb3185242e762be44a2c1707a46e8c01b18d17a9
Universe <universe@grida.co>
2024-10-04 02:17:25 +0900

    wip - card model

--- apps/forms/scaffolds/table-view-gallery/gallery.tsx
+++ apps/forms/scaffolds/table-view-gallery/gallery.tsx
@@ -1,7 +1,110 @@
 import { cn } from "@/utils";
 import { GalleryModelCard } from "./modelcard";
+import { useSchemaDefinition } from "../data-query";
+import assert from "assert";
+import {
+  CellIdentifier,
+  DataGridCellFileRefsResolver,
+  GFColumn,
+  GFResponseRow,
+} from "../grid";
+import { Label } from "@/components/ui/label";
+import { SupabasePostgRESTOpenApi } from "@/lib/supabase-postgrest";
+import { useMemo } from "react";
+import { PGSupportedColumnType } from "@/lib/supabase-postgrest/@types/pg";
+import { FormInputType } from "@/types";
+import { useFileRefs } from "../grid/providers";
+import { FileRefsStateRenderer } from "../grid/cells";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export function Gallery({ className }: { className?: string }) {
+const media_types: FormInputType[] = ["image", "video", "audio"] as const;
+
+function analyze({
+  definition,
+  columns,
+}: {
+  definition: SupabasePostgRESTOpenApi.SupabaseOpenAPIDefinitionJSONSchema;
+  columns: GFColumn[];
+}) {
+  //
+
+  const analyzed =
+    SupabasePostgRESTOpenApi.parse_supabase_postgrest_schema_definition(
+      definition
+    );
+
+  const keys = Object.keys(analyzed.properties);
+
+  const devpropertykeys = keys.filter(
+    (key) =>
+      ["uuid"].includes(analyzed.properties[key].scalar_format) ||
+      !!analyzed.properties[key].fk ||
+      analyzed.properties[key].pk
+  );
+
+  const normalpropertykeys = keys.filter(
+    (key) => devpropertykeys.indexOf(key) === -1
+  );
+
+  const virtual_columns = columns.filter((col) => {
+    return !keys.includes(col.key);
+  });
+
+  const virtual_media_columns = virtual_columns.filter((col) => {
+    return col.type && media_types.includes(col.type);
+  });
+
+  const prioritiezed_virtual_media_columns = virtual_media_columns.sort(
+    (a, b) => {
+      switch (a.type) {
+        case "video":
+          return 2;
+        case "audio":
+          return 1;
+        case "image":
+          return 0;
+        default:
+          return -1;
+      }
+    }
+  );
+
+  const primary_virtual_media_column =
+    prioritiezed_virtual_media_columns.length > 0
+      ? prioritiezed_virtual_media_columns[0]
+      : null;
+
+  return {
+    devpropertykeys,
+    normalpropertykeys,
+    primary_virtual_media_column,
+    prioritiezed_virtual_media_columns,
+    analyzed,
+  };
+}
+
+export function Gallery({
+  className,
+  rows,
+  columns,
+}: {
+  rows: GFResponseRow[];
+  columns: GFColumn[];
+  className?: string;
+}) {
+  const definition = useSchemaDefinition();
+
+  assert(definition);
+
+  const {
+    normalpropertykeys,
+    analyzed,
+    prioritiezed_virtual_media_columns,
+    primary_virtual_media_column,
+  } = useMemo(() => {
+    return analyze({ definition, columns });
+  }, [definition, columns]);
+
   return (
     <div
       className={cn(
@@ -9,12 +112,162 @@ export function Gallery({ className }: { className?: string }) {
         className
       )}
     >
-      <GalleryModelCard title={"Title"} />
-      <GalleryModelCard title={"Title"} />
-      <GalleryModelCard title={"Title"} />
-      <GalleryModelCard title={"Title"} />
-      <GalleryModelCard title={"Title"} />
-      <GalleryModelCard title={"Title"} />
+      {rows.map((row, i) => {
+        return (
+          <DynamicDataCard
+            key={i}
+            data={row}
+            primary_media_column_key={primary_virtual_media_column?.key || null}
+            media_columns={prioritiezed_virtual_media_columns}
+            properties={normalpropertykeys.map((key) => ({
+              name: key,
+              label: key,
+              format: analyzed.properties[key].scalar_format,
+            }))}
+          />
+        );
+      })}
     </div>
   );
+}
+
+function DynamicDataCard({
+  media_columns,
+  primary_media_column_key,
+  properties,
+  data,
+}: {
+  media_columns: GFColumn[];
+  primary_media_column_key: string | null;
+  properties: { name: string; label: string; format: PGSupportedColumnType }[];
+  data: GFResponseRow;
+}) {
+  const primary_media_column = media_columns.find(
+    (c) => c.key === primary_media_column_key
+  );
+
+  const primary_media = primary_media_column
+    ? data.fields[primary_media_column.key]
+    : null;
+
+  return (
+    <GalleryModelCard
+      media={
+        primary_media?.files ? (
+          <CardMediaSection
+            data={data}
+            column={primary_media_column!}
+            resolver={primary_media.files}
+          />
+        ) : undefined
+      }
+      lines={properties.map((prop) => {
+        const value = data.raw![prop.name];
+
+        return (
+          <div key={prop.name} className="flex flex-col w-full">
+            {/* <Label>{prop.label}</Label> */}
+            <span className="text-sm text-muted-foreground w-full text-ellipsis overflow-hidden whitespace-nowrap">
+              {fmtsafely(String(value), prop.format)}
+            </span>
+          </div>
+        );
+      })}
+    />
+  );
+}
+
+function CardMediaSection({
+  data,
+  column,
+  resolver,
+}: {
+  data: GFResponseRow;
+  column: GFColumn;
+  resolver?: DataGridCellFileRefsResolver;
+}) {
+  const identifier: CellIdentifier = {
+    attribute: column.key,
+    key: data.__gf_id,
+  };
+
+  switch (column.type) {
+    case "audio":
+    case "video":
+    case "image":
+      return (
+        <CardMediaImageContent
+          identifier={identifier}
+          rowdata={data.raw}
+          resolver={resolver}
+        />
+      );
+    default:
+      throw new Error(`invalid card media type "${column.type}"`);
+  }
+}
+
+function CardMediaImageContent({
+  identifier,
+  rowdata,
+  resolver,
+}: {
+  identifier: CellIdentifier;
+  rowdata: Record<string, any> | null;
+  resolver?: DataGridCellFileRefsResolver;
+}) {
+  const refs = useFileRefs(identifier, rowdata, resolver);
+
+  console.log(refs);
+
+  return (
+    <>
+      <FileRefsStateRenderer
+        refs={refs}
+        renderers={{
+          loading: <Skeleton className="w-full h-full" />,
+          error: "ERR",
+          files: (f, i) => {
+            return (
+              <figure key={i} className="w-full h-full">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={f.srcset.thumbnail}
+                  alt={f.name}
+                  className="w-full h-full overflow-hidden object-cover"
+                  loading="lazy"
+                />
+              </figure>
+            );
+          },
+        }}
+      />
+    </>
+  );
+}
+
+function fmtsafely(txt: string, format: PGSupportedColumnType) {
+  try {
+    switch (format) {
+      case "timetz":
+      case "timestamptz":
+      case "timestamp":
+      case "timestamp without time zone":
+      case "timestamp with time zone":
+      case "date":
+      case "time":
+      case "time without time zone":
+      case "time with time zone":
+        return new Date(txt).toLocaleString();
+      case "boolean":
+        return txt === "true" ? "Yes" : "No";
+      case "json":
+      case "jsonb":
+        return JSON.stringify(txt);
+    }
+
+    return txt;
+  } catch (e) {
+    return "ERR";
+  }
 }
--- apps/forms/scaffolds/table-view-gallery/modelcard.tsx
+++ apps/forms/scaffolds/table-view-gallery/modelcard.tsx
@@ -1,41 +1,47 @@
 import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 
-type Picture = {
-  src: string;
-  name: string;
-};
-
 export function GalleryModelCard({
   media,
   title,
   paragraph,
   lines,
 }: {
-  media?: Array<Picture>;
-  title: string | React.ReactNode;
+  media?: React.ReactNode;
+  title?: string | React.ReactNode;
   subtitle?: string | React.ReactNode;
   paragraph?: string | React.ReactNode;
   lines?: Array<string | React.ReactNode>;
 }) {
   return (
-    <Card className="overflow-hidden rounded-sm">
-      <div className="aspect-video bg-neutral-100 dark:bg-neutral-900">
-        {media ? (
-          <>
-            {media.map((picture) => (
-              <img
-                src={picture.src}
-                alt={picture.name}
-                className="w-full h-full object-cover"
-              />
-            ))}
-          </>
-        ) : (
-          <></>
-        )}
-      </div>
-      <CardContent className="pt-4">{title}</CardContent>
+    <Card className="overflow-hidden rounded-md">
+      <CardMediaRoot>
+        <>{media ? media : <div />}</>
+      </CardMediaRoot>
+      <CardContent className="pt-4 px-2 border-t">
+        {title}
+        <CardPropertyLines>
+          {lines?.map((l, i) => {
+            return <CardPropertyLine key={i}>{l}</CardPropertyLine>;
+          })}
+        </CardPropertyLines>
+      </CardContent>
     </Card>
   );
+}
+
+function CardMediaRoot({ children }: React.PropsWithChildren<{}>) {
+  return (
+    <div className="w-full aspect-video overflow-hidden bg-neutral-100 dark:bg-neutral-900">
+      {children}
+    </div>
+  );
+}
+
+function CardPropertyLines({ children }: React.PropsWithChildren<{}>) {
+  return <div className="flex flex-col w-full gap-2">{children}</div>;
+}
+
+function CardPropertyLine({ children }: React.PropsWithChildren<{}>) {
+  return <div className="flex">{children}</div>;
 }
