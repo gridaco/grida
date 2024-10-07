5d435aeb0382b2baf79fb0ff7dd488b6c952acd5
Universe <universe@grida.co>
2024-10-07 22:02:38 +0900

    document list

--- apps/forms/components/site/form-card.tsx
+++ apps/forms/components/site/form-card.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Image from "next/image";
 import { ResourceTypeIcon } from "../resource-type-icon";
-import type { GDocument } from "@/types";
+import type { GDocument, GDocumentType } from "@/types";
+import { Badge } from "../ui/badge";
+import { Labels } from "@/k/labels";
 
 export function GridCard({
   title,
@@ -12,7 +14,7 @@ export function GridCard({
   doctype,
 }: GDocument & { thumbnail?: string }) {
   return (
-    <div className="rounded border border-neutral-500/10 bg-white dark:bg-neutral-900 shadow-md">
+    <div className="group rounded border bg-background shadow-md h-full">
       {thumbnail ? (
         <Image
           className="object-cover w-full h-full"
@@ -24,20 +26,32 @@ export function GridCard({
       ) : (
         <div className="p-2 aspect-square w-full flex items-center justify-center border-b">
           <ResourceTypeIcon
-            type={has_connection_supabase ? "form-x-supabase" : doctype}
-            className="w-10 h-10"
+            type={doctype}
+            className="w-10 h-10 text-muted-foreground group-hover:text-foreground transition-colors"
           />
         </div>
       )}
       <div className="px-4 py-2 flex flex-col gap-2">
         <span>{title}</span>
+        <div className="flex flex-wrap gap-1">
+          <Badges
+            doctype={doctype}
+            has_connection_supabase={has_connection_supabase}
+          />
+        </div>
         <span className="text-xs opacity-50">
-          {max_responses ? (
+          {doctype === "v0_form" ? (
             <>
-              {responses} / {max_responses} responses
+              {max_responses ? (
+                <>
+                  {responses} / {max_responses} responses
+                </>
+              ) : (
+                <>{responses} responses</>
+              )}
             </>
           ) : (
-            <>{responses} responses</>
+            <>--</>
           )}
         </span>
       </div>
@@ -56,7 +70,7 @@ export function RowCard({
   has_connection_supabase,
 }: GDocument & { thumbnail?: string }) {
   return (
-    <div className="flex items-center border rounded-md overflow-hidden h-16 shadow bg-white dark:bg-neutral-900">
+    <div className="flex items-center border rounded-md overflow-hidden h-16 shadow bg-background">
       {thumbnail ? (
         <Image
           className="object-cover max-w-16 bg-neutral-500 aspect-square"
@@ -67,32 +81,67 @@ export function RowCard({
         />
       ) : (
         <div className="p-2 aspect-square h-full flex items-center justify-center border-r">
-          <ResourceTypeIcon
-            type={has_connection_supabase ? "form-x-supabase" : doctype}
-            className="w-5 h-5"
-          />
+          <ResourceTypeIcon type={doctype} className="w-5 h-5" />
         </div>
       )}
-      <div className="flex-1 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+      <div className="flex-1 px-6 font-medium whitespace-nowrap">
         <div className="flex flex-col">
-          <span>{title}</span>
-          <span className="text-xs font-normal opacity-50">
-            Created: {new Date(created_at).toLocaleDateString()}
+          <span className="flex gap-2">
+            <span>{title}</span>
+            <Badges
+              doctype={doctype}
+              has_connection_supabase={has_connection_supabase}
+            />
+          </span>
+          <span className="flex gap-2">
+            <span className="text-xs font-normal opacity-50">
+              Created: {new Date(created_at).toLocaleDateString()}
+            </span>
           </span>
         </div>
       </div>
       <div className="opacity-80 w-32 text-sm">
-        {max_responses ? (
+        {doctype === "v0_form" ? (
           <>
-            {responses} / {max_responses}
+            {max_responses ? (
+              <>
+                {responses} / {max_responses}
+              </>
+            ) : (
+              <>{responses}</>
+            )}
           </>
         ) : (
-          <>{responses}</>
+          <>--</>
         )}
       </div>
       <div className="opacity-80 w-44 text-sm">
         {new Date(updated_at).toLocaleDateString()}
       </div>
     </div>
+  );
+}
+
+function Badges({
+  doctype,
+  has_connection_supabase,
+}: {
+  doctype: GDocumentType;
+  has_connection_supabase: boolean;
+}) {
+  return (
+    <>
+      <Badge
+        variant="outline"
+        className="text-xs text-muted-foreground font-normal font-mono px-1.5"
+      >
+        {Labels.doctype(doctype)}
+      </Badge>
+      {has_connection_supabase && (
+        <Badge variant="outline" className="p-1">
+          <ResourceTypeIcon type="form-x-supabase" className="w-4 h-4" />
+        </Badge>
+      )}
+    </>
   );
 }
