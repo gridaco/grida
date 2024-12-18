aab15c2d153e0b70907c21d448aee71ea7ba2f21
Universe <universe@grida.co>
2024-12-18 21:19:12 +0900

    split

--- apps/forms/grida-canvas/viewport/surface.tsx
+++ apps/forms/grida-canvas/viewport/surface.tsx
@@ -26,6 +26,7 @@ import { Knob } from "./ui/knob";
 import { ColumnsIcon, RowsIcon } from "@radix-ui/react-icons";
 import { cmath } from "../cmath";
 import { cursors } from "../components/cursor";
+import { SurfaceTextEditor } from "./ui/textarea";
 
 export function EditorSurface() {
   const isWindowResizing = useIsWindowResizing();
@@ -569,76 +570,6 @@ function LayerOverlayResizeHandle({
   );
 
   return <Knob size={size} {...bind()} anchor={anchor} />;
-}
-
-function SurfaceTextEditor({ node_id }: { node_id: string }) {
-  const inputref = useRef<HTMLTextAreaElement>(null);
-  const change = useNodeAction(node_id)!;
-  const transform = useNodeSurfaceTransfrom(node_id);
-  const node = useNode(node_id!);
-  const { tryExitContentEditMode } = useEventTarget();
-
-  useEffect(() => {
-    // select all text
-    if (inputref.current) {
-      inputref.current.select();
-    }
-  }, [inputref.current]);
-
-  const stopPropagation = (e: React.BaseSyntheticEvent) => {
-    e.stopPropagation();
-  };
-
-  return (
-    <div
-      id="richtext-editor-surface"
-      className="fixed left-0 top-0 w-0 h-0 z-10"
-    >
-      <div
-        style={{
-          position: "absolute",
-          ...transform,
-          willChange: "transform",
-          overflow: "hidden",
-          resize: "none",
-          zIndex: 1,
-        }}
-      >
-        {/* <div
-          autoFocus
-          // onInput={handleInput}
-          tabIndex={0}
-          contentEditable
-          suppressContentEditableWarning
-          className="appearance-none bg-transparent border-none outline-none p-0 m-0"
-        /> */}
-        <textarea
-          ref={inputref}
-          // TODO: only supports literal text value
-          onPointerDown={stopPropagation}
-          value={node.text as string}
-          maxLength={node.maxLength}
-          onBlur={tryExitContentEditMode}
-          onKeyDown={(e) => {
-            stopPropagation(e);
-            if (e.key === "Escape") {
-              inputref.current?.blur();
-            }
-          }}
-          onChange={(e) => {
-            change.text(e.target.value);
-          }}
-          className="m-0 p-0 border-none outline-none appearance-none bg-transparent h-full overflow-visible resize-none"
-          style={{
-            width: "calc(100% + 1px)",
-            ...grida.program.css.toReactTextStyle(
-              node as grida.program.nodes.TextNode
-            ),
-          }}
-        />
-      </div>
-    </div>
-  );
 }
 
 function usePrefferedDistributionAxis() {
