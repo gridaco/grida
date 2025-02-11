481325ce1f6d2e2150cf38362c32095b11d091ca
ryujunghy3on <ryu@grida.co>
2025-02-12 03:04:55 +0900

    .

--- apps/forms/www/ui/carousel.tsx
+++ apps/forms/www/ui/carousel.tsx
@@ -187,7 +187,7 @@ const CarouselItem = React.forwardRef<
       aria-roledescription="slide"
       className={cn(
         "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "pl-4" : "pt-4",
+        orientation === "horizontal" ? "" : "pt-4",
         className
       )}
       {...props}
