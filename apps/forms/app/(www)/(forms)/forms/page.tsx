4d7d7ff58f4c3985c51549e2943a841252ca601a
Universe <universe@grida.co>
2025-02-12 05:08:22 +0900

    mv

--- apps/forms/app/(www)/(forms)/forms/page.tsx
+++ apps/forms/app/(www)/(forms)/forms/page.tsx
@@ -7,14 +7,14 @@ import {
   AnalysisIcon,
   VisualStudioIcon,
 } from "@/www/icons";
-import Hero from "./.sections/hero";
-import Demo from "./.sections/demo";
+import Hero from "./_sections/hero";
+import Demo from "./_sections/demo";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Header from "../header";
 import Footer from "@/www/footer";
-import { FAQ } from "./.sections/faq";
-import Bento from "./.sections/bento";
+import { FAQ } from "./_sections/faq";
+import Bento from "./_sections/bento";
 
 export default function Home() {
   return (
