b167b5811ec89b3c496e1fe11c84ec6bf941e312
Universe <universe@grida.co>
2025-03-01 16:27:55 +0900

    chore

--- editor/scaffolds/desktop/index.tsx
+++ editor/scaffolds/desktop/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { DesktopDragArea } from "@/host/desktop-drag-area";
 import { usePlatform } from "@/host/platform-provider";
+import * as k from "./k";
 
 export function DesktopSidebarHeaderDragArea() {
   const platform = usePlatform();
@@ -10,32 +11,6 @@ export function DesktopSidebarHeaderDragArea() {
   return null;
 }
 
-/**
- * macOS traffic lights width (without the padding or position)
- */
-const DARWIN_TRAFFIC_LIGHTS_WIDTH = 54;
-
-/**
- * macOS traffic lights height (without the padding or position)
- */
-const DARWIN_TRAFFIC_LIGHTS_HEIGHT = 16;
-
-/**
- * win32 window control buttons width - positioned at the top right corner
- */
-const WIN32_WINDOW_CONTROL_BUTTONS_WIDTH = 135;
-
-/**
- * linux window control buttons width - positioned at the top right corner
- * (may vary by theme) usually 100 ~ 120 (there is no accirate way to set/get this)
- */
-const LINUX_WINDOW_CONTROL_BUTTONS_WIDTH = 120;
-
-/**
- * title bar height, also a window control buttons height on win32 and linux.
- */
-const TITLEBAR_HEIGHT = 44;
-
 export function Win32LinuxWindowSafeArea() {
   const platform = usePlatform();
   if (!platform.is_desktop_app) return null;
@@ -44,7 +19,7 @@ export function Win32LinuxWindowSafeArea() {
       return (
         <div
           style={{
-            width: LINUX_WINDOW_CONTROL_BUTTONS_WIDTH,
+            width: k.DESKTOP_LINUX_WINDOW_CONTROL_BUTTONS_WIDTH,
           }}
         />
       );
@@ -52,7 +27,7 @@ export function Win32LinuxWindowSafeArea() {
       return (
         <div
           style={{
-            width: WIN32_WINDOW_CONTROL_BUTTONS_WIDTH,
+            width: k.DESKTOP_WIN32_WINDOW_CONTROL_BUTTONS_WIDTH,
           }}
         />
       );
