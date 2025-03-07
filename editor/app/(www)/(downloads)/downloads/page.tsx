de4fbd71fe68f9a448a869e1f8bfea9b5911eabd
Universe <universe@grida.co>
2025-03-07 19:35:43 +0900

    download links

--- editor/app/(www)/(downloads)/downloads/page.tsx
+++ editor/app/(www)/(downloads)/downloads/page.tsx
@@ -4,31 +4,54 @@ import Header from "@/www/header";
 import { Button } from "@/components/ui/button";
 import { Apple } from "@/components/logos/apple";
 import { Windows } from "@/components/logos/windows";
-import { DownloadIcon } from "@radix-ui/react-icons";
+import { Linux } from "@/components/logos/linux";
 import Link from "next/link";
 import { sitemap } from "@/www/data/sitemap";
 import type { Metadata } from "next";
-import { AppleIcon } from "lucide-react";
-import { motion } from "framer-motion";
+import { downloads } from "./downloads";
+import { headers } from "next/headers";
+import { DownloadIcon } from "@radix-ui/react-icons";
 
 export const metadata: Metadata = {
   title: "Downloads",
   description: "Download Grida",
 };
 
-export default function DownloadsPage() {
+const oslabel = {
+  mac: "macOS",
+  windows: "Windows",
+  linux: "Linux",
+};
+
+function OSIcon({
+  os,
+  className,
+}: {
+  os: "mac" | "windows" | "linux";
+  className?: string;
+}) {
+  switch (os) {
+    case "mac":
+      return <Apple className={className} />;
+    case "windows":
+      return <Windows className={className} />;
+    case "linux":
+      return <Linux className={className} />;
+    default:
+      return null;
+  }
+}
+
+export default async function DownloadsPage() {
+  const headersList = await headers();
+  const userAgent = headersList.get("user-agent");
+
+  const os = downloads.getDesktopOS(userAgent || "");
+  const links = await downloads.getLinks_v001(os);
+
   return (
     <main className="min-h-screen flex flex-col">
       <Header />
-      {/* <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        viewport={{ once: true }}
-        whileInView={{ opacity: 1, y: 20 }}
-        transition={{ duration: 5, ease: "easeOut" }}
-        className="absolute -z-10 inset-0 flex items-center justify-center"
-      >
-        <HeroBackground />
-      </motion.div> */}
       <div className="h-60" />
       <main className="flex-grow flex items-center">
         <section className="container max-w-2xl mx-auto py-20 text-center border rounded-lg">
@@ -39,21 +62,20 @@ export default function DownloadsPage() {
             Optimized for macOS. Download now and start creating effortlessly.
           </p>
           <div className="flex justify-center space-x-4">
-            <Link href={sitemap.links.releases_latest}>
-              <Button size="lg">
-                <Apple className="mr-2 h-4 w-4" /> Download for macOS
-              </Button>
-            </Link>
-            {/* <Link href={sitemap.links.releases_latest}>
-              <Button size="lg">
-                <DownloadIcon className="mr-2 h-4 w-4" /> Download for Windows
-              </Button>
-            </Link> */}
-            {/* <Link href={sitemap.links.releases_latest}>
-              <Button size="lg" >
-                <DownloadIcon className="mr-2 h-4 w-4" /> Download for Linux
-              </Button>
-            </Link> */}
+            {os ? (
+              <Link href={links.default!.url}>
+                <Button size="lg">
+                  <OSIcon os={os} className="mr-2 size-4" /> Download for{" "}
+                  {oslabel[os]}
+                </Button>
+              </Link>
+            ) : (
+              <Link href={sitemap.links.releases_latest}>
+                <Button size="lg">
+                  <DownloadIcon className="mr-2 size-4" /> Download
+                </Button>
+              </Link>
+            )}
           </div>
           <div className="mt-2"></div>
         </section>
@@ -66,7 +88,7 @@ export default function DownloadsPage() {
         <p className="text-center text-base text-muted-foreground mb-12">
           A faster, more focused experience awaits.
         </p>
-        <DownloadButtons />
+        <DownloadButtons links={links} />
       </div>
       <div className="h-96" />
       <Footer />
@@ -74,112 +96,52 @@ export default function DownloadsPage() {
   );
 }
 
-function DownloadButtons() {
+function DownloadButtons({ links }: { links: downloads.DownloadLinks }) {
   return (
-    <div className="container flex flex-wrap justify-center gap-3 mx-auto">
+    <div className="container max-w-3xl flex flex-wrap justify-center gap-3 mx-auto">
       {/* macOS Universal */}
-      <Link href={sitemap.links.releases_latest}>
+      <Link href={links.mac_dmg_universal} download>
         <Button size="lg">
-          <Apple className="mr-2 h-4 w-4" /> Download for macOS (Universal)
+          <Apple className="mr-2 size-4" /> Download for macOS (Universal)
         </Button>
       </Link>
 
       {/* macOS Apple Silicon */}
-      <Link href={sitemap.links.releases_latest}>
+      <Link href={links.mac_dmg_arm64} download>
         <Button size="lg">
-          <Apple className="mr-2 h-4 w-4" /> Download for macOS (Apple Silicon)
+          <Apple className="mr-2 size-4" /> Download for macOS (Apple Silicon)
         </Button>
       </Link>
 
       {/* macOS Intel */}
-      <Link href={sitemap.links.releases_latest}>
+      <Link href={links.mac_dmg_x64} download>
         <Button size="lg">
-          <Apple className="mr-2 h-4 w-4" /> Download for macOS (Intel-based
+          <Apple className="mr-2 size-4" /> Download for macOS (Intel-based
           Macs)
         </Button>
       </Link>
 
       {/* Windows x64 */}
-      <Link href={sitemap.links.releases_latest}>
+      <Link href={links.windows_exe_x64} download>
         <Button size="lg">
-          <Windows className="mr-2 h-3 w-3" /> Download for Windows (x64)
-        </Button>
-      </Link>
-
-      {/* Windows ARM */}
-      <Link href={sitemap.links.releases_latest}>
-        <Button size="lg">
-          <Windows className="mr-2 h-3 w-3" /> Download for Windows (Arm /
-          Arm64)
+          <Windows className="mr-2 size-4" /> Download for Windows (x64)
         </Button>
       </Link>
 
       {/* Windows Debian */}
-      <Link href={sitemap.links.releases_latest}>
+      <Link href={links.linux_deb_x64} download>
         <Button size="lg">
-          <Windows className="mr-2 h-3 w-3" /> Download for Linux (Debian /
-          Ubuntu)
+          <Linux className="mr-2 size-4" /> Download for Linux (Debian / Ubuntu)
         </Button>
       </Link>
 
       {/* Windows Red Hat */}
-      <Link href={sitemap.links.releases_latest}>
+      <Link href={links.linux_rpm_x64} download>
         <Button size="lg">
-          <Windows className="mr-2 h-3 w-3" /> Download for Linux (Red Hat /
-          Fedora / SUSE)
+          <Linux className="mr-2 size-4" /> Download for Linux (Red Hat / Fedora
+          / SUSE)
         </Button>
       </Link>
     </div>
   );
 }
-
-// export function HeroBackground() {
-//   const path =
-//     "M6.1912 307.719C-28.0369 229.284 87.9827 154.311 150.271 126.63C322.143 -54.4597 724.031 9.92764 929.546 16.1469C1135.06 22.3661 990.981 163.214 1119.7 329.304C1248.42 495.394 1006.34 581 960.263 669.898C914.187 758.797 589.093 602.218 494.015 669.898C398.937 737.578 168.555 623.803 171.847 470.517C175.138 317.231 48.9763 405.764 6.1912 307.719Z";
-//   return (
-//     <div className="absolute -top-40">
-//       <svg
-//         width={"1157"}
-//         height="698"
-//         viewBox="0 0 1157 698"
-//         fill="none"
-//         xmlns="http://www.w3.org/2000/svg"
-//         className="overflow-visible [--gradient:url(#light)] dark:[--gradient:url(#dark)]"
-//       >
-//         <filter id="blur" x="-50%" y="-50%" width="300%" height="300%">
-//           <feGaussianBlur in="SourceGraphic" stdDeviation="200" />
-//         </filter>
-//         <defs>
-//           <radialGradient
-//             id="light"
-//             cx="0"
-//             cy="0"
-//             r="1"
-//             gradientUnits="userSpaceOnUse"
-//             gradientTransform="translate(579 236) rotate(85) scale(522 865)"
-//           >
-//             <stop stopColor="#CDBAFF" />
-//             <stop stopColor="#A06EFF" stopOpacity="1" />
-//             <stop offset="0.36" stopColor="#8298FF" stopOpacity="1" />{" "}
-//             <stop offset="0.61" stopColor="#60CFFF" stopOpacity="0.9" />{" "}
-//             <stop offset="1" stopColor="#9EF3E8" stopOpacity="0.8" />{" "}
-//           </radialGradient>
-//           <radialGradient
-//             id="dark"
-//             cx="0"
-//             cy="0"
-//             r="1"
-//             gradientUnits="userSpaceOnUse"
-//             gradientTransform="translate(579 236) rotate(85) scale(522 865)"
-//           >
-//             <stop stopColor="#4F3791" />
-//             <stop offset="0.36" stopColor="#425397" />
-//             <stop offset="0.61" stopColor="#3383A0" />
-//             <stop offset="1" stopColor="#34D7C4" />
-//           </radialGradient>
-//         </defs>
-//         <path d={path} fill="var(--gradient)" filter="url(#blur)" />
-//       </svg>
-//     </div>
-//   );
-// }
--- editor/components/logos/windows.tsx
+++ editor/components/logos/windows.tsx
@@ -2,15 +2,8 @@ import React from "react";
 
 export const Windows = ({ className }: { className?: string }) => {
   return (
-    <svg
-      viewBox="0 0 512 512"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      fill="currentColor"
-      className={className}
-    >
-      <path d="M0 0h230v230H0zm282 0h230v230H282zM0 282h230v230H0zm282 0h230v230H282z" />{" "}
+    <svg viewBox="0 0 88 88" fill="currentColor" className={className}>
+      <path d="m0,12.402,35.687-4.8602,0.0156,34.423-35.67,0.20313zm35.67,33.529,0.0277,34.453-35.67-4.9041-0.002-29.78zm4.3261-39.025,47.318-6.906,0,41.527-47.318,0.37565zm47.329,39.349-0.0111,41.34-47.318-6.6784-0.0663-34.739z"></path>
     </svg>
   );
 };
