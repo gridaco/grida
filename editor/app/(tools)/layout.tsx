e79a8919cc6d4c9b1d31a896a4abd24900db1ed2
Universe <universe@grida.co>
2025-03-17 15:04:06 +0900

    add e164 tool page

---
+++ editor/app/(tools)/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Grida | Tools",
+  description: "Free collection of tools",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+      {process.env.NEXT_PUBLIC_GAID && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GAID} />
+      )}
+    </html>
+  );
+}
