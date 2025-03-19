f81c0aece64bda43aafe11cc4428e6cf112611a2
Universe <universe@grida.co>
2025-03-19 14:38:33 +0900

    update seed

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -5,16 +5,44 @@
 -- #region local user
 -- create local users (insiders)
 -- insiders@grida.co / password
-INSERT INTO auth.users ( instance_id, id, aud, role, email, encrypted_password, email_confirmed_at, recovery_sent_at, last_sign_in_at, raw_app_meta_data, raw_user_meta_data, created_at, updated_at, confirmation_token, email_change, email_change_token_new, recovery_token) 
-VALUES 
-  ('00000000-0000-0000-0000-000000000000', uuid_generate_v4(), 'authenticated', 'authenticated', 'insiders@grida.co', crypt('password', gen_salt('bf')), current_timestamp, current_timestamp, current_timestamp, '{"provider":"email","providers":["email"]}', '{}', current_timestamp, current_timestamp, '', '', '', '');
 
 
--- test user email identity
-INSERT INTO auth.identities (id, user_id, identity_data, provider, provider_id, last_sign_in_at, created_at, updated_at)
-VALUES 
-  (uuid_generate_v4(), (SELECT id FROM auth.users WHERE email = 'insiders@grida.co'), format('{"sub":"%s","email":"%s"}', (SELECT id FROM auth.users WHERE email = 'insiders@grida.co')::text, 'insiders@grida.co')::jsonb, 'email', uuid_generate_v4(), current_timestamp, current_timestamp, current_timestamp);
 
+-- #region local user
+-- create local users (insiders)
+
+CREATE OR REPLACE FUNCTION public.tmp_seed_create_user(user_email text, user_password text)
+RETURNS uuid AS $$
+DECLARE
+  new_user_id uuid := uuid_generate_v4();
+BEGIN
+  INSERT INTO auth.users ( instance_id, id, aud, role, email, encrypted_password, email_confirmed_at, recovery_sent_at, last_sign_in_at, raw_app_meta_data, raw_user_meta_data, created_at, updated_at, confirmation_token, email_change, email_change_token_new, recovery_token) 
+  VALUES 
+    ('00000000-0000-0000-0000-000000000000', uuid_generate_v4(), 'authenticated', 'authenticated', user_email, crypt(user_password, gen_salt('bf')), current_timestamp, current_timestamp, current_timestamp, '{"provider":"email","providers":["email"]}', '{}', current_timestamp, current_timestamp, '', '', '', '');
+
+
+  -- test user email identity
+  INSERT INTO auth.identities (id, user_id, identity_data, provider, provider_id, last_sign_in_at, created_at, updated_at)
+  VALUES 
+    (uuid_generate_v4(), (SELECT id FROM auth.users WHERE email = user_email), format('{"sub":"%s","email":"%s"}', (SELECT id FROM auth.users WHERE email = user_email)::text, user_email)::jsonb, 'email', uuid_generate_v4(), current_timestamp, current_timestamp, current_timestamp);
+
+
+  RETURN new_user_id;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- must be wrapped in do block (https://github.com/supabase/cli/issues/882#issuecomment-1595725535)
+do $$
+begin
+    -- insiders@grida.co / password (default - organization owner)
+    perform public.tmp_seed_create_user('insiders@grida.co', 'password');
+    -- outsiders@grida.co / password
+    perform public.tmp_seed_create_user('outsiders@grida.co', 'password');
+end$$;
+
+
+DROP FUNCTION public.tmp_seed_create_user(text, text);
 -- #endregion local user
 
 
