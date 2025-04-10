bfbeec19f0a348108397634e4fd96a9cbe190f25
Universe <universe@grida.co>
2025-04-10 20:41:49 +0900

    mv

---
+++ editor/app/(workbench)/[org]/[proj]/(console)/(resources)/layout.tsx
@@ -0,0 +1,156 @@
+import { ToasterWithMax } from "@/components/toaster";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarFooter,
+  SidebarGroupLabel,
+} from "@/components/ui/sidebar";
+import { EditorHelpFab } from "@/scaffolds/help/editor-help-fab";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
+import { ResourceTypeIcon } from "@/components/resource-type-icon";
+import { AboutGridaWestCard } from "./about-west-card";
+import Link from "next/link";
+
+type Params = { org: string; proj: string };
+
+export default async function Layout({
+  params,
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<Params>;
+}>) {
+  const { org, proj } = await params;
+
+  return (
+    <SidebarProvider>
+      <div className="flex flex-1 overflow-y-auto">
+        <div className="h-full flex flex-1 w-full">
+          <Sidebar>
+            <SidebarHeader>
+              <SidebarMenu>
+                <Link href={`/${org}/${proj}`}>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton>
+                      <ArrowLeftIcon />
+                      Project Console
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </Link>
+              </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+              <SidebarGroup>
+                <SidebarGroupLabel>Customer</SidebarGroupLabel>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    <Link href={`/${org}/${proj}/customers`}>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton size="sm">
+                          <ResourceTypeIcon
+                            type="customer"
+                            className="size-4"
+                          />
+                          Customers
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </Link>
+                    <Link href={`/${org}/${proj}/tags`}>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton size="sm">
+                          <ResourceTypeIcon type="tag" className="size-4" />
+                          Tags
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </Link>
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </SidebarGroup>
+              <SidebarGroup>
+                <SidebarGroupLabel>Marketing</SidebarGroupLabel>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    <Link href={`/${org}/${proj}/campaigns`}>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton size="sm">
+                          <ResourceTypeIcon
+                            type="campaign"
+                            className="size-4"
+                          />
+                          Campaigns
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </Link>
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </SidebarGroup>
+              <SidebarGroup>
+                <SidebarGroupLabel>Site</SidebarGroupLabel>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    <Link href={`/${org}/${proj}/www`}>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton size="sm">
+                          <ResourceTypeIcon type="v0_site" className="size-4" />
+                          Site Settings
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </Link>
+                    <Link href={`/${org}/${proj}/analytics`}>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton size="sm">
+                          <ResourceTypeIcon type="chart" className="size-4" />
+                          Analytics
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </Link>
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </SidebarGroup>
+              <SidebarGroup>
+                <SidebarGroupLabel>Advanced</SidebarGroupLabel>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    <Link href={`/${org}/${proj}/customers/policies`}>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton size="sm">
+                          <ResourceTypeIcon type="auth" className="size-4" />
+                          Customer Auth
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </Link>
+                    <Link href={`/${org}/${proj}/integrations`}>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton size="sm">
+                          <ResourceTypeIcon type="connect" className="size-4" />
+                          Connections
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </Link>
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+              <AboutGridaWestCard />
+            </SidebarFooter>
+          </Sidebar>
+          <div className="flex flex-col overflow-hidden w-full h-full">
+            <div className="w-full h-full overflow-x-hidden overflow-y-auto">
+              {children}
+            </div>
+          </div>
+        </div>
+      </div>
+      <EditorHelpFab />
+      <ToasterWithMax position="bottom-center" max={5} />
+    </SidebarProvider>
+  );
+}
---
+++ editor/app/(workbench)/[org]/[proj]/(console)/layout.tsx
@@ -0,0 +1,46 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ToasterWithMax } from "@/components/toaster";
+import { EditorHelpFab } from "@/scaffolds/help/editor-help-fab";
+import { Inter } from "next/font/google";
+import { ProjectLoaded, ProjectTagsProvider } from "@/scaffolds/workspace";
+
+const inter = Inter({ subsets: ["latin"] });
+
+type Params = { org: string; proj: string };
+
+export default async function Layout({
+  params,
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<Params>;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="h-screen flex flex-col">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div className="flex flex-1 overflow-y-auto">
+              <div className="h-full flex flex-1 w-full">
+                <div className="flex flex-col overflow-hidden w-full h-full">
+                  <div className="w-full h-full overflow-x-hidden overflow-y-auto">
+                    <ProjectLoaded>
+                      <ProjectTagsProvider>{children}</ProjectTagsProvider>
+                    </ProjectLoaded>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <EditorHelpFab />
+            <ToasterWithMax position="bottom-center" max={5} />
+          </ThemeProvider>
+        </div>
+      </body>
+    </html>
+  );
+}
