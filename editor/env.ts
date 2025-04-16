4b1a963c0fce673c0baeba9dd150a7de8b0938db
Universe <universe@grida.co>
2025-04-16 16:38:16 +0900

    add rr envs

--- editor/env.ts
+++ editor/env.ts
@@ -18,4 +18,26 @@ export namespace Env {
         "https://" + process.env.NEXT_PUBLIC_URL
       : "http://localhost:3000";
   }
+
+  /**
+   * supabase infra - envs are available for bothe server and client
+   */
+  export namespace supabase {
+    /**
+     * [Primary] -
+     */
+    export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+
+    /**
+     * [Replica] - ap-northeast-1 (Tokyo)
+     */
+    export const SUPABASE_URL_RR_AP_NORTHEAST_1 =
+      process.env.NEXT_PUBLIC_SUPABASE_URL_RR_AP_NORTHEAST_1;
+
+    /**
+     * [Replica] - ap-northeast-2 (Seoul)
+     */
+    export const SUPABASE_URL_RR_AP_NORTHEAST_2 =
+      process.env.NEXT_PUBLIC_SUPABASE_URL_RR_AP_NORTHEAST_2;
+  }
 }
