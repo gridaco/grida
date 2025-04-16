50859cc2a614a43c7ab93ea27c4fea20dd54a310
Universe <universe@grida.co>
2025-04-16 18:24:24 +0900

    use read replicas

--- editor/lib/supabase/server.ts
+++ editor/lib/supabase/server.ts
@@ -1,13 +1,15 @@
 import type { Database } from "@/database.types";
+import { Env } from "@/env";
 import { createServerComponentClient as _createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { createRouteHandlerClient as _createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { createClient } from "@supabase/supabase-js";
+import { createClient as _createClient } from "@supabase/supabase-js";
+import { geolocation } from "@vercel/functions";
 import type { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
 
 /**
  * @deprecated - deprecation warning for extra security (not actually deprecated)
  */
-export const workspaceclient = createClient<Database, "public">(
+export const workspaceclient = _createClient<Database, "public">(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
   {
@@ -20,7 +22,7 @@ export const workspaceclient = createClient<Database, "public">(
 /**
  * @deprecated - deprecation warning for extra security (not actually deprecated)
  */
-export const grida_forms_client = createClient<Database, "grida_forms">(
+export const grida_forms_client = _createClient<Database, "grida_forms">(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
   {
@@ -33,7 +35,7 @@ export const grida_forms_client = createClient<Database, "grida_forms">(
 /**
  * @deprecated - deprecation warning for extra security (not actually deprecated)
  */
-export const grida_storage_client = createClient<Database, "grida_storage">(
+export const grida_storage_client = _createClient<Database, "grida_storage">(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
   {
@@ -46,7 +48,7 @@ export const grida_storage_client = createClient<Database, "grida_storage">(
 /**
  * @deprecated - deprecation warning for extra security (not actually deprecated)
  */
-export const grida_canvas_client = createClient<Database, "grida_canvas">(
+export const grida_canvas_client = _createClient<Database, "grida_canvas">(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
   {
@@ -59,7 +61,7 @@ export const grida_canvas_client = createClient<Database, "grida_canvas">(
 /**
  * @deprecated - deprecation warning for extra security (not actually deprecated)
  */
-export const grida_sites_client = createClient<Database, "grida_sites">(
+export const grida_sites_client = _createClient<Database, "grida_sites">(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
   {
@@ -72,7 +74,7 @@ export const grida_sites_client = createClient<Database, "grida_sites">(
 /**
  * @deprecated - deprecation warning for extra security (not actually deprecated)
  */
-export const grida_commerce_client = createClient<Database, "grida_commerce">(
+export const grida_commerce_client = _createClient<Database, "grida_commerce">(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_KEY!,
   {
@@ -85,7 +87,7 @@ export const grida_commerce_client = createClient<Database, "grida_commerce">(
 /**
  * @deprecated - deprecation warning for extra security (not actually deprecated)
  */
-export const grida_west_referral_client = createClient<
+export const grida_west_referral_client = _createClient<
   Database,
   "grida_west_referral"
 >(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!, {
@@ -97,7 +99,7 @@ export const grida_west_referral_client = createClient<
 /**
  * @deprecated - deprecation warning for extra security (not actually deprecated)
  */
-export const grida_xsupabase_client = createClient<
+export const grida_xsupabase_client = _createClient<
   Database,
   "grida_x_supabase"
 >(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_KEY!, {
@@ -201,6 +203,73 @@ export const createRouteHandlerWWWClient = (
       },
     }
   );
+
+/**
+ * supabase read replica clients
+ */
+export namespace sb.rr {
+  interface Request {
+    headers: Headers;
+    cookies: ReadonlyRequestCookies;
+  }
+
+  function supabaseUrl(request: { headers: Headers }) {
+    const geo = geolocation(request);
+    const region = Env.vercel.region(geo.region);
+    const url = Env.supabase.rr(region);
+    return url;
+  }
+
+  export namespace www {
+    export function createRouteHandlerClient(request: Request) {
+      return _createRouteHandlerClient<Database, "grida_www">(
+        {
+          cookies: () => request.cookies,
+        },
+        {
+          supabaseUrl: supabaseUrl(request),
+          options: {
+            db: { schema: "grida_www" },
+          },
+        }
+      );
+    }
+  }
+
+  export namespace west_referral {
+    /**
+     * @deprecated - deprecation warning for extra security (not actually deprecated)
+     */
+    export function createClient(request: Request) {
+      return _createClient<Database, "grida_west_referral">(
+        supabaseUrl(request),
+        process.env.SUPABASE_SERVICE_KEY!,
+        {
+          db: {
+            schema: "grida_west_referral",
+          },
+        }
+      );
+    }
+
+    export function createRouteHandlerClient(request: {
+      headers: Headers;
+      cookies: ReadonlyRequestCookies;
+    }) {
+      return _createRouteHandlerClient<Database, "grida_west_referral">(
+        {
+          cookies: () => request.cookies,
+        },
+        {
+          supabaseUrl: supabaseUrl(request),
+          options: {
+            db: { schema: "grida_west_referral" },
+          },
+        }
+      );
+    }
+  }
+}
 
 export const createRouteHandlerXSBClient = (
   cookieStore: ReadonlyRequestCookies
