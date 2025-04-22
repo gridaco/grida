def5e28e0e38f27fe72026ab87472c36ca073563
Universe <universe@grida.co>
2025-04-22 18:16:17 +0900

    mv uploader

--- editor/env.ts
+++ editor/env.ts
@@ -9,6 +9,39 @@ export namespace Env {
       : "http://localhost:3000";
   }
 
+  export namespace storage {
+    /**
+     * public, temporary file uploader to playground bucket
+     * for internal dev or public tmp playgrounds
+     *
+     * @public true
+     */
+    export const BUCKET_DUMMY = "dummy";
+
+    /**
+     * form media files (not response uploads)
+     *
+     * @public true
+     */
+    export const BUCKET_GRIDA_FORMS = "grida-forms";
+
+    /**
+     * private asset files
+     *
+     * @private
+     */
+    export const BUCKET_ASSETS = "assets";
+
+    /**
+     * public asset files
+     *
+     * for user uploaded contents in public cms
+     *
+     * @public
+     */
+    export const BUCKET_ASSETS_PUBLIC = "assets-public";
+  }
+
   /**
    * anything related to web & client side
    */
---
+++ editor/scaffolds/platform/storage/use-upload.ts
@@ -0,0 +1,51 @@
+"use client";
+
+import { FileIO } from "@/lib/file";
+import { createBrowserClient } from "@/lib/supabase/client";
+import { useCallback, useMemo } from "react";
+
+export function useStorageClient() {
+  return useMemo(() => createBrowserClient().storage, []);
+}
+
+export function useUpload(
+  bucket: string,
+  path: string | ((file: File) => string | Promise<string>)
+) {
+  const storage = useStorageClient();
+
+  return useCallback(
+    async (file: File): Promise<FileIO.UploadResult> => {
+      if (!file) throw new Error("No file provided");
+      const _path = typeof path === "function" ? await path(file) : path;
+      if (!_path) throw new Error("No path provided");
+      return await storage
+        .from(bucket)
+        .upload(_path, file, {
+          contentType: file.type,
+        })
+        .then(({ data, error }) => {
+          if (error) {
+            throw new Error("Failed to upload file");
+          }
+          const publicUrl = storage.from(bucket).getPublicUrl(_path)
+            .data.publicUrl;
+
+          const result = {
+            object_id: data.id,
+            bucket,
+            path: data.path,
+            fullPath: data.fullPath,
+            publicUrl,
+          } satisfies FileIO.UploadResult;
+
+          return result;
+        })
+        .catch((e) => {
+          console.error("upload error", e);
+          throw e;
+        });
+    },
+    [storage, bucket, path]
+  );
+}
