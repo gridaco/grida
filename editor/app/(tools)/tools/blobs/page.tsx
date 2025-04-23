fd86acf2aebc5ef2ba57775275da18d00a681cf1
Universe <universe@grida.co>
2025-04-23 19:29:41 +0900

    chore

--- editor/app/(tools)/tools/blobs/page.tsx
+++ editor/app/(tools)/tools/blobs/page.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import PhoneNumberTool from "./_page";
+import BlobGeneratorTool from "./_page";
 import Header from "@/www/header";
 import Footer from "@/www/footer";
 
 export const metadata: Metadata = {
   title: "Blob Generator",
   description: "Generate random blob designs",
+  category: "Design Tools",
   keywords:
     "blob, generator, random, design, design tools, online, batch, svg, blob path, metaball",
 };
@@ -15,7 +16,7 @@ export default function BlobGeneratorToolPage() {
     <main>
       <Header />
       <div className="py-40 min-h-screen flex flex-col items-center justify-center">
-        <PhoneNumberTool />
+        <BlobGeneratorTool />
       </div>
       <Footer />
     </main>
