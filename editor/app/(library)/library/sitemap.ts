645fff287355ddecbe0805b835e460ed0831f489
Universe <universe@grida.co>
2025-04-30 13:08:26 +0900

    sitemap

--- editor/app/(library)/library/sitemap.ts
+++ editor/app/(library)/library/sitemap.ts
@@ -1,12 +1,42 @@
 import type { MetadataRoute } from "next/types";
-import { list } from "../actions";
 import { Env } from "@/env";
+import { service_role } from "@/lib/supabase/server";
+
+async function fetchAllObjects(): Promise<
+  { id: string; updated_at: string }[]
+> {
+  const allObjects = [];
+  // hard limit
+  const pageSize = 5000;
+  let from = 0;
+  let to = pageSize - 1;
+
+  while (true) {
+    const { data, error } = await service_role.library
+      .from("object")
+      .select("id, updated_at")
+      .range(from, to);
+
+    if (error) throw error;
+
+    if (!data || data.length === 0) break;
+
+    allObjects.push(...data);
+
+    if (data.length < pageSize) break; // last page
+    from += pageSize;
+    to += pageSize;
+  }
+
+  return allObjects;
+}
 
 // Note: Google's limit is 50,000 URLs per sitemap
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  // TODO: need to update query as the library size grows (max 5,000 per page)
-  const objects = await list();
-  return objects.data.map(
+  const objects = await fetchAllObjects();
+
+  // const objects = await list();
+  return objects.map(
     (object) =>
       ({
         url: `${Env.gridaco}/library/o/${object.id}`,
