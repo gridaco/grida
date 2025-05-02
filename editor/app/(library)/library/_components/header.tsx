5254368faeb99f9efaaba8d20d8e501944c0c35d
Universe <universe@grida.co>
2025-05-02 21:42:44 +0900

    fix csr

--- editor/app/(library)/library/_components/header.tsx
+++ editor/app/(library)/library/_components/header.tsx
@@ -1,14 +1,12 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import { GridaLogo } from "@/components/grida-logo";
 import { Input } from "@/components/ui/input";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 export default function LibraryHeader() {
-  const searchParams = useSearchParams();
-  const search = searchParams.get("search") || "";
   return (
     <header className="flex items-center justify-between px-6 py-2 sticky top-0 bg-background z-10 border-b">
       <div className="flex-1 flex justify-start">
@@ -20,18 +18,28 @@ export default function LibraryHeader() {
         </Link>
       </div>
       <div className="flex-1 flex justify-center">
-        <form method="get" action="/library" className="w-full max-w-xl">
-          <Input
-            type="search"
-            name="search"
-            autoComplete="off"
-            placeholder="Search"
-            defaultValue={search}
-            className="w-full px-4 h-12 bg-muted rounded-lg border-none"
-          />
-        </form>
+        <Suspense>
+          <Search />
+        </Suspense>
       </div>
       <div className="flex-1 flex justify-end"></div>
     </header>
+  );
+}
+
+function Search() {
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  return (
+    <form method="get" action="/library" className="w-full max-w-xl">
+      <Input
+        type="search"
+        name="search"
+        autoComplete="off"
+        placeholder="Search"
+        defaultValue={search}
+        className="w-full px-4 h-12 bg-muted rounded-lg border-none"
+      />
+    </form>
   );
 }
