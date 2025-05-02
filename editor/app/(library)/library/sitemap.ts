e539a047b8ecd9cab8dd34addaf695cc9c7de317
Universe <universe@grida.co>
2025-05-02 21:00:30 +0900

    category sitemap

--- editor/app/(library)/library/sitemap.ts
+++ editor/app/(library)/library/sitemap.ts
@@ -1,6 +1,7 @@
 import type { MetadataRoute } from "next/types";
 import { Env } from "@/env";
 import { service_role } from "@/lib/supabase/server";
+import { listCategories } from "./actions";
 
 async function fetchAllObjects(): Promise<
   { id: string; updated_at: string }[]
@@ -33,15 +34,27 @@ async function fetchAllObjects(): Promise<
 
 // Note: Google's limit is 50,000 URLs per sitemap
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const categories = await listCategories();
   const objects = await fetchAllObjects();
 
-  // const objects = await list();
-  return objects.map(
+  const _c = categories.map(
+    (category) =>
+      ({
+        url: `${Env.gridaco}/library/${category.id}`,
+        changeFrequency: "hourly",
+        priority: 0.8,
+      }) satisfies MetadataRoute.Sitemap[number]
+  );
+
+  const _o = objects.map(
     (object) =>
       ({
         url: `${Env.gridaco}/library/o/${object.id}`,
         changeFrequency: "yearly",
         lastModified: object.updated_at,
+        priority: 0.1,
       }) satisfies MetadataRoute.Sitemap[number]
   );
+
+  return [..._c, ..._o];
 }
