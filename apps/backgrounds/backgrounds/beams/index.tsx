d90bafd370f248e2146ce50a05f668657ed2df97
Universe <universe@grida.co>
2025-05-08 21:12:04 +0900

    bump

--- apps/backgrounds/backgrounds/beams/index.tsx
+++ apps/backgrounds/backgrounds/beams/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { cn } from "@/utils/cn";
 
 export const BackgroundBeams = React.memo(
