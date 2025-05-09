34d54fc2d1b0da250bebd794248aa01a52eb86a5
Universe <universe@grida.co>
2025-05-09 20:05:39 +0900

    a11y sync

--- editor/components/ui/dialog.tsx
+++ editor/components/ui/dialog.tsx
@@ -1,8 +1,10 @@
+"use client";
+
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { Cross2Icon } from "@radix-ui/react-icons";
+import { X } from "lucide-react";
 
-import { cn } from "@/utils";
+import { cn } from "@/components/lib/utils";
 
 const Dialog = DialogPrimitive.Root;
 
@@ -46,7 +48,7 @@ const DialogContent = React.forwardRef<
       {children}
       {!hideCloseButton && (
         <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-          <Cross2Icon className="h-4 w-4" />
+          <X className="h-4 w-4" />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
       )}
