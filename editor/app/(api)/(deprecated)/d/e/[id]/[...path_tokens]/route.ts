5a469d1a6e76462f7dbfbce416f8767f62ef5ff3
Universe <universe@grida.co>
2025-05-10 17:07:46 +0900

    extra path

--- editor/app/(api)/(deprecated)/d/e/[id]/[...path_tokens]/route.ts
+++ editor/app/(api)/(deprecated)/d/e/[id]/[...path_tokens]/route.ts
@@ -1,6 +1,8 @@
+import { Env } from "@/env";
+import { service_role } from "@/lib/supabase/server";
 import { notFound } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
-import query_d_e from "./_query";
+import query_d_e from "../_query";
 
 /**
  * legacy /d/e/[id] redirection to tenant.grida.site
@@ -8,14 +10,19 @@ import query_d_e from "./_query";
 export async function GET(
   request: NextRequest,
   segmentData: {
-    params: Promise<{ id: string }>;
+    params: Promise<{
+      id: string;
+      path_tokens: string[];
+    }>;
   }
 ) {
-  const { id } = await segmentData.params;
+  const { id, path_tokens } = await segmentData.params;
+
   try {
     const { baseUrl } = await query_d_e(id);
+    const path = `/d/e/${id}/${path_tokens.join("/")}`;
 
-    return NextResponse.redirect(new URL(`/d/e/${id}`, baseUrl), {
+    return NextResponse.redirect(new URL(path, baseUrl), {
       status: 301,
     });
   } catch (error) {
