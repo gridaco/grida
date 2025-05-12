450586feefb3be4bbbe0d72c44980b3ff769d48d
Universe <universe@grida.co>
2025-05-13 04:05:20 +0900

    init turbo

--- editor/next.config.ts
+++ editor/next.config.ts
@@ -229,7 +229,7 @@ const sentry_build_options: SentryBuildOptions | null = USE_TELEMETRY
       // See the following for more information:
       // https://docs.sentry.io/product/crons/
       // https://vercel.com/docs/cron-jobs
-      automaticVercelMonitors: true,
+      automaticVercelMonitors: false,
     } satisfies SentryBuildOptions)
   : null;
 
