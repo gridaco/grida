c4de39c6d5dd350b784bb3501787a78b18e4a686
Universe <universe@grida.co>
2025-08-22 04:24:36 +0900

    text style wght support

--- crates/grida-canvas/src/painter/painter.rs
+++ crates/grida-canvas/src/painter/painter.rs
@@ -987,28 +987,51 @@ impl<'a> NodePainter<'a> {
     }
 }
 
-pub(crate) fn make_textstyle(text_style: &TextStyle) -> skia_safe::textlayout::TextStyle {
+pub(crate) fn make_textstyle(style: &TextStyle) -> skia_safe::textlayout::TextStyle {
     let mut ts = skia_safe::textlayout::TextStyle::new();
-    ts.set_font_size(text_style.font_size);
-    if let Some(letter_spacing) = text_style.letter_spacing {
-        ts.set_letter_spacing(letter_spacing);
-    }
-    if let Some(line_height) = text_style.line_height {
-        ts.set_height(line_height);
-    }
+
+    // [decoration]
     let mut decor = skia_safe::textlayout::Decoration::default();
-    decor.ty = text_style.text_decoration.into();
-    ts.set_decoration(&decor);
-    ts.set_font_families(&[&text_style.font_family]);
+    decor.ty = style.text_decoration.into();
+
     let font_style = skia_safe::FontStyle::new(
-        skia_safe::font_style::Weight::from(text_style.font_weight.value() as i32),
+        skia_safe::font_style::Weight::from(style.font_weight.value() as i32),
         skia_safe::font_style::Width::NORMAL,
-        if text_style.italic {
+        if style.italic {
             skia_safe::font_style::Slant::Italic
         } else {
             skia_safe::font_style::Slant::Upright
         },
     );
+
+    // [variables]
+
+    // [wght]
+    let wght = var_wght(style.font_weight.value() as f32);
+    let variation_position = skia_safe::font_arguments::VariationPosition {
+        coordinates: &[wght],
+    };
+    let font_args =
+        skia_safe::FontArguments::new().set_variation_design_position(variation_position);
+
+    //
+    ts.set_font_size(style.font_size);
+    if let Some(letter_spacing) = style.letter_spacing {
+        ts.set_letter_spacing(letter_spacing);
+    }
+    if let Some(line_height) = style.line_height {
+        ts.set_height(line_height);
+    }
+    ts.set_decoration(&decor);
+    ts.set_font_families(&[&style.font_family]);
     ts.set_font_style(font_style);
+    ts.set_font_arguments(&font_args);
     ts
+}
+
+fn var_wght(weight: f32) -> skia_safe::font_arguments::variation_position::Coordinate {
+    skia_safe::font_arguments::variation_position::Coordinate {
+        axis: skia_safe::FourByteTag::from(('w', 'g', 'h', 't')),
+        value: weight,
+    }
 }
