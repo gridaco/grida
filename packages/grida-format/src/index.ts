52ba3d6897103930fec5677da6efbb5231c413fe
Universe <universe@grida.co>
2026-01-03 00:35:11 +0900

    feat: introduce @grida/format package with generated TypeScript bindings for FlatBuffers schema

---
+++ packages/grida-format/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @grida/format - Generated FlatBuffers TypeScript bindings
+ *
+ * Re-exports generated types and builders from the FlatBuffers schema.
+ */
+
+// Re-export all generated types
+export * from "./grida";
