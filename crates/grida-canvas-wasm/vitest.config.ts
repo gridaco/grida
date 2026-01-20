ceb06a267c0a517dddf24631f1c30e6c2e6ff7be
Universe <universe@grida.co>
2026-01-20 22:33:25 +0900

    rm vitest alias

--- crates/grida-canvas-wasm/vitest.config.ts
+++ crates/grida-canvas-wasm/vitest.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from "vitest/config";
-import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./lib/", import.meta.url)),
-    },
-  },
   test: {
     globals: true,
     testTimeout: 30000, // allow time for WASM-related tests
   },
 });
-
